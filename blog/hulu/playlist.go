23673f037b7d6543019c2730f54d739e4f28afab
Steven Penny <spen+3052@protonmail.com>
2023-11-09 19:30:52 -0600

    +type playlist_request struct {

--- blog/hulu/playlist.go
+++ blog/hulu/playlist.go
@@ -1,4 +1,4 @@
-package main
+package hulu
 
 import (
    "io"
@@ -9,6 +9,56 @@ import (
    "strings"
 )
 
+type playlist_request struct {
+   Content_EAB_ID   string `json:"content_eab_id"`
+   Deejay_Device_ID int    `json:"deejay_device_id"`
+   Playback       struct {
+      Audio struct {
+         Codecs struct {
+            Selection_Mode string `json:"selection_mode"`
+            Values        []struct {
+               Type string `json:"type"`
+            } `json:"values"`
+         } `json:"codecs"`
+      } `json:"audio"`
+      DRM struct {
+         Selection_Mode string `json:"selection_mode"`
+         Values        []struct {
+            Security_Level string `json:"security_level"`
+            Type          string `json:"type"`
+            Version       string `json:"version"`
+         } `json:"values"`
+      } `json:"drm"`
+      Manifest struct {
+         Type string `json:"type"`
+      } `json:"manifest"`
+      Segments struct {
+         Selection_Mode string `json:"selection_mode"`
+         Values        []struct {
+            Encryption struct {
+               Mode string `json:"mode"`
+               Type string `json:"type"`
+            } `json:"encryption"`
+            Type string `json:"type"`
+         } `json:"values"`
+      } `json:"segments"`
+      Version int `json:"version"`
+      Video   struct {
+         Codecs struct {
+            Selection_Mode string `json:"selection_mode"`
+            Values        []struct {
+               Level   string `json:"level"`
+               Profile string `json:"profile"`
+               Type    string `json:"type"`
+            } `json:"values"`
+         } `json:"codecs"`
+      } `json:"video"`
+   } `json:"playback"`
+   Token          string `json:"token"`
+   Unencrypted    bool   `json:"unencrypted"`
+   Version        int    `json:"version"`
+}
+
 func main() {
    var req http.Request
    req.Header = make(http.Header)
@@ -16,9 +66,9 @@ func main() {
    req.ProtoMajor = 1
    req.ProtoMinor = 1
    req.URL = new(url.URL)
+   req.URL.Scheme = "https"
    req.URL.Host = "play.hulu.com"
    req.URL.Path = "/v6/playlist"
-   req.URL.Scheme = "https"
    req.Body = io.NopCloser(req_body)
    req.Header["Content-Type"] = []string{"application/json"}
    res, err := new(http.Transport).RoundTrip(&req)
@@ -32,63 +82,3 @@ func main() {
    }
    os.Stdout.Write(res_body)
 }
-
-var req_body = strings.NewReader(`
-{
-   "token": "DhA8XQLI1A9DkskfH3IaZ7L/aRU-nlLJF0thPd3yFttJT6_Ivg--uEEzNxGQrnh...",
-   "version": 5012541,
-   "deejay_device_id": 166,
-   "content_eab_id": "EAB::023c49bf-6a99-4c67-851c-4c9e7609cc1d::196861183::262714326",
-   "unencrypted": true,
-   "playback": {
-      "audio": {
-         "codecs": {
-            "selection_mode": "ONE",
-            "values": [
-               {
-                  "type": "AAC"
-               }
-            ]
-         }
-      },
-      "drm": {
-         "selection_mode": "ONE",
-         "values": [
-            {
-               "security_level": "L3",
-               "type": "WIDEVINE",
-               "version": "MODULAR"
-            }
-         ]
-      },
-      "manifest": {
-         "type": "DASH"
-      },
-      "segments": {
-         "selection_mode": "ONE",
-         "values": [
-            {
-               "encryption": {
-                  "mode": "CENC",
-                  "type": "CENC"
-               },
-               "type": "FMP4"
-            }
-         ]
-      },
-      "version": 2,
-      "video": {
-         "codecs": {
-            "selection_mode": "FIRST",
-            "values": [
-               {
-                  "level": "5.2",
-                  "profile": "HIGH",
-                  "type": "H264"
-               }
-            ]
-         }
-      }
-   }
-}
-`)
