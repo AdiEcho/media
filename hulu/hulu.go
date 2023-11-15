16fdfdf368d2832674f2208852d23b136e270dd5
3052 <spen+3052@protonmail.com>
2023-11-15 17:08:13 -0600

    deleted:    hulu/password.go

--- hulu/hulu.go
+++ hulu/hulu.go
@@ -7,6 +7,31 @@ import (
    "net/url"
 )
 
+type Authenticate struct {
+   Data struct {
+      User_Token string
+   }
+}
+
+func Living_Room(user, password string) (*Authenticate, error) {
+   res, err := http.PostForm(
+      "https://auth.hulu.com/v2/livingroom/password/authenticate", url.Values{
+         "friendly_name": {"!"},
+         "password": {password},
+         "serial_number": {"!"},
+         "user_email": {user},
+      },
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   auth := new(Authenticate)
+   if err := json.NewDecoder(res.Body).Decode(auth); err != nil {
+      return nil, err
+   }
+   return auth, nil
+}
 type Details struct {
    Items []struct {
       Series_Name string
