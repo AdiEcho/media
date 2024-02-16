2a604e5113bef200e86056afe25e5b8f14c75130
3052 <spen+3052@protonmail.com>
2024-02-15 21:57:46 -0600

    -   "os"

--- cbc/cbc.go
+++ cbc/cbc.go
@@ -5,15 +5,123 @@ import (
    "errors"
    "net/http"
    "net/url"
-   "os"
    "strings"
 )
+
+func (c GemCatalog) Item() (*LineupItem, bool) {
+   for _, content := range c.Content {
+      for _, lineup := range content.Lineups {
+         for _, item := range lineup.Items {
+            if item.URL == c.SelectedUrl {
+               return &item, true
+            }
+         }
+      }
+   }
+   return nil, false
+}
+
+func (g *GemProfile) Unmarshal() error {
+   return json.Unmarshal(g.Raw, &g.Gem)
+}
+
+type GemProfile struct {
+   Gem struct {
+      ClaimsToken string
+   }
+   Raw []byte
+}
+
+type MediaService struct {
+   Message string
+   URL string
+}
+
+func (t LoginToken) Profile() (*GemProfile, error) {
+   req, err := http.NewRequest("GET", "https://services.radio-canada.ca", nil)
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Path = "/ott/subscription/v2/gem/Subscriber/profile"
+   req.URL.RawQuery = "device=phone_android"
+   req.Header.Set("Authorization", "Bearer " + t.Access_Token)
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   gem := new(GemProfile)
+   if err := json.NewDecoder(res.Body).Decode(gem); err != nil {
+      return nil, err
+   }
+   return gem, nil
+}
+
+func (g *GemCatalog) New(address string) error {
+   // you can also use `phone_android`, but it returns combined number and name:
+   // 3. Beauty Hath Strange Power
+   req, err := http.NewRequest("GET", "https://services.radio-canada.ca", nil)
+   if err != nil {
+      return err
+   }
+   req.URL.RawQuery = "device=web"
+   req.URL.Path, err = func() (string, error) {
+      u, err := url.Parse(address)
+      if err != nil {
+         return "", err
+      }
+      return "/ott/catalog/v2/gem/show" + u.Path, nil
+   }()
+   if err != nil {
+      return err
+   }
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   return json.NewDecoder(res.Body).Decode(g)
+}
+
+func (p GemProfile) Media(i *LineupItem) (*MediaService, error) {
+   req, err := http.NewRequest("GET", "https://services.radio-canada.ca", nil)
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Path = "/media/validation/v2"
+   req.URL.RawQuery = url.Values{
+      "appCode": {"gem"},
+      "idMedia": {i.FormattedIdMedia},
+      "manifestType": {manifest_type},
+      "output": {"json"},
+      // you need this one the first request for a video, but can omit after
+      // that. we will just send it every time.
+      "tech": {"hls"},
+   }.Encode()
+   req.Header = http.Header{
+      "X-Claims-Token": {p.ClaimsToken},
+      "X-Forwarded-For": {forwarded_for},
+   }
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   media := new(MediaService)
+   if err := json.NewDecoder(res.Body).Decode(media); err != nil {
+      return nil, err
+   }
+   if media.Message != "" {
+      return nil, errors.New(media.Message)
+   }
+   media.URL = strings.Replace(media.URL, "[manifestType]", manifest_type, 1)
+   return media, nil
+}
+const manifest_type = "desktop"
 
 type LoginToken struct {
    Access_Token string
 }
-
-const manifest_type = "desktop"
 
 func (t *LoginToken) New(username, password string) error {
    address := func() string {
@@ -44,137 +152,9 @@ type LineupItem struct {
 
 const forwarded_for = "99.224.0.0"
 
-type GemProfile struct {
-   ClaimsToken string
-}
-
-func (p GemProfile) Media(item *LineupItem) (*Media, error) {
-   req, err := http.NewRequest("GET", "https://services.radio-canada.ca", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = "/media/validation/v2"
-   req.URL.RawQuery = url.Values{
-      "appCode": {"gem"},
-      "idMedia": {item.FormattedIdMedia},
-      "manifestType": {manifest_type},
-      "output": {"json"},
-      // you need this one the first request for a video, but can omit after
-      // that. we will just send it every time.
-      "tech": {"hls"},
-   }.Encode()
-   req.Header = http.Header{
-      "X-Claims-Token": {p.ClaimsToken},
-      "X-Forwarded-For": {forwarded_for},
-   }
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   m := new(Media)
-   if err := json.NewDecoder(res.Body).Decode(m); err != nil {
-      return nil, err
-   }
-   if m.Message != "" {
-      return nil, errors.New(m.Message)
-   }
-   m.URL = strings.Replace(m.URL, "[manifestType]", manifest_type, 1)
-   return m, nil
-}
-
-type Media struct {
-   Message string
-   URL string
-}
-
 var scope = []string{
    "https://rcmnb2cprod.onmicrosoft.com/84593b65-0ef6-4a72-891c-d351ddd50aab/subscriptions.write",
    "https://rcmnb2cprod.onmicrosoft.com/84593b65-0ef6-4a72-891c-d351ddd50aab/toutv-profiling",
    "openid",
 }
 
-func (t LoginToken) Profile() (*GemProfile, error) {
-   req, err := http.NewRequest("GET", "https://services.radio-canada.ca", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = "/ott/subscription/v2/gem/Subscriber/profile"
-   req.URL.RawQuery = "device=phone_android"
-   req.Header.Set("Authorization", "Bearer " + t.Access_Token)
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   profile := new(GemProfile)
-   if err := json.NewDecoder(res.Body).Decode(profile); err != nil {
-      return nil, err
-   }
-   return profile, nil
-}
-
-////////////////////
-
-func NewCatalogGem(address string) (*CatalogGem, error) {
-   // you can also use `phone_android`, but it returns combined number and name:
-   // 3. Beauty Hath Strange Power
-   req, err := http.NewRequest("GET", "https://services.radio-canada.ca", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.RawQuery = "device=web"
-   req.URL.Path, err = func() (string, error) {
-      p, err := url.Parse(address)
-      if err != nil {
-         return "", err
-      }
-      return "/ott/catalog/v2/gem/show" + p.Path, nil
-   }()
-   if err != nil {
-      return nil, err
-   }
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   gem := new(CatalogGem)
-   if err := json.NewDecoder(res.Body).Decode(gem); err != nil {
-      return nil, err
-   }
-   return gem, nil
-}
-
-func (c CatalogGem) Item() *LineupItem {
-   for _, content := range c.Content {
-      for _, lineup := range content.Lineups {
-         for _, item := range lineup.Items {
-            if item.URL == c.SelectedUrl {
-               return &item
-            }
-         }
-      }
-   }
-   return nil
-}
-
-func (p GemProfile) WriteFile(name string) error {
-   text, err := json.Marshal(p)
-   if err != nil {
-      return err
-   }
-   return os.WriteFile(name, text, 0666)
-}
-
-func ReadProfile(name string) (*GemProfile, error) {
-   text, err := os.ReadFile(name)
-   if err != nil {
-      return nil, err
-   }
-   profile := new(GemProfile)
-   if err := json.Unmarshal(text, profile); err != nil {
-      return nil, err
-   }
-   return profile, nil
-}
