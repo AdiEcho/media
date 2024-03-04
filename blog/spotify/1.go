e0cc5b8ad3d867c57326603471320f8d0136b256
3052 <spen+3052@protonmail.com>
2024-03-03 21:17:52 -0600

    deleted:    blog/spotify/3.go

--- blog/spotify/1.go
+++ blog/spotify/1.go
@@ -1,55 +1,48 @@
 package spotify
 
 import (
-   "154.pages.dev/protobuf"
-   "bytes"
-   "errors"
-   "io"
-   "net/http"
+	"154.pages.dev/protobuf"
+	"bytes"
+	"errors"
+	"io"
+	"net/http"
 )
 
-// const client_id = "9a8d2f0ce77a4e248bb71fefcb557637"
-const client_id = "58bd3c95768941ea9eb4350aaa033eb3"
+const android_client_id = "9a8d2f0ce77a4e248bb71fefcb557637"
 
 func (r *login_response) New(username, password string) error {
-   if username == "" {
-      return errors.New("username")
-   }
-   if password == "" {
-      return errors.New("password")
-   }
-   var m protobuf.Message
-   m.Add(1, func(m *protobuf.Message) {
-      m.AddBytes(1, []byte(client_id))
-   })
-   m.Add(101, func(m *protobuf.Message) {
-      m.AddBytes(1, []byte(username))
-      m.AddBytes(2, []byte(password))
-   })
-   req, err := http.NewRequest(
-      "POST", "https://login5.spotify.com/v3/login", bytes.NewReader(m.Encode()),
-   )
-   if err != nil {
-      return err
-   }
-   req.Header.Set("content-type", "application/x-protobuf")
-   req.Header.Set("user-agent", "Symfony HttpClient (Curl)")
-   req.Header.Set("accept", "*/*")
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      return errors.New(res.Status)
-   }
-   data, err := io.ReadAll(res.Body)
-   if err != nil {
-      return err
-   }
-   return r.m.Consume(data)
+	var m protobuf.Message
+	m.Add(1, func(m *protobuf.Message) {
+		m.AddBytes(1, []byte(android_client_id))
+	})
+	m.Add(101, func(m *protobuf.Message) {
+		m.AddBytes(1, []byte(username))
+		m.AddBytes(2, []byte(password))
+	})
+	req, err := http.NewRequest(
+		"POST", "https://login5.spotify.com/v3/login", bytes.NewReader(m.Encode()),
+	)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("content-type", "application/x-protobuf")
+	req.Header.Set("user-agent", "Symfony HttpClient (Curl)")
+	req.Header.Set("accept", "*/*")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return errors.New(res.Status)
+	}
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	return r.m.Consume(data)
 }
 
 type login_response struct {
-   m protobuf.Message
+	m protobuf.Message
 }
--- blog/spotify/2_test.go
+++ blog/spotify/2_test.go
@@ -8,6 +8,9 @@ import (
 
 func TestChallenge(t *testing.T) {
    username := os.Getenv("spotify_username")
+   if username == "" {
+      t.Fatal("spotify_username")
+   }
    password := os.Getenv("spotify_password")
    var login login_response
    err := login.New(username, password)
@@ -19,4 +22,13 @@ func TestChallenge(t *testing.T) {
       t.Fatal(err)
    }
    fmt.Printf("%#v\n", message)
+}
+
+var _ = protobuf.Message{
+   protobuf.Field{Number: 1, Type: -2, Value: protobuf.Message{
+      protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("tzjngbdihh9uk2wd4w5016f21")},
+      protobuf.Field{Number: 2, Type: 2, Value: protobuf.Bytes("BQDB3nakoC8zFIBMOMHmMNe-QCK3jRl-0R_IT_kYPGI19FnRaTlMqO_MjTL3oBXpt0uqa_byAcKSLxSrjBxCCIFKHmVN5LpYPci6p3nzuclQjhwSHu76LoWK2PhB9i5wRbzFKTDD3T94q766Zsgqq6oMjtntf4qEjGzm-KU4ku9dovqsdJuCvYixFp8rLRnkajLywPUIJo-hXj6EYg-Z0eCbvfSiCDyLqIFVEqUY4wBtS_mZYeyFbRGIvlrTLyby5xG5JGX6NxpuqcR8lZHUvmTlyclCtaVEedsQQlgXGVh2jWLvdLybye_-WeLWDKXf0zKC9E5OyOqTB_seXioN")},
+      protobuf.Field{Number: 3, Type: 2, Value: protobuf.Bytes("AQD2qEd61e2koLBcTZSNSZF8ejR9jO6ofT8BB4BgFa5S0pUt3hcF6QIalzJn4CnJtf-cXQjwjZ47rllCcIfnNrTC2-T_aaWOIRxp6X5_drG_UN1NpcJEzRBNqR1cjWoMJv90Ud7c3UEB7hAU-RYr8j-bFYvHV-j3SBbjx8F67sroo42hkew9HBiHhkRUPvqWNZnU8AdyVaZd6iUMtIVgDVEHOg")},
+      protobuf.Field{Number: 4, Type: 0, Value: protobuf.Varint(3600)},
+   }},
 }
