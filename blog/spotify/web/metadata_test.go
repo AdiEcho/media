553c3869067dcab98c8c90694d263473a88378b3
3052 <spen+3052@protonmail.com>
2024-03-05 08:26:21 -0600

    new file:   blog/spotify/web/metadata.json

--- blog/spotify/web/metadata_test.go
+++ blog/spotify/web/metadata_test.go
@@ -1,20 +1,19 @@
 package web
 
 import (
-   "bytes"
-   "fmt"
+   "154.pages.dev/media/blog/spotify/android"
    "os"
    "testing"
 )
 
-req.URL.Path = "/metadata/4/track/2da9a11032664413b24de181c534f157"
+const track = "2da9a11032664413b24de181c534f157"
 
 func TestMetadata(t *testing.T) {
    home, err := os.UserHomeDir()
    if err != nil {
       t.Fatal(err)
    }
-   var login LoginOk
+   var login android.LoginOk
    login.Data, err = os.ReadFile(home + "/spotify.bin")
    if err != nil {
       t.Fatal(err)
@@ -22,12 +21,10 @@ func TestMetadata(t *testing.T) {
    if err := login.Consume(); err != nil {
       t.Fatal(err)
    }
-   res, err := login.metadata()
+   res, err := metadata(login, track)
    if err != nil {
       t.Fatal(err)
    }
    defer res.Body.Close()
-   var b bytes.Buffer
-   res.Write(&b)
-   fmt.Printf("%q\n", b.Bytes())
+   res.Write(os.Stdout)
 }
