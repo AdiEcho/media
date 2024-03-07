a8ccbebccefe98b3f16bce0a4bd2766556876981
3052 <spen+3052@protonmail.com>
2024-03-06 22:16:18 -0600

    new file:   blog/spotify/android/license.go

--- blog/spotify/web/storage.go
+++ blog/spotify/web/storage.go
@@ -1,22 +1,38 @@
 package web
 
 import (
+   "154.pages.dev/media/blog/spotify/android"
+   "encoding/json"
+   "errors"
    "net/http"
-   "net/url"
+   "strings"
 )
 
-func storage(file_id string) (*http.Response, error) {
-   var req http.Request
-   req.Header = make(http.Header)
-   req.ProtoMajor = 1
-   req.ProtoMinor = 1
-   req.URL = new(url.URL)
-   req.URL.Host = "guc3-spclient.spotify.com"
+func (s *storage_resolve) New(login android.LoginOk, file_id string) error {
+   token, ok := login.AccessToken()
+   if !ok {
+      return errors.New("android.LoginOk.AccessToken")
+   }
+   req, err := http.NewRequest("GET", "https://guc3-spclient.spotify.com", nil)
+   if err != nil {
+      return err
+   }
    req.URL.Path = "/storage-resolve/v2/files/audio/interactive/10/" + file_id
-   req.URL.Scheme = "https"
-   req.Header["Authorization"] = []string{"Bearer BQA9WA-ZkKdTitbqkrs9XWYCkfJwDsCR80eW1LIVD6vEnua0V2g60hLaWb1d-ycakaRskAEboHQ2kS4xsh00BoGk9P-t4Ji_EBwiiWzl2Q18_WAl-USydjKNQqQNC1jd87m2ZvmmbDjJbJWZ05HnnTRQVgzmCUJyHXeS4Slk1yx0K_p-LoWnRw4Stvio0xHHLYJTU_4wxsNmpjdg5B-hnYGRckhJh566YLS4Zc1uBWHWUgc8cRIHNeeILIlm9cE84CcZ37ZhLhLSHiO9GKu69H_3LOkufVUMt5-omiiDlrF1xr-u_Wpv5mDuM7MeI16Gpnxr8vWDM-cAhxEvXDgZ"}
-   val := make(url.Values)
-   val["alt"] = []string{"json"}
-   req.URL.RawQuery = val.Encode()
-   return http.DefaultClient.Do(&req)
+   req.Header.Set("authorization", "Bearer " + token)
+   req.URL.RawQuery = "alt=json"
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      var b strings.Builder
+      res.Write(&b)
+      return errors.New(b.String())
+   }
+   return json.NewDecoder(res.Body).Decode(s)
+}
+
+type storage_resolve struct {
+   CDNURL []string
 }
--- blog/spotify/web/storage_test.go
+++ blog/spotify/web/storage_test.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+   "154.pages.dev/media/blog/spotify/android"
+   "fmt"
    "os"
    "testing"
 )
@@ -8,10 +10,23 @@ import (
 const file_id = "392482fe9bed7372d1657d7e22f32b792902f3bd"
 
 func TestStorage(t *testing.T) {
-   res, err := storage(file_id)
+   home, err := os.UserHomeDir()
    if err != nil {
       t.Fatal(err)
    }
-   defer res.Body.Close()
-   res.Write(os.Stdout)
+   var login android.LoginOk
+   login.Data, err = os.ReadFile(home + "/spotify.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   if err := login.Consume(); err != nil {
+      t.Fatal(err)
+   }
+   var storage storage_resolve
+   if err := storage.New(login, file_id); err != nil {
+      t.Fatal(err)
+   }
+   for _, url := range storage.CDNURL {
+      fmt.Println(url)
+   }
 }
