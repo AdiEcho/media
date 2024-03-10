bbfef410f0f50064f5cfd81bb619bd6d8dd6f662
3052 <spen+3052@protonmail.com>
2024-03-10 18:47:28 -0500

    new file:   blog/spotify/web/seektable.go

---
+++ blog/spotify/web/seektable.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+   "net/http"
+   "net/url"
+   "os"
+)
+
+func main() {
+   var req http.Request
+   req.Header = make(http.Header)
+   req.ProtoMajor = 1
+   req.ProtoMinor = 1
+   req.URL = new(url.URL)
+   req.URL.Host = "seektables.scdn.co"
+   req.URL.Path = "/seektable/392482fe9bed7372d1657d7e22f32b792902f3bd.json"
+   req.URL.Scheme = "https"
+   res, err := http.DefaultClient.Do(&req)
+   if err != nil {
+      panic(err)
+   }
+   defer res.Body.Close()
+   res.Write(os.Stdout)
+}
