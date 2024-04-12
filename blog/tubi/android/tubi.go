88cfd6383a825bc88ef0bf993206b61a58aa378e
3052 <spen+3052@protonmail.com>
2024-04-12 17:43:23 -0500

    +func (content_management) Show() string {

--- blog/tubi/android/tubi.go
+++ blog/tubi/android/tubi.go
@@ -7,12 +7,37 @@ import (
    "strconv"
 )
 
+func (content_management) Show() string {
+   return ""
+}
+
+func (content_management) Season() int {
+   return 0
+}
+
+func (content_management) Episode() int {
+   return 0
+}
+
+func (content_management) Title() string {
+   return ""
+}
+
+func (content_management) Year() int {
+   return 0
+}
+
 type content_management struct {
-   Video_Resources []struct {
-      Manifest struct {
-         URL string
+   Video_Resources []VideoResource
+}
+
+func (c content_management) Resolution720p() (*VideoResource, bool) {
+   for _, video := range c.Video_Resources {
+      if video.Resolution == "VIDEO_RESOLUTION_720P" {
+         return &video, true
       }
    }
+   return nil, false
 }
 
 func (c *content_management) New(content_id int) error {
@@ -32,4 +57,30 @@ func (c *content_management) New(content_id int) error {
    }
    defer res.Body.Close()
    return json.NewDecoder(res.Body).Decode(c)
+}
+
+func (v VideoResource) RequestUrl() (string, bool) {
+   return v.License_Server.URL, true
+}
+
+type VideoResource struct {
+   License_Server struct {
+      URL string
+   }
+   Manifest struct {
+      URL string
+   }
+   Resolution string
+}
+
+func (VideoResource) RequestHeader() (http.Header, error) {
+   return http.Header{}, nil
+}
+
+func (VideoResource) RequestBody(b []byte) ([]byte, error) {
+   return b, nil
+}
+
+func (VideoResource) ResponseBody(b []byte) ([]byte, error) {
+   return b, nil
 }
--- blog/tubi/android/tubi_test.go
+++ blog/tubi/android/tubi_test.go
@@ -1,18 +1,62 @@
 package tubi
 
 import (
+   "154.pages.dev/widevine"
+   "encoding/hex"
    "fmt"
+   "os"
    "testing"
 )
 
+// tubitv.com/tv-shows/200042567/s01-e03-hell-hath-no-fury
+
 // tubitv.com/movies/589292
-const content_id = 589292
+const (
+   content_id = 589292
+   default_kid = "943974887f2a4b87a3ded9e99f03c962"
+)
 
 func TestContent(t *testing.T) {
    var content content_management
-   err := content.New(content_id)
-   if err != nil {
+   if err := content.New(content_id); err != nil {
       t.Fatal(err)
    }
    fmt.Printf("%+v\n", content)
+}
+
+func TestLicense(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   key_id, err := hex.DecodeString(default_kid)
+   if err != nil {
+      t.Fatal(err)
+   }
+   var module widevine.CDM
+   if err := module.New(private_key, client_id, key_id); err != nil {
+      t.Fatal(err)
+   }
+   var content content_management
+   if err := content.New(content_id); err != nil {
+      t.Fatal(err)
+   }
+   video, ok := content.Resolution720p()
+   if !ok {
+      t.Fatal("Resolution720p")
+   }
+   license, err := module.License(video)
+   if err != nil {
+      t.Fatal(err)
+   }
+   key, ok := module.Key(license)
+   fmt.Printf("%x %v\n", key, ok)
 }
