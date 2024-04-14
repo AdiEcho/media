ec3c56f587907ab77a3a5d2b587fbca303bb53e5
3052 <spen+3052@protonmail.com>
2024-04-14 12:16:40 -0500

    +   "the-mask": {

--- tubi/video.go
+++ tubi/video.go
@@ -1,53 +1,53 @@
 package tubi
 
 import (
-	"net/http"
-	"slices"
-	"strconv"
-	"strings"
+   "net/http"
+   "slices"
+   "strconv"
+   "strings"
 )
 
 func (c Content) Video() VideoResource {
-	slices.SortFunc(c.Video_Resources, func(a, b VideoResource) int {
-		return int(b.Resolution - a.Resolution)
-	})
-	return c.Video_Resources[0]
+   slices.SortFunc(c.Video_Resources, func(a, b VideoResource) int {
+      return int(b.Resolution - a.Resolution)
+   })
+   return c.Video_Resources[0]
 }
 
 type Resolution int
 
 func (r *Resolution) UnmarshalText(text []byte) error {
-	a := strings.TrimPrefix(string(text), "VIDEO_RESOLUTION_")
-	i, err := strconv.Atoi(strings.TrimSuffix(a, "P"))
-	if err != nil {
-		return err
-	}
-	*r = Resolution(i)
-	return nil
+   a := strings.TrimPrefix(string(text), "VIDEO_RESOLUTION_")
+   i, err := strconv.Atoi(strings.TrimSuffix(a, "P"))
+   if err != nil {
+      return err
+   }
+   *r = Resolution(i)
+   return nil
 }
 
 type VideoResource struct {
-	License_Server struct {
-		URL string
-	}
-	Manifest struct {
-		URL string
-	}
-	Resolution Resolution
+   License_Server struct {
+      URL string
+   }
+   Manifest struct {
+      URL string
+   }
+   Resolution Resolution
 }
 
 func (VideoResource) RequestBody(b []byte) ([]byte, error) {
-	return b, nil
+   return b, nil
 }
 
 func (VideoResource) RequestHeader() (http.Header, error) {
-	return http.Header{}, nil
+   return http.Header{}, nil
 }
 
 func (v VideoResource) RequestUrl() (string, bool) {
-	return v.License_Server.URL, true
+   return v.License_Server.URL, true
 }
 
 func (VideoResource) ResponseBody(b []byte) ([]byte, error) {
-	return b, nil
+   return b, nil
 }
--- tubi/video_test.go
+++ tubi/video_test.go
@@ -1,12 +1,12 @@
 package tubi
 
 import (
-	"154.pages.dev/widevine"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"os"
-	"testing"
+   "154.pages.dev/widevine"
+   "encoding/hex"
+   "encoding/json"
+   "fmt"
+   "os"
+   "testing"
 )
 
 const resolution_data = `
@@ -20,46 +20,46 @@ const resolution_data = `
 `
 
 func TestResolution(t *testing.T) {
-	var v struct {
-		Resolution Resolution
-	}
-	err := json.Unmarshal([]byte(resolution_data), &v)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("%+v\n", v)
+   var v struct {
+      Resolution Resolution
+   }
+   err := json.Unmarshal([]byte(resolution_data), &v)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", v)
 }
 
 func TestLicense(t *testing.T) {
-	test := tests["movie"]
-	home, err := os.UserHomeDir()
-	if err != nil {
-		t.Fatal(err)
-	}
-	private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
-	if err != nil {
-		t.Fatal(err)
-	}
-	client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
-	if err != nil {
-		t.Fatal(err)
-	}
-	key_id, err := hex.DecodeString(test.key_id)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var module widevine.CDM
-	if err := module.New(private_key, client_id, key_id); err != nil {
-		t.Fatal(err)
-	}
-	var cms Content
-	if err := cms.New(test.content_id); err != nil {
-		t.Fatal(err)
-	}
-	license, err := module.License(cms.Video())
-	if err != nil {
-		t.Fatal(err)
-	}
-	key, ok := module.Key(license)
-	fmt.Printf("%x %v\n", key, ok)
+   test := tests["the-mask"]
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   key_id, err := hex.DecodeString(test.key_id)
+   if err != nil {
+      t.Fatal(err)
+   }
+   var module widevine.CDM
+   if err := module.New(private_key, client_id, key_id); err != nil {
+      t.Fatal(err)
+   }
+   var cms Content
+   if err := cms.New(test.content_id); err != nil {
+      t.Fatal(err)
+   }
+   license, err := module.License(cms.Video())
+   if err != nil {
+      t.Fatal(err)
+   }
+   key, ok := module.Key(license)
+   fmt.Printf("%x %v\n", key, ok)
 }
