4d31fa4ddebf001d681cc8c0c7ce340067fe8f53
3052 <spen+3052@protonmail.com>
2024-04-19 08:23:14 -0500

    +func (o on_demand) clips() (episode_clips, error) {

--- blog/pluto/clips.go
+++ blog/pluto/clips.go
@@ -1,11 +1,12 @@
 package pluto
 
 import (
+   "encoding/json"
    "net/http"
    "strings"
 )
 
-func (o on_demand) clips() (*http.Response, error) {
+func (o on_demand) clips() (episode_clips, error) {
    req, err := http.NewRequest("GET", "http://api.pluto.tv", nil)
    if err != nil {
       return nil, err
@@ -17,7 +18,17 @@ func (o on_demand) clips() (*http.Response, error) {
       b.WriteString("/clips.json")
       return b.String()
    }()
-   return http.DefaultClient.Do(req)
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   var clips episode_clips
+   err = json.NewDecoder(res.Body).Decode(&clips)
+   if err != nil {
+      return nil, err
+   }
+   return clips, nil
 }
 
 func (b boot_start) video() (*on_demand, bool) {
@@ -25,4 +36,19 @@ func (b boot_start) video() (*on_demand, bool) {
       return &video, true
    }
    return nil, false
+}
+
+func (e episode_clips) clip() (*episode_clip, bool) {
+   for _, clip := range e {
+      return &clip, true
+   }
+   return nil, false
+}
+
+type episode_clips []episode_clip
+
+type episode_clip struct {
+   Sources []struct {
+      File string
+   }
 }
