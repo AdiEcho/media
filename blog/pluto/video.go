bdf194675ea5a8c3e2813457f205ccb2c4fdfcd3
3052 <spen+3052@protonmail.com>
2024-04-20 15:57:58 -0500

    +func (w web_address) String() string {

--- blog/pluto/video.go
+++ blog/pluto/video.go
@@ -5,14 +5,48 @@ import (
    "fmt"
    "net/http"
    "net/url"
+   "strings"
 )
 
-type on_demand struct {
-   ID string
-   Slug string
+func (w web_address) String() string {
+   var b strings.Builder
+   b.WriteString("https://pluto.tv/on-demand/")
+   if w.episode != "" {
+      b.WriteString("series")
+   } else {
+      b.WriteString("movies")
+   }
+   b.WriteByte('/')
+   b.WriteString(w.series)
+   if w.episode != "" {
+      b.WriteString("/episode/")
+      b.WriteString(w.episode)
+   }
+   return b.String()
 }
 
-func new_video(id string) (*on_demand, error) {
+func (w *web_address) Set(s string) error {
+   for {
+      var (
+         key string
+         ok bool
+      )
+      key, s, ok = strings.Cut(s, "/")
+      if !ok {
+         return nil
+      }
+      switch key {
+      case "episode":
+         w.episode = s
+      case "movies":
+         w.series = s
+      case "series":
+         w.series, s, _ = strings.Cut(s, "/")
+      }
+   }
+}
+
+func (w web_address) video() (*video, error) {
    req, err := http.NewRequest("GET", "https://boot.pluto.tv/v4/start", nil)
    if err != nil {
       return nil, err
@@ -23,25 +57,56 @@ func new_video(id string) (*on_demand, error) {
       "clientID": {"9"},
       "clientModelNumber": {"9"},
       "drmCapabilities": {"widevine:L3"},
-      "seriesIDs": {id},
+      "seriesIDs": {w.series},
    }.Encode()
    res, err := http.DefaultClient.Do(req)
    if err != nil {
       return nil, err
    }
    defer res.Body.Close()
-   var s struct {
-      VOD []on_demand
+   var start struct {
+      VOD []video
    }
-   err = json.NewDecoder(res.Body).Decode(&s)
+   err = json.NewDecoder(res.Body).Decode(&start)
    if err != nil {
       return nil, err
    }
-   video := s.VOD[0]
-   if video.ID != id {
-      if video.Slug != id {
-         return nil, fmt.Errorf("%+v", video)
+   demand := start.VOD[0]
+   if demand.Slug != w.series {
+      if demand.ID != w.series {
+         return nil, fmt.Errorf("%+v", demand)
       }
    }
-   return &video, nil
+   for _, s := range demand.Seasons {
+      s.parent = &demand
+      for _, e := range s.Episodes {
+         e.parent = s
+         if e.Episode == w.episode {
+            return e, nil
+         }
+         if e.Slug == w.episode {
+            return e, nil
+         }
+      }
+   }
+   return &demand, nil
+}
+
+type web_address struct {
+   series string
+   episode string
+}
+
+type video struct {
+   Episode string `json:"_id"`
+   ID string
+   Name string
+   Seasons []*season
+   Slug string
+   parent *season
+}
+
+type season struct {
+   Episodes []*video
+   parent *video
 }
