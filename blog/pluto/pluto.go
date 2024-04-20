bd4b320db35b30a60b335d261a42bd9ccf1ae915
3052 <spen+3052@protonmail.com>
2024-04-19 22:48:18 -0500

    deleted:    blog/pluto/video.go

--- blog/pluto/pluto.go
+++ blog/pluto/pluto.go
@@ -3,10 +3,55 @@ package pluto
 import (
    "encoding/json"
    "errors"
+   "fmt"
    "net/http"
    "net/url"
    "strings"
 )
+
+var forwards = map[string]string{"Canada": "99.224.0.0"}
+
+type on_demand struct {
+   ID string
+   Slug string
+}
+
+func new_video(slug, forward string) (*on_demand, error) {
+   req, err := http.NewRequest("GET", "https://boot.pluto.tv/v4/start", nil)
+   if err != nil {
+      return nil, err
+   }
+   if forward != "" {
+      req.Header.Set("x-forwarded-for", forward)
+   }
+   req.URL.RawQuery = url.Values{
+      "appName": {"web"},
+      "appVersion": {"9"},
+      "clientID": {"9"},
+      "clientModelNumber": {"9"},
+      "drmCapabilities": {"widevine:L3"},
+      "episodeSlugs": {slug},
+   }.Encode()
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   var s struct {
+      VOD []on_demand
+   }
+   err = json.NewDecoder(res.Body).Decode(&s)
+   if err != nil {
+      return nil, err
+   }
+   video := s.VOD[0]
+   if video.ID != slug {
+      if video.Slug != slug {
+         return nil, fmt.Errorf("%+v", video)
+      }
+   }
+   return &video, nil
+}
 
 var bases = []url.URL{
    // these return `403 OK` with compressed content
@@ -31,34 +76,6 @@ func (s source) parse(base url.URL) (*url.URL, error) {
 type source struct {
    File string
    Type string
-}
-
-func new_clip(id string) (*episode_clip, error) {
-   req, err := http.NewRequest("GET", "http://api.pluto.tv", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = func() string {
-      var b strings.Builder
-      b.WriteString("/v2/episodes/")
-      b.WriteString(id)
-      b.WriteString("/clips.json")
-      return b.String()
-   }()
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      return nil, errors.New(res.Status)
-   }
-   var clips []episode_clip
-   err = json.NewDecoder(res.Body).Decode(&clips)
-   if err != nil {
-      return nil, err
-   }
-   return &clips[0], nil
 }
 
 type poster struct{}
@@ -90,4 +107,34 @@ func (e episode_clip) dash() (*source, bool) {
       }
    }
    return nil, false
+}
+
+////////
+
+func new_clip(id string) (*episode_clip, error) {
+   req, err := http.NewRequest("GET", "http://api.pluto.tv", nil)
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Path = func() string {
+      var b strings.Builder
+      b.WriteString("/v2/episodes/")
+      b.WriteString(id)
+      b.WriteString("/clips.json")
+      return b.String()
+   }()
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      return nil, errors.New(res.Status)
+   }
+   var clips []episode_clip
+   err = json.NewDecoder(res.Body).Decode(&clips)
+   if err != nil {
+      return nil, err
+   }
+   return &clips[0], nil
 }
--- blog/pluto/pluto_test.go
+++ blog/pluto/pluto_test.go
@@ -9,6 +9,41 @@ import (
    "time"
 )
 
+var video_tests = []struct{
+   forward string
+   slug string
+   url string
+}{
+   {
+      url: "pluto.tv/on-demand/movies/63c8215d8ba71d0013f29b43",
+      slug: "63c8215d8ba71d0013f29b43",
+      forward: "Canada",
+   },
+   {
+      url: "pluto.tv/on-demand/movies/ex-machina-2015-1-1-ptv1",
+      slug: "ex-machina-2015-1-1-ptv1",
+      forward: "Canada",
+   },
+   {
+      url: "pluto.tv/on-demand/series/65ce5c60a3a8580013c4b64a/episode/65ce5c7ca3a8580013c4be02",
+      slug: "65ce5c7ca3a8580013c4be02",
+   },
+   {
+      url: "pluto.tv/on-demand/series/king-of-queens/episode/head-first-1998-1-2",
+      slug: "head-first-1998-1-2",
+   },
+}
+
+func TestVideo(t *testing.T) {
+   for _, test := range video_tests {
+      video, err := new_video(test.slug, forwards[test.forward])
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%+v\n", video)
+      time.Sleep(time.Second)
+   }
+}
 var addresses = []string{
    // pluto.tv/on-demand/series/king-of-queens/episode/head-first-1998-1-2
    // pluto.tv/on-demand/series/65ce5c60a3a8580013c4b64a/episode/65ce5c7ca3a8580013c4be02
