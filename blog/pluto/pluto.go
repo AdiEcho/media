2aa630009cb96be712f3f118a667abba80eb0043
3052 <spen+3052@protonmail.com>
2024-04-19 23:15:11 -0500

    new file:   blog/pluto/video.go

--- blog/pluto/pluto.go
+++ blog/pluto/pluto.go
@@ -3,55 +3,10 @@ package pluto
 import (
    "encoding/json"
    "errors"
-   "fmt"
    "net/http"
    "net/url"
    "strings"
 )
-
-var forwards = map[string]string{"Canada": "99.224.0.0"}
-
-type on_demand struct {
-   ID string
-   Slug string
-}
-
-func new_video(slug, forward string) (*on_demand, error) {
-   req, err := http.NewRequest("GET", "https://boot.pluto.tv/v4/start", nil)
-   if err != nil {
-      return nil, err
-   }
-   if forward != "" {
-      req.Header.Set("x-forwarded-for", forward)
-   }
-   req.URL.RawQuery = url.Values{
-      "appName": {"web"},
-      "appVersion": {"9"},
-      "clientID": {"9"},
-      "clientModelNumber": {"9"},
-      "drmCapabilities": {"widevine:L3"},
-      "episodeSlugs": {slug},
-   }.Encode()
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   var s struct {
-      VOD []on_demand
-   }
-   err = json.NewDecoder(res.Body).Decode(&s)
-   if err != nil {
-      return nil, err
-   }
-   video := s.VOD[0]
-   if video.ID != slug {
-      if video.Slug != slug {
-         return nil, fmt.Errorf("%+v", video)
-      }
-   }
-   return &video, nil
-}
 
 var bases = []url.URL{
    // these return `403 OK` with compressed content
@@ -108,8 +63,6 @@ func (e episode_clip) dash() (*source, bool) {
    }
    return nil, false
 }
-
-////////
 
 func new_clip(id string) (*episode_clip, error) {
    req, err := http.NewRequest("GET", "http://api.pluto.tv", nil)
