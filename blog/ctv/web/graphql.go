7fae8d617cadc0427a4797b32470c935334ecb25
3052 <spen+3052@protonmail.com>
2024-04-27 10:49:30 -0500

    +      "operationName": "resolvePath",

--- blog/ctv/web/graphql.go
+++ blog/ctv/web/graphql.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+   "bytes"
+   "encoding/json"
+   "io"
    "net/http"
    "net/url"
    "os"
@@ -16,44 +19,27 @@ func main() {
    req.URL = new(url.URL)
    req.URL.Host = "www.ctv.ca"
    req.URL.Path = "/space-graphql/apq/graphql"
-   val := make(url.Values)
-   val["operationName"] = []string{"resolvePath"}
-   val["extensions"] = []string{`
-   {
-      "persistedQuery": {
-         "version": 1,
-         "sha256Hash": "26d314b59ba2708d261067964353f9a92f1c2689f50d1254fa4d03ddb9b9092a"
-      }
+   req.Method = "POST"
+   body := map[string]any{
+      "operationName": "resolvePath",
+      "variables": map[string]any{
+         "page":0,
+         "path":"/movies/the-girl-with-the-dragon-tattoo-2011",
+         "subscriptions":[]string{
+            "CTV","CTV_DRAMA","CTV_COMEDY","CTV_LIFE","CTV_SCIFI","CTV_THROWBACK","CTV_MOVIES","CTV_MTV","CTV_MUCH","DISCOVERY","DISCOVERY_SCIENCE","DISCOVERY_VELOCITY","INVESTIGATION_DISCOVERY","ANIMAL_PLANET","E_NOW",
+         },
+         "maturity":"ADULT",
+         "language":"ENGLISH",
+         "authenticationState":"UNAUTH",
+         "playbackLanguage":"ENGLISH",
+      },
+      "query": hello,
    }
-   `}
-   val["variables"] = []string{`
-   {
-     "page": 0,
-     "path": "/movies/the-girl-with-the-dragon-tattoo-2011/the-girl-with-the-dragon-tattoo-2011",
-     "subscriptions": [
-       "CTV",
-       "CTV_DRAMA",
-       "CTV_COMEDY",
-       "CTV_LIFE",
-       "CTV_SCIFI",
-       "CTV_THROWBACK",
-       "CTV_MOVIES",
-       "CTV_MTV",
-       "CTV_MUCH",
-       "DISCOVERY",
-       "DISCOVERY_SCIENCE",
-       "DISCOVERY_VELOCITY",
-       "INVESTIGATION_DISCOVERY",
-       "ANIMAL_PLANET",
-       "E_NOW"
-     ],
-     "maturity": "ADULT",
-     "language": "ENGLISH",
-     "authenticationState": "UNAUTH",
-     "playbackLanguage": "ENGLISH"
+   text, err := json.Marshal(body)
+   if err != nil {
+      panic(err)
    }
-   `}
-   req.URL.RawQuery = val.Encode()
+   req.Body = io.NopCloser(bytes.NewReader(text))
    req.URL.Scheme = "https"
    res, err := http.DefaultClient.Do(&req)
    if err != nil {
@@ -62,3 +48,133 @@ func main() {
    defer res.Body.Close()
    res.Write(os.Stdout)
 }
+
+const hello = `
+  query resolvePath(
+    $path: String!
+    $subscriptions: [Subscription]!
+    $maturity: Maturity!
+    $language: Language!
+    $authenticationState: AuthenticationState!
+    $playbackLanguage: PlaybackLanguage!
+    $page: Int = 0
+  )
+  @uaContext(
+    subscriptions: $subscriptions
+    maturity: $maturity
+    language: $language
+    authenticationState: $authenticationState
+    playbackLanguage: $playbackLanguage
+  ) {
+    resolvedPath(path: $path) {
+      redirected
+      path
+      segments {
+        position
+        content {
+          title
+          id
+          path
+        }
+      }
+      lastSegment {
+        position
+        content {
+          id
+          title
+          path
+          __typename
+          ... on AceWebContent {
+            path
+          }
+          ... on AxisObject {
+            __typename
+            description
+            axisId
+            ... on AxisContent {
+              keywords
+              seasonNumber
+              episodeNumber
+              contentType
+            }
+            ... on AxisMedia {
+              keywords
+              firstPlayableContent {
+                id
+                axisId
+                badges {
+                  title
+                  label
+                }
+              }
+            }
+          }
+          ... on Section {
+            containerType
+            secondNavigation {
+              title
+              renderTitleAs
+              titleImage {
+                __typename
+                id
+                url
+              }
+            }
+          }
+        }
+      }
+      searchResults {
+        ... on Medias {
+          page(page: $page) {
+            totalItemCount
+            totalPageCount
+            hasNextPage
+            items {
+              id
+              title
+              summary
+              agvotCode
+              qfrCode
+              axisId
+              path
+              posterImages: images(formats: POSTER) {
+                url
+              }
+              squareImages: images(formats: SQUARE) {
+                url
+              }
+              thumbnailImages: images(formats: THUMBNAIL) {
+                url
+              }
+              badges {
+                title
+                label
+              }
+              genres {
+                name
+              }
+              firstAirYear
+              originatingNetworkLogoId
+              heroBrandLogoId
+              seasons {
+                id
+              }
+            }
+          }
+        }
+        ... on Articles {
+          page(page: $page) {
+            totalItemCount
+            totalPageCount
+            hasNextPage
+            items {
+              id
+              title
+              path
+            }
+          }
+        }
+      }
+    }
+  }
+`
