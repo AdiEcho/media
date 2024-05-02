faa498e95d6c9f2ce5d08cd37e765e4620eb050f
3052 <spen+3052@protonmail.com>
2024-05-02 17:29:24 -0500

    deleted:    roku/json.go

--- roku/roku.go
+++ roku/roku.go
@@ -3,7 +3,6 @@ package roku
 import (
    "bytes"
    "encoding/json"
-   "errors"
    "net/http"
 )
 
@@ -13,15 +12,6 @@ type Playback struct {
          LicenseServer string
       }
    }
-}
-
-func (c CrossSite) csrf() (*http.Cookie, bool) {
-   for _, cookie := range c.cookies {
-      if cookie.Name == "_csrf" {
-         return cookie, true
-      }
-   }
-   return nil, false
 }
 
 func (Playback) RequestHeader() (http.Header, error) {
@@ -38,4 +28,69 @@ func (Playback) WrapRequest(b []byte) ([]byte, error) {
 
 func (Playback) UnwrapResponse(b []byte) ([]byte, error) {
    return b, nil
+}
+
+func (a account_token) playback(roku_id string) (*playback, error) {
+   body, err := func() ([]byte, error) {
+      m := map[string]string{
+         "mediaFormat": "DASH",
+         "rokuId": roku_id,
+      }
+      return json.Marshal(m)
+   }()
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://googletv.web.roku.com/api/v3/playback",
+      bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header = http.Header{
+      "Content-Type": {"application/json"},
+      "User-Agent": {user_agent},
+      "X-Roku-Content-Token": {a.AuthToken},
+   }
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   play := new(playback)
+   err = json.NewDecoder(res.Body).Decode(play)
+   if err != nil {
+      return nil, err
+   }
+   return play, nil
+}
+
+type playback struct {
+   DRM struct {
+      Widevine struct {
+         LicenseServer string
+      }
+   }
+}
+const user_agent = "trc-googletv; production; 0"
+
+type account_token struct {
+   AuthToken string
+}
+
+func (a *account_token) New() error {
+   req, err := http.NewRequest(
+      "GET", "https://googletv.web.roku.com/api/v1/account/token", nil,
+   )
+   if err != nil {
+      return err
+   }
+   req.Header.Set("user-agent", user_agent)
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   return json.NewDecoder(res.Body).Decode(a)
 }
