353b15ff3033593fa559abf1dcbe151a8d9a7b7e
3052 <spen+3052@protonmail.com>
2024-05-01 22:29:44 -0500

    +func (m *MediaManifest) Unmarshal(text []byte) error {

--- ctv/ctv.go
+++ ctv/ctv.go
@@ -1,91 +1,91 @@
 package ctv
 
 import (
-   "bytes"
-   "encoding/json"
-   "errors"
-   "io"
-   "net/http"
-   "strconv"
-   "strings"
-   "time"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type Namer struct {
-   M *MediaContent
+	M *MediaContent
 }
 
 func (n Namer) Episode() int {
-   return n.M.Episode
+	return n.M.Episode
 }
 
 func (n Namer) Season() int {
-   return n.M.Season.Number
+	return n.M.Season.Number
 }
 
 func (n Namer) Show() string {
-   if v := n.M.Media; v.Type == "series" {
-      return v.Name
-   }
-   return ""
+	if v := n.M.Media; v.Type == "series" {
+		return v.Name
+	}
+	return ""
 }
 
 func (n Namer) Title() string {
-   if n.M.Media.Type == "movie" {
-      return n.M.Name[:len(n.M.Name)-len(" (9999)")]
-   }
-   return n.M.Name
+	if n.M.Media.Type == "movie" {
+		return n.M.Name[:len(n.M.Name)-len(" (9999)")]
+	}
+	return n.M.Name
 }
 
 func (n Namer) Year() int {
-   return n.M.BroadcastDate.T.Year()
+	return n.M.BroadcastDate.T.Year()
 }
 
 type MediaManifest struct {
-   Content *MediaContent
-   URL     string
+	Content *MediaContent
+	URL     string
 }
 
 func (d date) MarshalText() ([]byte, error) {
-   return d.T.AppendFormat(nil, time.DateOnly), nil
+	return d.T.AppendFormat(nil, time.DateOnly), nil
 }
 
 func (d *date) UnmarshalText(text []byte) error {
-   var err error
-   d.T, err = time.Parse(time.DateOnly, string(text))
-   if err != nil {
-      return err
-   }
-   return nil
+	var err error
+	d.T, err = time.Parse(time.DateOnly, string(text))
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (a AxisContent) Media() (*MediaContent, error) {
-   address := func() string {
-      b := []byte("https://capi.9c9media.com/destinations/")
-      b = append(b, a.AxisPlaybackLanguages[0].DestinationCode...)
-      b = append(b, "/platforms/desktop/contents/"...)
-      b = strconv.AppendInt(b, a.AxisId, 10)
-      b = append(b, "?$include=[ContentPackages,Media,Season]"...)
-      return string(b)
-   }()
-   res, err := http.Get(address)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   media := new(MediaContent)
-   err = json.NewDecoder(res.Body).Decode(media)
-   if err != nil {
-      return nil, err
-   }
-   return media, nil
+	address := func() string {
+		b := []byte("https://capi.9c9media.com/destinations/")
+		b = append(b, a.AxisPlaybackLanguages[0].DestinationCode...)
+		b = append(b, "/platforms/desktop/contents/"...)
+		b = strconv.AppendInt(b, a.AxisId, 10)
+		b = append(b, "?$include=[ContentPackages,Media,Season]"...)
+		return string(b)
+	}()
+	res, err := http.Get(address)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	media := new(MediaContent)
+	err = json.NewDecoder(res.Body).Decode(media)
+	if err != nil {
+		return nil, err
+	}
+	return media, nil
 }
 
 type AxisContent struct {
-   AxisId                int64
-   AxisPlaybackLanguages []struct {
-      DestinationCode string
-   }
+	AxisId                int64
+	AxisPlaybackLanguages []struct {
+		DestinationCode string
+	}
 }
 
 const query_axis = `
@@ -102,119 +102,119 @@ query axisContent($id: ID!) {
 `
 
 func (r ResolvePath) Axis() (*AxisContent, error) {
-   body, err := func() ([]byte, error) {
-      var s struct {
-         OperationName string `json:"operationName"`
-         Query         string `json:"query"`
-         Variables     struct {
-            ID string `json:"id"`
-         } `json:"variables"`
-      }
-      s.OperationName = "axisContent"
-      s.Query = graphql_compact(query_axis)
-      s.Variables.ID = r.id()
-      return json.MarshalIndent(s, "", " ")
-   }()
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://www.ctv.ca/space-graphql/apq/graphql",
-      bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   // you need this for the first request, then can omit
-   req.Header.Set("graphql-client-platform", "entpay_web")
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   var s struct {
-      Data struct {
-         AxisContent AxisContent
-      }
-   }
-   err = json.NewDecoder(res.Body).Decode(&s)
-   if err != nil {
-      return nil, err
-   }
-   return &s.Data.AxisContent, nil
+	body, err := func() ([]byte, error) {
+		var s struct {
+			OperationName string `json:"operationName"`
+			Query         string `json:"query"`
+			Variables     struct {
+				ID string `json:"id"`
+			} `json:"variables"`
+		}
+		s.OperationName = "axisContent"
+		s.Query = graphql_compact(query_axis)
+		s.Variables.ID = r.id()
+		return json.MarshalIndent(s, "", " ")
+	}()
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(
+		"POST", "https://www.ctv.ca/space-graphql/apq/graphql",
+		bytes.NewReader(body),
+	)
+	if err != nil {
+		return nil, err
+	}
+	// you need this for the first request, then can omit
+	req.Header.Set("graphql-client-platform", "entpay_web")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var s struct {
+		Data struct {
+			AxisContent AxisContent
+		}
+	}
+	err = json.NewDecoder(res.Body).Decode(&s)
+	if err != nil {
+		return nil, err
+	}
+	return &s.Data.AxisContent, nil
 }
 
 type date struct {
-   T time.Time
+	T time.Time
 }
 
 type MediaContent struct {
-   BroadcastDate   date
-   ContentPackages []struct {
-      ID int64
-   }
-   Episode int
-   Media   struct {
-      Name string
-      Type string
-   }
-   Name   string
-   Season struct {
-      Number int
-   }
+	BroadcastDate   date
+	ContentPackages []struct {
+		ID int64
+	}
+	Episode int
+	Media   struct {
+		Name string
+		Type string
+	}
+	Name   string
+	Season struct {
+		Number int
+	}
 }
 
 func (m MediaManifest) marshal() ([]byte, error) {
-   return json.MarshalIndent(m, "", " ")
+	return json.MarshalIndent(m, "", " ")
 }
 
-func (m *MediaManifest) unmarshal(text []byte) error {
-   return json.Unmarshal(text, m)
+func (m *MediaManifest) Unmarshal(text []byte) error {
+	return json.Unmarshal(text, m)
 }
 
 // wikipedia.org/wiki/Geo-blocking
 func (a AxisContent) Manifest(m *MediaContent) (*MediaManifest, error) {
-   address := func() string {
-      b := []byte("https://capi.9c9media.com/destinations/")
-      b = append(b, a.AxisPlaybackLanguages[0].DestinationCode...)
-      b = append(b, "/platforms/desktop/playback/contents/"...)
-      b = strconv.AppendInt(b, a.AxisId, 10)
-      b = append(b, "/contentPackages/"...)
-      b = strconv.AppendInt(b, m.ContentPackages[0].ID, 10)
-      b = append(b, "/manifest.mpd?action=reference"...)
-      return string(b)
-   }()
-   res, err := http.Get(address)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      var b strings.Builder
-      res.Write(&b)
-      return nil, errors.New(b.String())
-   }
-   text, err := io.ReadAll(res.Body)
-   if err != nil {
-      return nil, err
-   }
-   return &MediaManifest{m, string(text)}, nil
+	address := func() string {
+		b := []byte("https://capi.9c9media.com/destinations/")
+		b = append(b, a.AxisPlaybackLanguages[0].DestinationCode...)
+		b = append(b, "/platforms/desktop/playback/contents/"...)
+		b = strconv.AppendInt(b, a.AxisId, 10)
+		b = append(b, "/contentPackages/"...)
+		b = strconv.AppendInt(b, m.ContentPackages[0].ID, 10)
+		b = append(b, "/manifest.mpd?action=reference"...)
+		return string(b)
+	}()
+	res, err := http.Get(address)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		var b strings.Builder
+		res.Write(&b)
+		return nil, errors.New(b.String())
+	}
+	text, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return &MediaManifest{m, string(text)}, nil
 }
 
 type Poster struct{}
 
 func (Poster) RequestHeader() (http.Header, error) {
-   return http.Header{}, nil
+	return http.Header{}, nil
 }
 
 func (Poster) RequestUrl() (string, bool) {
-   return "https://license.9c9media.ca/widevine", true
+	return "https://license.9c9media.ca/widevine", true
 }
 
 func (Poster) WrapRequest(b []byte) ([]byte, error) {
-   return b, nil
+	return b, nil
 }
 
 func (Poster) UnwrapResponse(b []byte) ([]byte, error) {
-   return b, nil
+	return b, nil
 }
--- ctv/ctv_test.go
+++ ctv/ctv_test.go
@@ -4,50 +4,21 @@ import (
    "154.pages.dev/encoding"
    "154.pages.dev/widevine"
    "encoding/base64"
+   "encoding/hex"
    "fmt"
    "os"
    "testing"
    "time"
 )
 
-func TestManifest(t *testing.T) {
-   for _, test_path := range test_paths {
-      resolve, err := NewResolve(test_path)
-      if err != nil {
-         t.Fatal(err)
-      }
-      time.Sleep(99 * time.Millisecond)
-      axis, err := resolve.Axis()
-      if err != nil {
-         t.Fatal(err)
-      }
-      time.Sleep(99 * time.Millisecond)
-      media, err := axis.Media()
-      if err != nil {
-         t.Fatal(err)
-      }
-      manifest, err := axis.Manifest(media)
-      if err != nil {
-         t.Fatal(err)
-      }
-      text, err := manifest.marshal()
-      if err != nil {
-         t.Fatal(err)
-      }
-      fmt.Println(string(text))
-      time.Sleep(99 * time.Millisecond)
-   }
-}
-
 // ctv.ca/movies/the-girl-with-the-dragon-tattoo-2011
-const raw_pssh = "CAESEMsJVx7ryz9yhyAmV/a596YaCWJlbGxtZWRpYSISZmYtZDAxM2NhN2EtMjY0MjY1"
+const (
+   raw_key_id = "cb09571eebcb3f7287202657f6b9f7a6"
+   raw_pssh = "CAESEMsJVx7ryz9yhyAmV/a596YaCWJlbGxtZWRpYSISZmYtZDAxM2NhN2EtMjY0MjY1"
+)
 
 func TestLicense(t *testing.T) {
    home, err := os.UserHomeDir()
-   if err != nil {
-      t.Fatal(err)
-   }
-   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
    if err != nil {
       t.Fatal(err)
    }
@@ -55,24 +26,30 @@ func TestLicense(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
    pssh, err := base64.StdEncoding.DecodeString(raw_pssh)
    if err != nil {
       t.Fatal(err)
    }
-   module, err := widevine.PSSH(pssh).CDM(client_id, private_key)
+   var module widevine.CDM
+   err = module.New(private_key, client_id, pssh)
    if err != nil {
       t.Fatal(err)
    }
-   license, err := module.License(Poster{})
+   key_id, err := hex.DecodeString(raw_key_id)
    if err != nil {
       t.Fatal(err)
    }
-   key, err := module.Key(license)
+   key, err := module.Key(Poster{}, key_id)
    if err != nil {
       t.Fatal(err)
    }
    fmt.Printf("%x\n", key)
 }
+
 func TestMedia(t *testing.T) {
    for _, test_path := range test_paths {
       resolve, err := Path(test_path).Resolve()
@@ -105,3 +82,31 @@ var test_paths = []string{
    "/shows/friends/the-one-with-the-bullies-s2e21",
 }
 
+func TestManifest(t *testing.T) {
+   for _, test_path := range test_paths {
+      resolve, err := Path(test_path).Resolve()
+      if err != nil {
+         t.Fatal(err)
+      }
+      time.Sleep(99 * time.Millisecond)
+      axis, err := resolve.Axis()
+      if err != nil {
+         t.Fatal(err)
+      }
+      time.Sleep(99 * time.Millisecond)
+      media, err := axis.Media()
+      if err != nil {
+         t.Fatal(err)
+      }
+      manifest, err := axis.Manifest(media)
+      if err != nil {
+         t.Fatal(err)
+      }
+      text, err := manifest.marshal()
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Println(string(text))
+      time.Sleep(99 * time.Millisecond)
+   }
+}
