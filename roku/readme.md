e4498b4771f7788ccb20b3193029b0c01fae8d18
3052 <spen+3052@protonmail.com>
2024-05-02 17:05:57 -0500

    deleted:    blog/roku/1_test.go

--- roku/readme.md
+++ roku/readme.md
@@ -24,3 +24,19 @@ but login uses CAPTCHA:
 ~~~
 {"error": "captcha"}
 ~~~
+
+## Android
+
+com.roku.remote is remote only:
+
+https://play.google.com/store/apps/details?id=com.roku.remote
+
+com.roku.trc is broken:
+
+https://apkmirror.com/apk/roku-inc/the-roku-channel
+
+com.roku.web.trc is The Roku Channel (Android TV):
+
+https://play.google.com/store/apps/details?id=com.roku.web.trc
+
+create Android 9 device. install system certificate.
--- roku/roku.go
+++ roku/roku.go
@@ -32,61 +32,10 @@ func (p Playback) RequestUrl() (string, bool) {
    return p.DRM.Widevine.LicenseServer, true
 }
 
-func (c CrossSite) Playback(id string) (*Playback, error) {
-   csrf, ok := c.csrf()
-   if !ok {
-      return nil, http.ErrNoCookie
-   }
-   body, err := func() ([]byte, error) {
-      m := map[string]string{
-         "mediaFormat": "mpeg-dash",
-         "providerId": "rokuavod",
-         "rokuId": id,
-      }
-      return json.Marshal(m)
-   }()
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://therokuchannel.roku.com/api/v3/playback",
-      bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   // we could use Request.AddCookie, but we would need to call it after this,
-   // otherwise it would be clobbered
-   req.Header = http.Header{
-      "CSRF-Token": {c.token},
-      "Content-Type": {"application/json"},
-      "Cookie": {csrf.Raw},
-   }
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      return nil, errors.New(res.Status)
-   }
-   play := new(Playback)
-   err = json.NewDecoder(res.Body).Decode(play)
-   if err != nil {
-      return nil, err
-   }
-   return play, nil
-}
-
 func (Playback) WrapRequest(b []byte) ([]byte, error) {
    return b, nil
 }
 
 func (Playback) UnwrapResponse(b []byte) ([]byte, error) {
    return b, nil
-}
-
-type CrossSite struct {
-   cookies []*http.Cookie
-   token string
 }
