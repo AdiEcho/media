468cad0a4af6c991c9deee8af3cdc7b41f7e2b99
3052 <spen+3052@protonmail.com>
2024-05-03 07:18:47 -0500

    +   var token roku.AccountToken

--- internal/roku/roku.go
+++ internal/roku/roku.go
@@ -2,22 +2,21 @@ package main
 
 import (
    "154.pages.dev/media/roku"
-   "errors"
    "fmt"
    "net/http"
 )
 
 func (f flags) download() error {
-   var home roku.HomeScreen
-   err := home.New(f.roku)
+   var token roku.AccountToken
+   err := token.New()
    if err != nil {
       return err
    }
-   video, ok := home.DASH()
-   if !ok {
-      return errors.New("roku.HomeScreen.DASH")
+   play, err := token.Playback(f.roku)
+   if err != nil {
+      return err
    }
-   req, err := http.NewRequest("", video.URL, nil)
+   req, err := http.NewRequest("", play.URL, nil)
    if err != nil {
       return err
    }
@@ -27,20 +26,16 @@ func (f flags) download() error {
    }
    for _, medium := range media {
       if medium.ID == f.representation {
-         var site roku.CrossSite
-         err := site.New()
-         if err != nil {
-            return err
-         }
-         f.s.Poster, err = site.Playback(f.roku)
+         var home roku.HomeScreen
+         err := home.New(f.roku)
          if err != nil {
             return err
          }
          f.s.Name = roku.Namer{home}
+         f.s.Poster = play
          return f.s.Download(medium)
       }
    }
-   // 2 MPD all
    for i, medium := range media {
       if i >= 1 {
          fmt.Println()
