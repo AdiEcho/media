610040ec8ac3501a216737892ad064d7b9f7cfcc
3052 <spen+3052@protonmail.com>
2024-05-03 16:52:52 -0500

    new file:   blog/sbs/sbs.go

---
+++ blog/sbs/sbs.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+   "net/http"
+   "net/url"
+   "os"
+   "strings"
+)
+
+func main() {
+   var req http.Request
+   req.Header = make(http.Header)
+   req.Method = "POST"
+   req.ProtoMajor = 1
+   req.ProtoMinor = 1
+   req.URL = new(url.URL)
+   req.URL.Host = "pubads.g.doubleclick.net"
+   req.URL.Path = "/ondemand/hls/content/2488267/vid/2229616195516A/streams"
+   req.URL.Scheme = "https"
+   res, err := http.DefaultClient.Do(&req)
+   if err != nil {
+      panic(err)
+   }
+   defer res.Body.Close()
+   res.Write(os.Stdout)
+}
