937a4f53fc0f929360c3c67c343860e40f4b31f9
3052 <spen+3052@protonmail.com>
2024-05-12 15:39:34 -0500

    +   Article(id: $articleId, full_url_slug: $articleUrlSlug) {

--- blog/cine/member/Article.go
+++ blog/cine/member/Article.go
@@ -33,116 +33,37 @@ func article() (*http.Response, error) {
 
 const query_article = `
 query Article($articleId: Int, $articleUrlSlug: String) {
-  Article(id: $articleId, full_url_slug: $articleUrlSlug) {
-    ... on Article {
-        id
-        url_slug
-        full_url_slug
-        canonical_url
-        canonical_title
-        name
-        type
-        is_auth_required
-        parent_id
-        assets {
-          ...Asset
-        }
-        images {
-          ...ArticleFile
-        }
-        posters {
-          ...ArticleFile
-        }
-        metas(output: html) {
-          ...ArticleMeta
-        }
-        categories {
-          ...ArticleCategory
-        }
-        published_at
-        products {
-          ...ProductListed
-        }
-        upsell_products {
-          ...ProductListed
-        }
-        upsell_product_call_to_action_tag
-        is_downloadable
-        ribbon_title
-        ribbon_settings
+   Article(id: $articleId, full_url_slug: $articleUrlSlug) {
+      ... on Article {
+         id
+         url_slug
+         full_url_slug
+         canonical_url
+         canonical_title
+         name
+         type
+         is_auth_required
+         parent_id
+         assets {
+            ... on Asset {
+               id
+               duration
+               linked_type
+               accessibility
+            }
+         }
+         metas(output: html) {
+            ... on ArticleMeta {
+               key
+               value
+            }
+         }
+         published_at
+         upsell_product_call_to_action_tag
+         is_downloadable
+         ribbon_title
+         ribbon_settings
       }
-
-  }
-}
-
-fragment ArticleMeta on ArticleMeta {
-  key
-  value
-}
-
-fragment Asset on Asset {
-  id
-  duration
-  linked_type
-  accessibility
-  screenshots {
-    ...File
-  }
-}
-
-fragment File on File {
-  type
-  url
-  title
-  base_url
-  file_name
-  width
-  height
-}
-
-fragment ArticleFile on File {
-  type
-  url
-  base_url
-  file_name
-  title
-  width
-  height
-}
-
-fragment ArticleCategory on CategoryListedAsRelation {
-  parent_id
-  id
-  name
-  type
-  metas {
-    key
-    value
-  }
-}
-
-fragment ProductListed on ProductListedAsRelation {
-  id
-  remote_product_id
-  name
-  title
-  description(output: html)
-  description_short(output: html)
-  price
-  credit_price
-  currency
-  currency_symbol
-  expires_in
-  call_to_action_tag
-  articles {
-    ...ArticleProductListed
-  }
-  type
-}
-
-fragment ArticleProductListed on ArticleListedAsRelation {
-  id
-  name
-  type
+   }
 }
 `
