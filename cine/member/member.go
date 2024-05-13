e262c313f9d44f9c11582882698575773c9960e9
3052 <spen+3052@protonmail.com>
2024-05-12 22:02:03 -0500

    new file:   internal/cine-member/main.go

--- cine/member/member.go
+++ cine/member/member.go
@@ -1,11 +1,11 @@
 package member
 
 import (
-   "bytes"
-   "encoding/json"
-   "errors"
-   "io"
-   "net/http"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
 )
 
 const user_authenticate = `
@@ -16,119 +16,73 @@ mutation($email: String, $password: String) {
 }
 `
 
-func (a *authenticate) New(email, password string) error {
-   body, err := func() ([]byte, error) {
-      var s struct {
-         Query string `json:"query"`
-         Variables struct {
-            Email string `json:"email"`
-            Password string `json:"password"`
-         } `json:"variables"`
-      }
-      s.Query = user_authenticate
-      s.Variables.Email = email
-      s.Variables.Password = password
-      return json.Marshal(s)
-   }()
-   if err != nil {
-      return err
-   }
-   res, err := http.Post(
-      "https://api.audienceplayer.com/graphql/2/user",
-      "application/json", bytes.NewReader(body),
-   )
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   a.data, err = io.ReadAll(res.Body)
-   if err != nil {
-      return err
-   }
-   return nil
-}
-
-type authenticate struct {
-   data []byte
-   v struct {
-      Data struct {
-         UserAuthenticate struct {
-            AccessToken string `json:"access_token"`
-         }
-      }
-   }
-}
-
-func (a *authenticate) unmarshal() error {
-   return json.Unmarshal(a.data, &a.v)
-}
 func (a asset_play) dash() (string, bool) {
-   for _, title := range a.Entitlements {
-      if title.Protocol == "dash" {
-         return title.Manifest, true
-      }
-   }
-   return "", false
+	for _, title := range a.Entitlements {
+		if title.Protocol == "dash" {
+			return title.Manifest, true
+		}
+	}
+	return "", false
 }
 
 type asset_play struct {
-   Entitlements []struct {
-      Manifest string
-      Protocol string
-   }
+	Entitlements []struct {
+		Manifest string
+		Protocol string
+	}
 }
 
 // geo block - VPN not x-forwarded-for
-func (a authenticate) play(asset *article_asset) (*asset_play, error) {
-   body, err := func() ([]byte, error) {
-      var s struct {
-         Query string `json:"query"`
-         Variables struct {
-            ArticleId int `json:"article_id"`
-            AssetId int `json:"asset_id"`
-         } `json:"variables"`
-      }
-      s.Query = query_asset
-      s.Variables.ArticleId = asset.article.ID
-      s.Variables.AssetId = asset.ID
-      return json.Marshal(s)
-   }()
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://api.audienceplayer.com/graphql/2/user",
-      bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "authorization": {"Bearer " + a.v.Data.UserAuthenticate.AccessToken},
-      "content-type": {"application/json"},
-   }
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   text, err := io.ReadAll(res.Body)
-   if err != nil {
-      return nil, err
-   }
-   var s struct {
-      Data struct {
-         ArticleAssetPlay *asset_play
-      }
-   }
-   err = json.Unmarshal(text, &s)
-   if err != nil {
-      return nil, err
-   }
-   if v := s.Data.ArticleAssetPlay; v != nil {
-      return v, nil
-   }
-   return nil, errors.New(string(text))
+func (a Authenticate) play(asset *article_asset) (*asset_play, error) {
+	body, err := func() ([]byte, error) {
+		var s struct {
+			Query     string `json:"query"`
+			Variables struct {
+				ArticleId int `json:"article_id"`
+				AssetId   int `json:"asset_id"`
+			} `json:"variables"`
+		}
+		s.Query = query_asset
+		s.Variables.ArticleId = asset.article.ID
+		s.Variables.AssetId = asset.ID
+		return json.Marshal(s)
+	}()
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(
+		"POST", "https://api.audienceplayer.com/graphql/2/user",
+		bytes.NewReader(body),
+	)
+	if err != nil {
+		return nil, err
+	}
+	req.Header = http.Header{
+		"authorization": {"Bearer " + a.v.Data.UserAuthenticate.AccessToken},
+		"content-type":  {"application/json"},
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	text, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var s struct {
+		Data struct {
+			ArticleAssetPlay *asset_play
+		}
+	}
+	err = json.Unmarshal(text, &s)
+	if err != nil {
+		return nil, err
+	}
+	if v := s.Data.ArticleAssetPlay; v != nil {
+		return v, nil
+	}
+	return nil, errors.New(string(text))
 }
 
 const query_asset = `
@@ -143,3 +97,52 @@ mutation($article_id: Int, $asset_id: Int) {
    }
 }
 `
+
+/////////////
+
+type Authenticate struct {
+	data []byte
+	v    struct {
+		Data struct {
+			UserAuthenticate struct {
+				AccessToken string `json:"access_token"`
+			}
+		}
+	}
+}
+
+func (a *Authenticate) New(email, password string) error {
+	body, err := func() ([]byte, error) {
+		var s struct {
+			Query     string `json:"query"`
+			Variables struct {
+				Email    string `json:"email"`
+				Password string `json:"password"`
+			} `json:"variables"`
+		}
+		s.Query = user_authenticate
+		s.Variables.Email = email
+		s.Variables.Password = password
+		return json.Marshal(s)
+	}()
+	if err != nil {
+		return err
+	}
+	res, err := http.Post(
+		"https://api.audienceplayer.com/graphql/2/user",
+		"application/json", bytes.NewReader(body),
+	)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	a.data, err = io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *Authenticate) unmarshal() error {
+	return json.Unmarshal(a.data, &a.v)
+}
--- cine/member/member_test.go
+++ cine/member/member_test.go
@@ -1,43 +1,43 @@
 package member
 
 import (
-   "fmt"
-   "os"
-   "testing"
+	"fmt"
+	"os"
+	"testing"
 )
 
 func TestAsset(t *testing.T) {
-   article, err := american_hustle.article()
-   if err != nil {
-      t.Fatal(err)
-   }
-   var auth authenticate
-   auth.data, err = os.ReadFile("authenticate.json")
-   if err != nil {
-      t.Fatal(err)
-   }
-   auth.unmarshal()
-   asset, ok := article.film()
-   if !ok {
-      t.Fatal("data_article.film")
-   }
-   play, err := auth.play(asset)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Println(play.dash())
+	article, err := american_hustle.article()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var auth Authenticate
+	auth.data, err = os.ReadFile("authenticate.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	auth.unmarshal()
+	asset, ok := article.film()
+	if !ok {
+		t.Fatal("data_article.film")
+	}
+	play, err := auth.play(asset)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(play.dash())
 }
 
 func TestAuthenticate(t *testing.T) {
-   username := os.Getenv("cineMember_username")
-   if username == "" {
-      t.Fatal("Getenv")
-   }
-   password := os.Getenv("cineMember_password")
-   var auth authenticate
-   err := auth.New(username, password)
-   if err != nil {
-      t.Fatal(err)
-   }
-   os.WriteFile("authenticate.json", auth.data, 0666)
+	username := os.Getenv("cineMember_username")
+	if username == "" {
+		t.Fatal("Getenv")
+	}
+	password := os.Getenv("cineMember_password")
+	var auth Authenticate
+	err := auth.New(username, password)
+	if err != nil {
+		t.Fatal(err)
+	}
+	os.WriteFile("authenticate.json", auth.data, 0666)
 }
