e5a8a55ca085bc78e99522ef1d39fe88a3581329
3052 <spen+3052@protonmail.com>
2024-05-19 13:49:24 -0500

    renamed:    criterion/video.go -> criterion/videos.go

--- criterion/criterion.go
+++ criterion/criterion.go
@@ -1,98 +1,104 @@
 package criterion
 
 import (
-	"encoding/json"
-	"io"
-	"net/http"
-	"net/url"
-	"strings"
+   "encoding/json"
+   "errors"
+   "io"
+   "net/http"
+   "net/url"
+   "strings"
 )
+
+func (a AuthToken) item(slug string) (*embed_item, error) {
+   address := func() string {
+      var b strings.Builder
+      b.WriteString("https://api.vhx.com/collections/")
+      b.WriteString(slug)
+      b.WriteString("/items?site_id=59054")
+      return b.String()
+   }()
+   req, err := http.NewRequest("", address, nil)
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("authorization", "Bearer "+a.v.AccessToken)
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      var b strings.Builder
+      res.Write(&b)
+      return nil, errors.New(b.String())
+   }
+   var s struct {
+      Embedded struct {
+         Items []embed_item
+      } `json:"_embedded"`
+   }
+   err = json.NewDecoder(res.Body).Decode(&s)
+   if err != nil {
+      return nil, err
+   }
+   return &s.Embedded.Items[0], nil
+}
 
 const client_id = "9a87f110f79cd25250f6c7f3a6ec8b9851063ca156dae493bf362a7faf146c78"
 
 type AuthToken struct {
-	data []byte
-	v    struct {
-		AccessToken string `json:"access_token"`
-	}
+   data []byte
+   v    struct {
+      AccessToken string `json:"access_token"`
+   }
 }
 
 func (a *AuthToken) New(username, password string) error {
-	res, err := http.PostForm("https://auth.vhx.com/v1/oauth/token", url.Values{
-		"client_id":  {client_id},
-		"grant_type": {"password"},
-		"password":   {password},
-		"username":   {username},
-	})
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	a.data, err = io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (a AuthToken) item(slug string) (*embed_item, error) {
-	address := func() string {
-		var b strings.Builder
-		b.WriteString("https://api.vhx.com/collections/")
-		b.WriteString(slug)
-		b.WriteString("/items?site_id=59054")
-		return b.String()
-	}()
-	req, err := http.NewRequest("", address, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("authorization", "Bearer "+a.v.AccessToken)
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	var s struct {
-		Embedded struct {
-			Items []embed_item
-		} `json:"_embedded"`
-	}
-	err = json.NewDecoder(res.Body).Decode(&s)
-	if err != nil {
-		return nil, err
-	}
-	return &s.Embedded.Items[0], nil
+   res, err := http.PostForm("https://auth.vhx.com/v1/oauth/token", url.Values{
+      "client_id":  {client_id},
+      "grant_type": {"password"},
+      "password":   {password},
+      "username":   {username},
+   })
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   a.data, err = io.ReadAll(res.Body)
+   if err != nil {
+      return err
+   }
+   return nil
 }
 
 func (a *AuthToken) unmarshal() error {
-	return json.Unmarshal(a.data, &a.v)
+   return json.Unmarshal(a.data, &a.v)
 }
 
 type embed_item struct {
-	ID       int64
-	Metadata struct {
-		YearReleased int `json:"year_released"`
-	}
-	Name string
+   ID       int64
+   Metadata struct {
+      YearReleased int `json:"year_released"`
+   }
+   Name string
 }
 
 func (embed_item) Episode() int {
-	return 0
+   return 0
 }
 
 func (embed_item) Season() int {
-	return 0
+   return 0
 }
 
 func (embed_item) Show() string {
-	return ""
+   return ""
 }
 
 func (e embed_item) Title() string {
-	return e.Name
+   return e.Name
 }
 
 func (e embed_item) Year() int {
-	return e.Metadata.YearReleased
+   return e.Metadata.YearReleased
 }
--- criterion/videos.go
+++ criterion/videos.go
@@ -1,12 +1,11 @@
-package main
+package criterion
 
 import (
    "net/http"
    "net/url"
-   "os"
 )
 
-func main() {
+func videos() (*http.Response, error) {
    var req http.Request
    req.Header = make(http.Header)
    req.Header["Authorization"] = []string{"Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ2YmZlZmMzNGIyNTdhYTE4Y2E2NDUzNDE2ZTlmZmRjNjk4MDAxMDdhZTQ2ZWJhODg0YTU2ZDBjOGQ4NTYzMzgifQ.eyJhcHBfaWQiOjM0NDksImV4cCI6MTcxNjE0NzQyNiwibm9uY2UiOiI1MWRlYzhmYTE4NjZiYjI5Iiwic2NvcGVzIjpbXSwic2Vzc2lvbl9pZCI6InorL2I5aC9laUVCNGkxZmR2VEw5a2c9PSIsInNpdGVfaWQiOjU5MDU0LCJ1c2VyX2lkIjo2NTc0NjE0Nn0.cIAPnjuaxgRj7rHJcVaFlZsiPkLt-6e6AdKCutEKp2z9JFaSPXkbOn6VS_z6tgcNtlzPmrK08G-A_nvLrdAPurojQMGzJYvY4853ni8tRsvdqvzHyGK9m6Zarcyg9gPJa-Sj5b4x1VtUuWeo4aZ7WrF8pXriptULPL-VZj9XnpX4BRK4xAq6AAVlFVjjOv8MD0_ry3KVKNTxgDgtvFumjO1qgjK3JcLRSmHmxUyjzp3_xtulFBFdYNcTs0ivNnLGbaIEtVfujyDnmrFsetiQyNMxEwAR-CLkdZxMMRZdLTiAgAOd5lPJAmTFIY3EHuB3u6y3IuSoI1UsTY7gXduG78jxFu6045XlSSj-fWAwlYzqwhI-Hi7JJsPMPlh23q4cS3e6KAG_1Jg2EcFCuCr0aXDJjm43UUrt9zXwdaMs7O8VpzqbxF1YDp-lZwNLku8kUxvgvD_krdISWa0c-tN_NIon44sbIJBZj0EfrWAb_oGswPKu4_Ng8Urgi26g9HCt8HYalfuSgW_QZzZ5Wq0czK-Gpz7H0V1vmcYJAweU7tc2pws84fC8GZSSbSzzNcUD-fhKwHh_qeWxj_L8RfGG7O65lGBNoeI1CsM6luGcS-ioc0_fSMnxU4Xd8_RQw-127-zT1aTlCArPQZFM7Mb7xooX2tCjsUAma691G6lP7A4"}
@@ -17,10 +16,5 @@ func main() {
    req.URL.Scheme = "http"
    req.URL.Path = "/videos/my-dinner-with-andre"
    req.URL.RawQuery = "url=my-dinner-with-andre"
-   res, err := http.DefaultClient.Do(&req)
-   if err != nil {
-      panic(err)
-   }
-   defer res.Body.Close()
-   res.Write(os.Stdout)
+   return http.DefaultClient.Do(&req)
 }
