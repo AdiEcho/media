b90a15976d59ece19ec012160fbfd349aaa73b00
3052 <spen+3052@protonmail.com>
2024-05-21 18:45:03 -0500

    +type on_demand struct {

--- blog/rakuten/rakuten.go
+++ blog/rakuten/rakuten.go
@@ -3,23 +3,45 @@ package rakuten
 import (
    "bytes"
    "encoding/json"
+   "errors"
    "net/http"
-   "net/url"
-   "strconv"
 )
 
-func streamings(class int, content_id string) (*http.Response, error) {
-   body, err := json.Marshal(map[string]string{
-      "audio_language": "ENG",
-      "audio_quality": "2.0",
-      "classification_id": strconv.Itoa(class),
-      "content_id": content_id,
-      "content_type": "movies",
-      "device_serial": "!",
-      "player": "atvui40:DASH-CENC:WVM",
-      "subtitle_language": "MIS",
-      "video_type": "stream",
-   })
+var classification = map[string]int{
+   "fr": 23,
+   "se": 282,
+}
+
+type on_demand struct {
+   AudioLanguage string `json:"audio_language"`
+   AudioQuality string `json:"audio_quality"`
+   ClassificationId int `json:"classification_id"`
+   ContentId string `json:"content_id"`
+   ContentType string `json:"content_type"`
+   DeviceIdentifier string `json:"device_identifier"`
+   DeviceSerial string `json:"device_serial"`
+   DeviceStreamVideoQuality string `json:"device_stream_video_quality"`
+   Player string `json:"player"`
+   SubtitleLanguage string `json:"subtitle_language"`
+   VideoType string `json:"video_type"`
+}
+
+func (o *on_demand) New(class int, content_id string) {
+   o.AudioLanguage = "ENG"
+   o.AudioQuality = "2.0"
+   o.ClassificationId = class
+   o.ContentId = content_id
+   o.ContentType = "movies"
+   o.DeviceIdentifier = "atvui40"
+   o.DeviceSerial = "!"
+   o.DeviceStreamVideoQuality = "FHD"
+   o.Player = "atvui40:DASH-CENC:WVM"
+   o.SubtitleLanguage = "MIS"
+   o.VideoType = "stream"
+}
+
+func (o on_demand) stream() (*stream_info, error) {
+   body, err := json.Marshal(o)
    if err != nil {
       return nil, err
    }
@@ -30,15 +52,47 @@ func streamings(class int, content_id string) (*http.Response, error) {
    if err != nil {
       return nil, err
    }
-   req.URL.RawQuery = url.Values{
-      "device_identifier": {"atvui40"},
-      "device_stream_video_quality": {"FHD"},
-   }.Encode()
    req.Header.Set("content-type", "application/json")
-   return http.DefaultClient.Do(req)
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      var b bytes.Buffer
+      res.Write(&b)
+      return nil, errors.New(b.String())
+   }
+   var s struct {
+      Data struct {
+         StreamInfos []stream_info `json:"stream_infos"`
+      }
+   }
+   err = json.NewDecoder(res.Body).Decode(&s)
+   if err != nil {
+      return nil, err
+   }
+   return &s.Data.StreamInfos[0], nil
 }
 
-var classification = map[string]int{
-   "fr": 23,
-   "se": 282,
+func (s stream_info) RequestUrl() (string, bool) {
+   return s.LicenseUrl, true
+}
+
+func (stream_info) RequestHeader() (http.Header, error) {
+   return http.Header{}, nil
+}
+
+func (stream_info) WrapRequest(b []byte) ([]byte, error) {
+   return b, nil
+}
+
+type stream_info struct {
+   LicenseUrl string `json:"license_url"`
+   URL string
+   VideoQuality string `json:"video_quality"`
+}
+
+func (stream_info) UnwrapResponse(b []byte) ([]byte, error) {
+   return b, nil
 }
--- blog/rakuten/rakuten_test.go
+++ blog/rakuten/rakuten_test.go
@@ -1,26 +1,72 @@
 package rakuten
 
 import (
+   "154.pages.dev/widevine"
+   "encoding/hex"
+   "fmt"
    "os"
    "testing"
 )
 
-//if strings.Contains(text, `"video_quality":"FHD"`) {
+// rakuten.tv/se/movies/i-heart-huckabees
+const default_kid = "9a534a1f12d68e1a2359f38710fddb65" 
 
-func TestFr(t *testing.T) {
-   res, err := streamings(classification["fr"], "jerry-maguire")
+func TestLicense(t *testing.T) {
+   home, err := os.UserHomeDir()
    if err != nil {
       t.Fatal(err)
    }
-   defer res.Body.Close()
-   res.Write(os.Stdout)
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   key_id, err := hex.DecodeString(default_kid)
+   if err != nil {
+      t.Fatal(err)
+   }
+   var module widevine.CDM
+   err = module.New(private_key, client_id, widevine.PSSH(key_id, nil))
+   if err != nil {
+      t.Fatal(err)
+   }
+   var video on_demand
+   video.New(classification["se"], "i-heart-huckabees")
+   stream, err := video.stream()
+   if err != nil {
+      t.Fatal(err)
+   }
+   key, err := module.Key(stream, key_id)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
 }
 
 func TestSe(t *testing.T) {
-   res, err := streamings(classification["se"], "i-heart-huckabees")
+   var video on_demand
+   video.New(classification["se"], "i-heart-huckabees")
+   stream, err := video.stream()
    if err != nil {
       t.Fatal(err)
    }
-   defer res.Body.Close()
-   res.Write(os.Stdout)
+   if stream.VideoQuality != "FHD" {
+      t.Fatal(stream)
+   }
+   fmt.Printf("%+v\n", stream)
+}
+
+func TestFr(t *testing.T) {
+   var video on_demand
+   video.New(classification["fr"], "jerry-maguire")
+   stream, err := video.stream()
+   if err != nil {
+      t.Fatal(err)
+   }
+   if stream.VideoQuality != "FHD" {
+      t.Fatal(stream)
+   }
 }
