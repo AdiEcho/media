7be7bc8632ef2609126a474ae6fc224ba406c837
3052 <spen+3052@protonmail.com>
2024-05-20 21:41:54 -0500

    new file:   blog/rakuten/se.json

--- blog/rakuten/rakuten_test.go
+++ blog/rakuten/rakuten_test.go
@@ -5,13 +5,22 @@ import (
    "testing"
 )
 
+//if strings.Contains(text, `"video_quality":"FHD"`) {
+
 func TestFr(t *testing.T) {
-   // {"se", "i-heart-huckabees"},
    res, err := streamings(classification["fr"], "jerry-maguire")
    if err != nil {
       t.Fatal(err)
    }
    defer res.Body.Close()
    res.Write(os.Stdout)
-   //if strings.Contains(text, `"video_quality":"FHD"`) {
+}
+
+func TestSe(t *testing.T) {
+   res, err := streamings(classification["se"], "i-heart-huckabees")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer res.Body.Close()
+   res.Write(os.Stdout)
 }
