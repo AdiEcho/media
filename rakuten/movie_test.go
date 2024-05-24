e62b7b658d8fa4b815968b225515e74ca4d531e7
3052 <spen+3052@protonmail.com>
2024-05-24 17:04:49 -0500

    modified:   rakuten/rakuten.go

--- rakuten/movie_test.go
+++ rakuten/movie_test.go
@@ -1,48 +1,48 @@
 package rakuten
 
 import (
-	"154.pages.dev/encoding"
-	"fmt"
-	"testing"
-	"time"
+   "154.pages.dev/encoding"
+   "fmt"
+   "testing"
+   "time"
 )
 
 type movie_test struct {
-	content_id string
-	key_id     string
-	url        string
+   content_id string
+   key_id     string
+   url        string
 }
 
 var tests = map[string]movie_test{
-	"fr": {
-		content_id: "Y2YzNGEwM2JiYjRhYTg5OWRmNDJjM2NmN2E2Y2I5MjUtbWMtMC0xMzctMC0w",
-		key_id:     "zzSgO7tKqJnfQsPPemy5JQ==",
-		url:        "rakuten.tv/fr/movies/jerry-maguire",
-	},
-	"se": {
-		content_id: "OWE1MzRhMWYxMmQ2OGUxYTIzNTlmMzg3MTBmZGRiNjUtbWMtMC0xNDctMC0w",
-		key_id:     "mlNKHxLWjhojWfOHEP3bZQ==",
-		url:        "rakuten.tv/se/movies/i-heart-huckabees",
-	},
+   "fr": {
+      content_id: "Y2YzNGEwM2JiYjRhYTg5OWRmNDJjM2NmN2E2Y2I5MjUtbWMtMC0xMzctMC0w",
+      key_id:     "zzSgO7tKqJnfQsPPemy5JQ==",
+      url:        "rakuten.tv/fr/movies/jerry-maguire",
+   },
+   "se": {
+      content_id: "OWE1MzRhMWYxMmQ2OGUxYTIzNTlmMzg3MTBmZGRiNjUtbWMtMC0xNDctMC0w",
+      key_id:     "mlNKHxLWjhojWfOHEP3bZQ==",
+      url:        "rakuten.tv/se/movies/i-heart-huckabees",
+   },
 }
 
 func TestMovie(t *testing.T) {
-	for _, test := range tests {
-		var web WebAddress
-		err := web.Set(test.url)
-		if err != nil {
-			t.Fatal(err)
-		}
-		movie, err := web.Movie()
-		if err != nil {
-			t.Fatal(err)
-		}
-		fmt.Printf("%+v\n", movie)
-		name, err := encoding.Name(movie)
-		if err != nil {
-			t.Fatal(err)
-		}
-		fmt.Printf("%q\n", name)
-		time.Sleep(time.Second)
-	}
+   for _, test := range tests {
+      var web WebAddress
+      err := web.Set(test.url)
+      if err != nil {
+         t.Fatal(err)
+      }
+      movie, err := web.Movie()
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%+v\n", movie)
+      name, err := encoding.Name(movie)
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%q\n", name)
+      time.Sleep(time.Second)
+   }
 }
--- rakuten/rakuten.go
+++ rakuten/rakuten.go
@@ -1,112 +1,112 @@
 package rakuten
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"net/http"
+   "bytes"
+   "encoding/json"
+   "errors"
+   "net/http"
 )
 
 func (w WebAddress) HD() OnDemand {
-	return w.video("HD")
+   return w.video("HD")
 }
 
 func (s *StreamInfo) Unmarshal(text []byte) error {
-	return json.Unmarshal(text, s)
+   return json.Unmarshal(text, s)
 }
 
 func (s StreamInfo) Marshal() ([]byte, error) {
-	return json.MarshalIndent(s, "", " ")
+   return json.MarshalIndent(s, "", " ")
 }
 
 type StreamInfo struct {
-	LicenseUrl   string `json:"license_url"`
-	URL          string
-	VideoQuality string `json:"video_quality"`
+   LicenseUrl   string `json:"license_url"`
+   URL          string
+   VideoQuality string `json:"video_quality"`
 }
 
 // geo block
 func (o OnDemand) Info() (*StreamInfo, error) {
-	body, err := json.Marshal(o)
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequest(
-		"POST", "https://gizmo.rakuten.tv/v3/avod/streamings",
-		bytes.NewReader(body),
-	)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("content-type", "application/json")
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		var b bytes.Buffer
-		res.Write(&b)
-		return nil, errors.New(b.String())
-	}
-	var s struct {
-		Data struct {
-			StreamInfos []StreamInfo `json:"stream_infos"`
-		}
-	}
-	err = json.NewDecoder(res.Body).Decode(&s)
-	if err != nil {
-		return nil, err
-	}
-	return &s.Data.StreamInfos[0], nil
+   body, err := json.Marshal(o)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://gizmo.rakuten.tv/v3/avod/streamings",
+      bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("content-type", "application/json")
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      var b bytes.Buffer
+      res.Write(&b)
+      return nil, errors.New(b.String())
+   }
+   var s struct {
+      Data struct {
+         StreamInfos []StreamInfo `json:"stream_infos"`
+      }
+   }
+   err = json.NewDecoder(res.Body).Decode(&s)
+   if err != nil {
+      return nil, err
+   }
+   return &s.Data.StreamInfos[0], nil
 }
 
 func (w WebAddress) FHD() OnDemand {
-	return w.video("FHD")
+   return w.video("FHD")
 }
 
 func (s StreamInfo) RequestUrl() (string, bool) {
-	return s.LicenseUrl, true
+   return s.LicenseUrl, true
 }
 
 func (StreamInfo) RequestHeader() (http.Header, error) {
-	return http.Header{}, nil
+   return http.Header{}, nil
 }
 
 func (StreamInfo) WrapRequest(b []byte) ([]byte, error) {
-	return b, nil
+   return b, nil
 }
 
 func (StreamInfo) UnwrapResponse(b []byte) ([]byte, error) {
-	return b, nil
+   return b, nil
 }
 
 type OnDemand struct {
-	AudioLanguage            string `json:"audio_language"`
-	AudioQuality             string `json:"audio_quality"`
-	ClassificationId         int    `json:"classification_id"`
-	ContentId                string `json:"content_id"`
-	ContentType              string `json:"content_type"`
-	DeviceIdentifier         string `json:"device_identifier"`
-	DeviceSerial             string `json:"device_serial"`
-	DeviceStreamVideoQuality string `json:"device_stream_video_quality"`
-	Player                   string `json:"player"`
-	SubtitleLanguage         string `json:"subtitle_language"`
-	VideoType                string `json:"video_type"`
+   AudioLanguage            string `json:"audio_language"`
+   AudioQuality             string `json:"audio_quality"`
+   ClassificationId         int    `json:"classification_id"`
+   ContentId                string `json:"content_id"`
+   ContentType              string `json:"content_type"`
+   DeviceIdentifier         string `json:"device_identifier"`
+   DeviceSerial             string `json:"device_serial"`
+   DeviceStreamVideoQuality string `json:"device_stream_video_quality"`
+   Player                   string `json:"player"`
+   SubtitleLanguage         string `json:"subtitle_language"`
+   VideoType                string `json:"video_type"`
 }
 
 func (w WebAddress) video(quality string) OnDemand {
-	var v OnDemand
-	v.AudioLanguage = "ENG"
-	v.AudioQuality = "2.0"
-	v.ContentType = "movies"
-	v.DeviceSerial = "!"
-	v.Player = "atvui40:DASH-CENC:WVM"
-	v.SubtitleLanguage = "MIS"
-	v.VideoType = "stream"
-	v.DeviceIdentifier = "atvui40"
-	v.ClassificationId = w.classification_id
-	v.ContentId = w.content_id
-	v.DeviceStreamVideoQuality = quality
-	return v
+   var v OnDemand
+   v.AudioLanguage = "ENG"
+   v.AudioQuality = "2.0"
+   v.ContentType = "movies"
+   v.DeviceSerial = "!"
+   v.Player = "atvui40:DASH-CENC:WVM"
+   v.SubtitleLanguage = "MIS"
+   v.VideoType = "stream"
+   v.DeviceIdentifier = "atvui40"
+   v.ClassificationId = w.classification_id
+   v.ContentId = w.content_id
+   v.DeviceStreamVideoQuality = quality
+   return v
 }
