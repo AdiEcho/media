9959effd258a8cacb6608938982800f2d86a1c2c
3052 <spen+3052@protonmail.com>
2024-05-27 22:26:00 -0500

    +	154.pages.dev/text v1.2.2

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.22.0
 require (
 	154.pages.dev/dash v1.22.8
 	154.pages.dev/sofia v1.4.6
-	154.pages.dev/text v1.2.1
+	154.pages.dev/text v1.2.2
 	154.pages.dev/widevine v1.4.1
 )
 
--- internal/stream.go
+++ internal/stream.go
@@ -16,6 +16,101 @@ import (
    "strings"
 )
 
+func (s Stream) segment_template(
+   rep *dash.Representation,
+   base *url.URL,
+   initial string,
+   ext string,
+) error {
+   req, err := http.NewRequest("GET", initial, nil)
+   if err != nil {
+      return err
+   }
+   req.URL = base.ResolveReference(req.URL)
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      return errors.New(res.Status)
+   }
+   file, err := func() (*os.File, error) {
+      s, err := text.Name(s.Name)
+      if err != nil {
+         return nil, err
+      }
+      return os.Create(text.Clean(s) + ext)
+   }()
+   if err != nil {
+      return err
+   }
+   defer file.Close()
+   var protect protection
+   err = protect.init(file, res.Body)
+   if err != nil {
+      return err
+   }
+   key, err := s.key(protect)
+   if err != nil {
+      return err
+   }
+   var meter text.ProgressMeter
+   text.SetTransport(nil)
+   defer text.Transport{}.Set()
+   template, ok := rep.GetSegmentTemplate()
+   if !ok {
+      return errors.New("GetSegmentTemplate")
+   }
+   media, err := template.GetMedia(rep)
+   if err != nil {
+      return err
+   }
+   meter.Set(len(media))
+   client := http.Client{ // github.com/golang/go/issues/18639
+      Transport: &http.Transport{
+         Proxy: http.ProxyFromEnvironment,
+         TLSNextProto: map[string]func(string, *tls.Conn) http.RoundTripper{},
+      },
+   }
+   for _, medium := range media {
+      req.URL, err = base.Parse(medium)
+      if err != nil {
+         return err
+      }
+      err := func() error {
+         res, err := client.Do(req)
+         if err != nil {
+            return err
+         }
+         defer res.Body.Close()
+         if res.StatusCode != http.StatusOK {
+            var b strings.Builder
+            res.Write(&b)
+            return errors.New(b.String())
+         }
+         return write_segment(file, meter.Reader(res), key)
+      }()
+      if err != nil {
+         return err
+      }
+   }
+   return nil
+}
+
+func (s Stream) Download(rep *dash.Representation) error {
+   ext, ok := rep.Ext()
+   if !ok {
+      return errors.New("Representation.Ext")
+   }
+   base := rep.GetAdaptationSet().GetPeriod().GetMpd().BaseUrl.URL
+   if v, ok := rep.GetSegmentTemplate(); ok {
+      if v, ok := v.GetInitialization(rep); ok {
+         return s.segment_template(rep, base, v, ext)
+      }
+   }
+   return s.segment_base(rep.SegmentBase, base, *rep.BaseUrl, ext)
+}
 func (s Stream) segment_base(
    segment *dash.SegmentBase,
    base *url.URL,
@@ -58,10 +153,11 @@ func (s Stream) segment_base(
    if err != nil {
       return err
    }
-   text.SetTransport(nil)
-   defer text.Transport{}.Set()
    var meter text.ProgressMeter
    meter.Set(len(references))
+   var log text.LogLevel
+   log.SetTransport(false)
+   defer log.SetTransport(true)
    for _, reference := range references {
       segment.IndexRange.Start = segment.IndexRange.End + 1
       segment.IndexRange.End += uint64(reference.ReferencedSize())
@@ -190,100 +286,4 @@ type Stream struct {
    PrivateKey string
    Name text.Namer
    Poster widevine.Poster
-}
-
-func (s Stream) segment_template(
-   rep *dash.Representation,
-   base *url.URL,
-   initial string,
-   ext string,
-) error {
-   req, err := http.NewRequest("GET", initial, nil)
-   if err != nil {
-      return err
-   }
-   req.URL = base.ResolveReference(req.URL)
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      return errors.New(res.Status)
-   }
-   file, err := func() (*os.File, error) {
-      s, err := text.Name(s.Name)
-      if err != nil {
-         return nil, err
-      }
-      return os.Create(text.Clean(s) + ext)
-   }()
-   if err != nil {
-      return err
-   }
-   defer file.Close()
-   var protect protection
-   err = protect.init(file, res.Body)
-   if err != nil {
-      return err
-   }
-   key, err := s.key(protect)
-   if err != nil {
-      return err
-   }
-   var meter text.ProgressMeter
-   text.SetTransport(nil)
-   defer text.Transport{}.Set()
-   template, ok := rep.GetSegmentTemplate()
-   if !ok {
-      return errors.New("GetSegmentTemplate")
-   }
-   media, err := template.GetMedia(rep)
-   if err != nil {
-      return err
-   }
-   meter.Set(len(media))
-   client := http.Client{ // github.com/golang/go/issues/18639
-      Transport: &http.Transport{
-         Proxy: http.ProxyFromEnvironment,
-         TLSNextProto: map[string]func(string, *tls.Conn) http.RoundTripper{},
-      },
-   }
-   for _, medium := range media {
-      req.URL, err = base.Parse(medium)
-      if err != nil {
-         return err
-      }
-      err := func() error {
-         res, err := client.Do(req)
-         if err != nil {
-            return err
-         }
-         defer res.Body.Close()
-         if res.StatusCode != http.StatusOK {
-            var b strings.Builder
-            res.Write(&b)
-            return errors.New(b.String())
-         }
-         return write_segment(file, meter.Reader(res), key)
-      }()
-      if err != nil {
-         return err
-      }
-   }
-   return nil
-}
-
-func (s Stream) Download(rep *dash.Representation) error {
-   ext, ok := rep.Ext()
-   if !ok {
-      return errors.New("Representation.Ext")
-   }
-   base := rep.GetAdaptationSet().GetPeriod().GetMpd().BaseUrl.URL
-   if v, ok := rep.GetSegmentTemplate(); ok {
-      if v, ok := v.GetInitialization(rep); ok {
-         return s.segment_template(rep, base, v, ext)
-      }
-   }
-   return s.segment_base(rep.SegmentBase, base, *rep.BaseUrl, ext)
 }
