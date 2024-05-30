731973dfc8f233fb769496ae78853603279b1944
3052 <spen+3052@protonmail.com>
2024-05-29 19:26:18 -0500

    modified:   roku/roku.go

--- roku/roku_test.go
+++ roku/roku_test.go
@@ -1,104 +1,104 @@
 package roku
 
 import (
-	"154.pages.dev/widevine"
-	"encoding/hex"
-	"fmt"
-	"os"
-	"path"
-	"testing"
-	"time"
+   "154.pages.dev/widevine"
+   "encoding/hex"
+   "fmt"
+   "os"
+   "path"
+   "testing"
+   "time"
 )
 
 func TestAccountToken(t *testing.T) {
-	var (
-		activate ActivationToken
-		err      error
-	)
-	activate.Data, err = os.ReadFile("token.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = activate.Unmarshal()
-	if err != nil {
-		t.Fatal(err)
-	}
-	var account AccountToken
-	err = account.New(&activate)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("%+v\n", account)
+   var (
+      activate ActivationToken
+      err      error
+   )
+   activate.Data, err = os.ReadFile("token.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = activate.Unmarshal()
+   if err != nil {
+      t.Fatal(err)
+   }
+   var account AccountToken
+   err = account.New(&activate)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", account)
 }
 
 func TestActivationToken(t *testing.T) {
-	text, err := os.ReadFile("code.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	var code ActivationCode
-	err = code.Unmarshal(text)
-	if err != nil {
-		t.Fatal(err)
-	}
-	token, err := code.Token()
-	if err != nil {
-		t.Fatal(err)
-	}
-	os.WriteFile("token.json", token.Data, 0666)
+   text, err := os.ReadFile("code.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var code ActivationCode
+   err = code.Unmarshal(text)
+   if err != nil {
+      t.Fatal(err)
+   }
+   token, err := code.Token()
+   if err != nil {
+      t.Fatal(err)
+   }
+   os.WriteFile("token.json", token.Data, 0666)
 }
 
 func TestPlayback(t *testing.T) {
-	var token AccountToken
-	err := token.New(nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for _, test := range tests {
-		play, err := token.Playback(path.Base(test.url))
-		if err != nil {
-			t.Fatal(err)
-		}
-		fmt.Printf("%+v\n", play)
-		time.Sleep(time.Second)
-	}
+   var token AccountToken
+   err := token.New(nil)
+   if err != nil {
+      t.Fatal(err)
+   }
+   for _, test := range tests {
+      play, err := token.Playback(path.Base(test.url))
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%+v\n", play)
+      time.Sleep(time.Second)
+   }
 }
 
 func TestLicense(t *testing.T) {
-	test := tests["episode"]
-	home, err := os.UserHomeDir()
-	if err != nil {
-		t.Fatal(err)
-	}
-	private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
-	if err != nil {
-		t.Fatal(err)
-	}
-	client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
-	if err != nil {
-		t.Fatal(err)
-	}
-	key_id, err := hex.DecodeString(test.key_id)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var module widevine.CDM
-	err = module.New(private_key, client_id, widevine.PSSH(key_id, nil))
-	if err != nil {
-		t.Fatal(err)
-	}
-	var token AccountToken
-	err = token.New(nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	play, err := token.Playback(path.Base(test.url))
-	if err != nil {
-		t.Fatal(err)
-	}
-	key, err := module.Key(play, key_id)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("%x\n", key)
+   test := tests["episode"]
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   key_id, err := hex.DecodeString(test.key_id)
+   if err != nil {
+      t.Fatal(err)
+   }
+   var module widevine.CDM
+   err = module.New(private_key, client_id, widevine.PSSH(key_id, nil))
+   if err != nil {
+      t.Fatal(err)
+   }
+   var token AccountToken
+   err = token.New(nil)
+   if err != nil {
+      t.Fatal(err)
+   }
+   play, err := token.Playback(path.Base(test.url))
+   if err != nil {
+      t.Fatal(err)
+   }
+   key, err := module.Key(play, key_id)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
 }
