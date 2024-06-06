b8b5d3bb9bbcc790249dccd6a24291e9985649e4
3052 <spen+3052@protonmail.com>
2024-06-05 22:27:51 -0500

    new file:   blog/rtbf/7.go

--- blog/rtbf/1.go
+++ blog/rtbf/1.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+   "io"
    "net/http"
    "net/url"
    "os"
+   "strings"
 )
 
 func main() {
    var req http.Request
    req.Header = make(http.Header)
+   req.Method = "POST"
    req.ProtoMajor = 1
    req.ProtoMinor = 1
    req.URL = new(url.URL)
    req.URL.Host = "login.auvio.rtbf.be"
-   req.URL.Path = "/accounts.webSdkBootstrap"
+   req.URL.Path = "/accounts.login"
    req.URL.Scheme = "https"
+   req.Body = io.NopCloser(body)
+   req.Header["Content-Type"] = []string{"application/x-www-form-urlencoded"}
+   req.Header["Cookie"] = []string{
+      "gmid=gmid.ver4.AtLtH4HMHg.LMhRVRJCFKP7uqs-cOeQLiHO5p4Gnf0AKg759MRJG72Xj9AzXsw20ySPPDaOmdSQ.EUz7cp0LCa8ATNMrSxDy9DuG5UvI5e_ZRJxrvDjrtEZJu-MqTqAcWHIz5ImHzxjzpS5i_tzQ8OOrRWUG07wvDg.sc3",
+   }
    res, err := http.DefaultClient.Do(&req)
    if err != nil {
       panic(err)
@@ -22,3 +30,9 @@ func main() {
    defer res.Body.Close()
    res.Write(os.Stdout)
 }
+
+var body = strings.NewReader(url.Values{
+   "APIKey":[]string{"4_Ml_fJ47GnBAW6FrPzMxh0w"},
+   "loginID":[]string{"EMAIL"},
+   "password":[]string{"PASSWORD"},
+}.Encode())
--- blog/rtbf/5.go
+++ blog/rtbf/5.go
@@ -12,11 +12,9 @@ func main() {
    req.ProtoMajor = 1
    req.ProtoMinor = 1
    req.URL = new(url.URL)
-   req.URL.Host = "exposure.api.redbee.live"
+   req.URL.Host = "bff-service.rtbf.be"
+   req.URL.Path = "/auvio/v1.23/embed/media/3201987"
    req.URL.Scheme = "https"
-   req.Header.Set("x-forwarded-for", "91.90.123.17")
-   req.URL.Path = "/v2/customer/RTBF/businessunit/Auvio/entitlement/3201987_6BA97Bb/play"
-   req.Header["Authorization"] = []string{"Bearer ses_c25be097-b9e2-4bdc-b12f-e70a3556b910p|acc_8982699050ed41f98fed0877bda6f616_6BA97Bb|usr_8982699050ed41f98fed0877bda6f616_6BA97Bb|null|1717634395676|2017634395638|false|7f5cdd55-1cfe-4841-9e8e-ecd8b823cfad|WEB||RTBFAuvio||2iyx75pBySG7rz7wJh6HW4NrObpoUN+enZLRTJzkzJw="}
    res, err := http.DefaultClient.Do(&req)
    if err != nil {
       panic(err)
