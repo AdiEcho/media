57ff09ff68e24465aacc8fe975fae3b5c0ba7c87
3052 <spen+3052@protonmail.com>
2024-06-07 14:37:44 -0500

    +      Subtitle string

--- rtbf/1.go
+++ rtbf/1.go
@@ -6,31 +6,17 @@ import (
    "strconv"
 )
 
+// json.data.subtitle = "06 - Les ombres de la guerre";
+// json.data.subtitle = "Avec Rosamund Pike";
+// 
+// json.data.title = "I care a lot";
+// json.data.title = "Grantchester S01";
 type embed_media struct {
    Data struct {
       AssetId string
-      Program struct {
-         Title string
-      }
       Title string
+      Subtitle string
    }
-   Meta struct {
-      SmartAds struct {
-         // these can be empty string, so we cannot use these:
-         // int `json:",string"`
-         // json.Number
-         CTE string
-         CTS string
-      }
-   }
-}
-
-func (embed_media) Show() string {
-   return ""
-}
-
-func (embed_media) Season() int {
-   return 0
 }
 
 func (e *embed_media) New(media int64) error {
@@ -48,15 +34,23 @@ func (e *embed_media) New(media int64) error {
    return json.NewDecoder(res.Body).Decode(e)
 }
 
+// its just not available from what I can tell
+func (embed_media) Year() int {
+   return 0
+}
+
+func (embed_media) Show() string {
+   return ""
+}
+
+func (embed_media) Season() int {
+   return 0
+}
+
 func (embed_media) Episode() int {
    return 0
 }
 
 func (e embed_media) Title() string {
    return e.Data.Title
-}
-
-// its just not available from what I can tell
-func (embed_media) Year() int {
-   return 0
 }
