72f270bc9b4a649bcaf5f4200c8e5a9784398dff
3052 <spen+3052@protonmail.com>
2024-06-08 16:29:00 -0500

    new file:   internal/rtbf/main.go

--- internal/rtbf/rtbf.go
+++ internal/rtbf/rtbf.go
@@ -11,59 +11,19 @@ import (
    "path/filepath"
 )
 
-type flags struct {
-   email string
-   s internal.Stream
-   home string
-   entity rtbf.EntityId
-   representation string
-   password string
-   log text.LogLevel
+func (f flags) authenticate() error {
+   var login accounts_login
+   err := login.New(username, password)
+   if err != nil {
+      t.Fatal(err)
+   }
+   text, err := login.marshal()
+   if err != nil {
+      t.Fatal(err)
+   }
+   os.WriteFile("account.json", text, 0666)
 }
 
-func (f *flags) New() error {
-   var err error
-   f.home, err = os.UserHomeDir()
-   if err != nil {
-      return err
-   }
-   f.home = filepath.ToSlash(f.home)
-   f.s.ClientId = f.home + "/widevine/client_id.bin"
-   f.s.PrivateKey = f.home + "/widevine/private_key.pem"
-   return nil
-}
-
-func main() {
-   var f flags
-   err := f.New()
-   if err != nil {
-      panic(err)
-   }
-   flag.Var(&f.entity, "a", "address")
-   flag.StringVar(&f.email, "e", "", "email")
-   flag.StringVar(&f.representation, "i", "", "representation")
-   flag.StringVar(&f.password, "p", "", "password")
-   flag.TextVar(&f.log.Level, "v", f.log.Level, "level")
-   flag.StringVar(&f.s.ClientId, "c", f.s.ClientId, "client ID")
-   flag.StringVar(&f.s.PrivateKey, "k", f.s.PrivateKey, "private key")
-   flag.Parse()
-   f.log.Set()
-   f.log.SetTransport(true)
-   switch {
-   case f.password != "":
-      err := f.authenticate()
-      if err != nil {
-         panic(err)
-      }
-   case f.entity.String() != "":
-      err := f.download()
-      if err != nil {
-         panic(err)
-      }
-   default:
-      flag.Usage()
-   }
-}
 func (f flags) download() error {
    var (
       auth rtbf.Authenticate
@@ -108,13 +68,4 @@ func (f flags) download() error {
       fmt.Println(medium)
    }
    return nil
-}
-
-func (f flags) authenticate() error {
-   var auth rtbf.Authenticate
-   err := auth.New(f.email, f.password)
-   if err != nil {
-      return err
-   }
-   return os.WriteFile(f.home + "/rtbf.json", auth.Data, 0666)
 }
--- rtbf/rtbf.go
+++ rtbf/rtbf.go
@@ -1,211 +1,211 @@
 package rtbf
 
 import (
-   "bytes"
-   "encoding/json"
-   "errors"
-   "net/http"
-   "net/url"
-   "strings"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 func (g gigya_login) entitlement(page *auvio_page) (*entitlement, error) {
-   req, err := http.NewRequest("", "https://exposure.api.redbee.live", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = func() string {
-      var b strings.Builder
-      b.WriteString("/v2/customer/RTBF/businessunit/Auvio/entitlement/")
-      b.WriteString(page.Content.AssetId)
-      b.WriteString("/play")
-      return b.String()
-   }()
-   req.Header = http.Header{
-      "x-forwarded-for": {"91.90.123.17"},
-      "authorization": {"Bearer " + g.SessionToken},
-   }
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      var b strings.Builder
-      res.Write(&b)
-      return nil, errors.New(b.String())
-   }
-   title := new(entitlement)
-   err = json.NewDecoder(res.Body).Decode(title)
-   if err != nil {
-      return nil, err
-   }
-   return title, nil
+	req, err := http.NewRequest("", "https://exposure.api.redbee.live", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.URL.Path = func() string {
+		var b strings.Builder
+		b.WriteString("/v2/customer/RTBF/businessunit/Auvio/entitlement/")
+		b.WriteString(page.Content.AssetId)
+		b.WriteString("/play")
+		return b.String()
+	}()
+	req.Header = http.Header{
+		"x-forwarded-for": {"91.90.123.17"},
+		"authorization":   {"Bearer " + g.SessionToken},
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		var b strings.Builder
+		res.Write(&b)
+		return nil, errors.New(b.String())
+	}
+	title := new(entitlement)
+	err = json.NewDecoder(res.Body).Decode(title)
+	if err != nil {
+		return nil, err
+	}
+	return title, nil
 }
 
 type entitlement struct {
-   AssetId string
-   PlayToken string
-   Formats []struct {
-      Format string
-      MediaLocator string
-   }
+	AssetId   string
+	PlayToken string
+	Formats   []struct {
+		Format       string
+		MediaLocator string
+	}
 }
 
 func (e entitlement) dash() (string, bool) {
-   for _, format := range e.Formats {
-      if format.Format == "DASH" {
-         return format.MediaLocator, true
-      }
-   }
-   return "", false
+	for _, format := range e.Formats {
+		if format.Format == "DASH" {
+			return format.MediaLocator, true
+		}
+	}
+	return "", false
 }
 
 func (e entitlement) RequestUrl() (string, bool) {
-   var u url.URL
-   u.Host = "rbm-rtbf.live.ott.irdeto.com"
-   u.Path = "/licenseServer/widevine/v1/rbm-rtbf/license"
-   u.Scheme = "https"
-   u.RawQuery = url.Values{
-      "contentId": {e.AssetId},
-      "ls_session": {e.PlayToken},
-   }.Encode()
-   return u.String(), true
+	var u url.URL
+	u.Host = "rbm-rtbf.live.ott.irdeto.com"
+	u.Path = "/licenseServer/widevine/v1/rbm-rtbf/license"
+	u.Scheme = "https"
+	u.RawQuery = url.Values{
+		"contentId":  {e.AssetId},
+		"ls_session": {e.PlayToken},
+	}.Encode()
+	return u.String(), true
 }
 
 func (entitlement) RequestHeader() (http.Header, error) {
-   h := make(http.Header)
-   h.Set("content-type", "application/x-protobuf")
-   return h, nil
+	h := make(http.Header)
+	h.Set("content-type", "application/x-protobuf")
+	return h, nil
 }
 
 func (entitlement) WrapRequest(b []byte) ([]byte, error) {
-   return b, nil
+	return b, nil
 }
 
 func (entitlement) UnwrapResponse(b []byte) ([]byte, error) {
-   return b, nil
+	return b, nil
 }
 
 // hard coded in JavaScript
 const api_key = "4_Ml_fJ47GnBAW6FrPzMxh0w"
 
-func (a *accounts_login) New(id, password string) error {
-   body := url.Values{
-      "APIKey": {api_key},
-      "loginID": {id},
-      "password": {password},
-   }.Encode()
-   req, err := http.NewRequest(
-      "POST", "https://login.auvio.rtbf.be/accounts.login",
-      strings.NewReader(body),
-   )
-   if err != nil {
-      return err
-   }
-   req.Header.Set("content-type", "application/x-www-form-urlencoded")
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   err = json.NewDecoder(res.Body).Decode(a)
-   if err != nil {
-      return err
-   }
-   if v := a.ErrorMessage; v != "" {
-      return errors.New(v)
-   }
-   return nil
+func (a *AccountLogin) New(id, password string) error {
+	body := url.Values{
+		"APIKey":   {api_key},
+		"loginID":  {id},
+		"password": {password},
+	}.Encode()
+	req, err := http.NewRequest(
+		"POST", "https://login.auvio.rtbf.be/accounts.login",
+		strings.NewReader(body),
+	)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("content-type", "application/x-www-form-urlencoded")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	err = json.NewDecoder(res.Body).Decode(a)
+	if err != nil {
+		return err
+	}
+	if v := a.ErrorMessage; v != "" {
+		return errors.New(v)
+	}
+	return nil
 }
 
-type accounts_login struct {
-   ErrorMessage string
-   SessionInfo struct {
-      CookieValue string
-   }
+type AccountLogin struct {
+	ErrorMessage string
+	SessionInfo  struct {
+		CookieValue string
+	}
 }
 
-func (a *accounts_login) unmarshal(text []byte) error {
-   return json.Unmarshal(text, a)
+func (a *AccountLogin) unmarshal(text []byte) error {
+	return json.Unmarshal(text, a)
 }
 
-func (a accounts_login) marshal() ([]byte, error) {
-   return json.Marshal(a)
+func (a AccountLogin) marshal() ([]byte, error) {
+	return json.Marshal(a)
 }
 
-func (a accounts_login) token() (*web_token, error) {
-   body := url.Values{
-      "APIKey": {api_key},
-      // from /accounts.login
-      "login_token": {a.SessionInfo.CookieValue},
-   }.Encode()
-   req, err := http.NewRequest(
-      "POST", "https://login.auvio.rtbf.be/accounts.getJWT",
-      strings.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("content-type", "application/x-www-form-urlencoded")
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   var web web_token
-   err = json.NewDecoder(res.Body).Decode(&web)
-   if err != nil {
-      return nil, err
-   }
-   if v := web.ErrorMessage; v != "" {
-      return nil, errors.New(v)
-   }
-   return &web, nil
+func (a AccountLogin) token() (*web_token, error) {
+	body := url.Values{
+		"APIKey": {api_key},
+		// from /accounts.login
+		"login_token": {a.SessionInfo.CookieValue},
+	}.Encode()
+	req, err := http.NewRequest(
+		"POST", "https://login.auvio.rtbf.be/accounts.getJWT",
+		strings.NewReader(body),
+	)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("content-type", "application/x-www-form-urlencoded")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var web web_token
+	err = json.NewDecoder(res.Body).Decode(&web)
+	if err != nil {
+		return nil, err
+	}
+	if v := web.ErrorMessage; v != "" {
+		return nil, errors.New(v)
+	}
+	return &web, nil
 }
 
 type gigya_login struct {
-   SessionToken string
+	SessionToken string
 }
 
 type web_token struct {
-   ErrorMessage string
-   IdToken string `json:"id_token"`
+	ErrorMessage string
+	IdToken      string `json:"id_token"`
 }
 
 func (w web_token) login() (*gigya_login, error) {
-   body, err := func() ([]byte, error) {
-      var s struct {
-         Device struct {
-            DeviceId string `json:"deviceId"`
-            Type string `json:"type"`
-         } `json:"device"`
-         JWT string `json:"jwt"`
-      }
-      s.Device.Type = "WEB"
-      s.JWT = w.IdToken
-      return json.Marshal(s)
-   }()
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://exposure.api.redbee.live", bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = "/v2/customer/RTBF/businessunit/Auvio/auth/gigyaLogin"
-   req.Header.Set("content-type", "application/json")
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   login := new(gigya_login)
-   err = json.NewDecoder(res.Body).Decode(login)
-   if err != nil {
-      return nil, err
-   }
-   return login, nil
+	body, err := func() ([]byte, error) {
+		var s struct {
+			Device struct {
+				DeviceId string `json:"deviceId"`
+				Type     string `json:"type"`
+			} `json:"device"`
+			JWT string `json:"jwt"`
+		}
+		s.Device.Type = "WEB"
+		s.JWT = w.IdToken
+		return json.Marshal(s)
+	}()
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(
+		"POST", "https://exposure.api.redbee.live", bytes.NewReader(body),
+	)
+	if err != nil {
+		return nil, err
+	}
+	req.URL.Path = "/v2/customer/RTBF/businessunit/Auvio/auth/gigyaLogin"
+	req.Header.Set("content-type", "application/json")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	login := new(gigya_login)
+	err = json.NewDecoder(res.Body).Decode(login)
+	if err != nil {
+		return nil, err
+	}
+	return login, nil
 }
