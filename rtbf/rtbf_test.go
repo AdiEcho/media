d2e479e8623717bd826a915f2ae281c63a99cfee
3052 <spen+3052@protonmail.com>
2024-06-08 17:43:44 -0500

    modified:   rtbf/rtbf_test.go

--- rtbf/rtbf_test.go
+++ rtbf/rtbf_test.go
@@ -1,129 +1,128 @@
 package rtbf
 
 import (
-	"154.pages.dev/widevine"
-	"encoding/base64"
-	"fmt"
-	"os"
-	"testing"
+   "154.pages.dev/widevine"
+   "encoding/base64"
+   "fmt"
+   "os"
+   "testing"
 )
 
 func TestWidevine(t *testing.T) {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		t.Fatal(err)
-	}
-	private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
-	if err != nil {
-		t.Fatal(err)
-	}
-	client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
-	if err != nil {
-		t.Fatal(err)
-	}
-	medium := media[0]
-	key_id, err := base64.StdEncoding.DecodeString(medium.key_id)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var module widevine.CDM
-	err = module.New(private_key, client_id, widevine.PSSH(key_id, nil))
-	if err != nil {
-		t.Fatal(err)
-	}
-	text, err := os.ReadFile("account.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	var account AccountLogin
-	err = account.Unmarshal(text)
-	if err != nil {
-		t.Fatal(err)
-	}
-	token, err := account.Token()
-	if err != nil {
-		t.Fatal(err)
-	}
-	gigya, err := token.Login()
-	if err != nil {
-		t.Fatal(err)
-	}
-	page, err := NewPage(medium.path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	title, err := gigya.Entitlement(page)
-	if err != nil {
-		t.Fatal(err)
-	}
-	key, err := module.Key(title, key_id)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("%x\n", key)
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   medium := media[0]
+   key_id, err := base64.StdEncoding.DecodeString(medium.key_id)
+   if err != nil {
+      t.Fatal(err)
+   }
+   var module widevine.CDM
+   err = module.New(private_key, client_id, widevine.PSSH(key_id, nil))
+   if err != nil {
+      t.Fatal(err)
+   }
+   text, err := os.ReadFile("account.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var account AccountLogin
+   err = account.Unmarshal(text)
+   if err != nil {
+      t.Fatal(err)
+   }
+   token, err := account.Token()
+   if err != nil {
+      t.Fatal(err)
+   }
+   gigya, err := token.Login()
+   if err != nil {
+      t.Fatal(err)
+   }
+   page, err := NewPage(medium.path)
+   if err != nil {
+      t.Fatal(err)
+   }
+   title, err := gigya.Entitlement(page)
+   if err != nil {
+      t.Fatal(err)
+   }
+   key, err := module.Key(title, key_id)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
 }
 
 func TestWebToken(t *testing.T) {
-	text, err := os.ReadFile("account.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	var account AccountLogin
-	err = account.Unmarshal(text)
-	if err != nil {
-		t.Fatal(err)
-	}
-	token, err := account.Token()
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("%+v\n", token)
+   text, err := os.ReadFile("account.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var account AccountLogin
+   err = account.Unmarshal(text)
+   if err != nil {
+      t.Fatal(err)
+   }
+   token, err := account.Token()
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", token)
 }
 func TestEntitlement(t *testing.T) {
-	text, err := os.ReadFile("account.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	var account AccountLogin
-	err = account.Unmarshal(text)
-	if err != nil {
-		t.Fatal(err)
-	}
-	token, err := account.Token()
-	if err != nil {
-		t.Fatal(err)
-	}
-	gigya, err := token.Login()
-	if err != nil {
-		t.Fatal(err)
-	}
-	page, err := NewPage(media[0].path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	title, err := gigya.Entitlement(page)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("%+v\n", title)
-	fmt.Println(title.DASH())
+   text, err := os.ReadFile("account.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var account AccountLogin
+   err = account.Unmarshal(text)
+   if err != nil {
+      t.Fatal(err)
+   }
+   token, err := account.Token()
+   if err != nil {
+      t.Fatal(err)
+   }
+   gigya, err := token.Login()
+   if err != nil {
+      t.Fatal(err)
+   }
+   page, err := NewPage(media[0].path)
+   if err != nil {
+      t.Fatal(err)
+   }
+   title, err := gigya.Entitlement(page)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", title)
+   fmt.Println(title.DASH())
 }
 
 func TestAccountsLogin(t *testing.T) {
-	username := os.Getenv("rtbf_username")
-	if username == "" {
-		t.Fatal("Getenv")
-	}
-	password := os.Getenv("rtbf_password")
-	var login AccountLogin
-	err := login.New(username, password)
-	if err != nil {
-		t.Fatal(err)
-	}
-	text, err := login.Marshal()
-	if err != nil {
-		t.Fatal(err)
-	}
-	os.WriteFile("account.json", text, 0666)
+   username := os.Getenv("rtbf_username")
+   if username == "" {
+      t.Fatal("Getenv")
+   }
+   password := os.Getenv("rtbf_password")
+   var login AccountLogin
+   err := login.New(username, password)
+   if err != nil {
+      t.Fatal(err)
+   }
+   text, err := login.Marshal()
+   if err != nil {
+      t.Fatal(err)
+   }
+   os.WriteFile("account.json", text, 0666)
 }
-
