f4e9475725c6e371240e4063a0a1e2840611f440
3052 <spen+3052@protonmail.com>
2024-06-11 21:15:23 -0500

    +func (st *st_cookie) login(key public_key, login default_login) error {

--- blog/max/arkose.go
+++ blog/max/arkose.go
@@ -12,22 +12,19 @@ import (
    "time"
 )
 
-func (r login_request) login(
-   key public_key,
-   token default_token,
-) (*login_response, error) {
-   body, err := json.Marshal(r)
+func (st *st_cookie) login(key public_key, login default_login) error {
+   body, err := json.Marshal(login)
    if err != nil {
-      return nil, err
+      return err
    }
    req, err := http.NewRequest(
       "POST", "https://default.any-amer.prd.api.max.com/login",
       bytes.NewReader(body),
    )
    if err != nil {
-      return nil, err
+      return err
    }
-   req.AddCookie(token.cookie)
+   req.AddCookie(st.cookie)
    req.Header.Set("content-type", "application/json")
    req.Header.Set("x-disco-arkose-token", key.Token)
    req.Header.Set("x-disco-client-id", func() string {
@@ -39,84 +36,20 @@ func (r login_request) login(
    }())
    resp, err := http.DefaultClient.Do(req)
    if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   login := new(login_response)
-   err = json.NewDecoder(resp.Body).Decode(login)
-   if err != nil {
-      return nil, err
-   }
-   return login, nil
-}
-
-const arkose_site_key = "B0217B00-2CA4-41CC-925D-1EEB57BFFC2F"
-
-func (d default_token) config() (*key_config, error) {
-   body, err := json.Marshal(map[string]string{
-      "projectId": "67e7aa0f-b186-4b85-9cb0-86d40a23636c",
-   })
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://default.beam-any.prd.api.max.com", bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = "/labs/api/v1/sessions/feature-flags/decisions"
-   req.AddCookie(d.cookie)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var decision struct {
-      HmacKeys struct {
-         Config struct {
-            Web key_config
-         }
-      }
-   }
-   err = json.NewDecoder(resp.Body).Decode(&decision)
-   if err != nil {
-      return nil, err
-   }
-   return &decision.HmacKeys.Config.Web, nil
-}
-
-type default_token struct {
-   cookie *http.Cookie
-}
-
-func (d *default_token) New() error {
-   req, err := http.NewRequest(
-      "", "https://default.any-any.prd.api.max.com/token?realm=bolt", nil,
-   )
-   if err != nil {
       return err
    }
-   req.Header.Set(
-      "x-device-info", "beam/4.0.1 (desktop/desktop; Windows/10; !/!)",
-   )
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      var b bytes.Buffer
-      res.Write(&b)
-      return errors.New(b.String())
-   }
-   for _, cookie := range res.Cookies() {
+   defer resp.Body.Close()
+   for _, cookie := range resp.Cookies() {
       if cookie.Name == "st" {
-         d.cookie = cookie
+         st.cookie = cookie
          return nil
       }
    }
    return http.ErrNoCookie
+}
+
+type st_cookie struct {
+   cookie *http.Cookie
 }
 
 type key_config struct {
@@ -147,17 +80,74 @@ func (p *public_key) New() error {
    return json.NewDecoder(resp.Body).Decode(p)
 }
 
-type login_request struct {
+const arkose_site_key = "B0217B00-2CA4-41CC-925D-1EEB57BFFC2F"
+
+func (st *st_cookie) New() error {
+   req, err := http.NewRequest(
+      "", "https://default.any-any.prd.api.max.com/token?realm=bolt", nil,
+   )
+   if err != nil {
+      return err
+   }
+   req.Header.Set(
+      "x-device-info", "beam/4.0.1 (desktop/desktop; Windows/10; !/!)",
+   )
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      var b bytes.Buffer
+      resp.Write(&b)
+      return errors.New(b.String())
+   }
+   for _, cookie := range resp.Cookies() {
+      if cookie.Name == "st" {
+         st.cookie = cookie
+         return nil
+      }
+   }
+   return http.ErrNoCookie
+}
+
+func (st st_cookie) config() (*key_config, error) {
+   body, err := json.Marshal(map[string]string{
+      "projectId": "67e7aa0f-b186-4b85-9cb0-86d40a23636c",
+   })
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://default.beam-any.prd.api.max.com", bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Path = "/labs/api/v1/sessions/feature-flags/decisions"
+   req.AddCookie(st.cookie)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   var decision struct {
+      HmacKeys struct {
+         Config struct {
+            Web key_config
+         }
+      }
+   }
+   err = json.NewDecoder(resp.Body).Decode(&decision)
+   if err != nil {
+      return nil, err
+   }
+   return &decision.HmacKeys.Config.Web, nil
+}
+
+type default_login struct {
    Credentials struct {
       Username string `json:"username"`
       Password string `json:"password"`
    } `json:"credentials"`
-}
-
-type login_response struct {
-   Data struct {
-      Attributes struct {
-         Token string
-      }
-   }
 }
--- blog/max/playback.go
+++ blog/max/playback.go
@@ -1,11 +1,29 @@
 package max
 
 import (
-   "io"
+   "bytes"
+   "encoding/json"
    "net/http"
-   "net/url"
-   "strings"
 )
+
+func playback(p playback_request) (*http.Response, error) {
+   body, err := json.Marshal(p)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://default.any-any.prd.api.max.com", bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Path = "/any/playback/v1/playbackInfo"
+   req.Header["Content-Type"] = []string{"application/json"}
+   req.Header["Cookie"] = []string{
+      "st=eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJ0b2tlbi1iODRmOTIxMy0yMzA0LTQ4MGEtOGEzMy1lOTViMTNhOGFiZjgiLCJpc3MiOiJmcGEtaXNzdWVyIiwic3ViIjoiVVNFUklEOmJvbHQ6Y2YwMWI0ZDItZDIyNS00Njc4LThkOTItOGU0NTg1MDhkN2U4IiwiaWF0IjoxNzE3OTcwODYxLCJleHAiOjIwMzMzMzA4NjEsInR5cGUiOiJBQ0NFU1NfVE9LRU4iLCJzdWJkaXZpc2lvbiI6ImJlYW1fYW1lciIsInNjb3BlIjoiZGVmYXVsdCIsInZlcnNpb24iOiJ2MiIsImFub255bW91cyI6ZmFsc2UsImRldmljZUlkIjoiMDQxMzdhYTItMWUxZS02ZjUyLTdhMDgtMTIyNDljODY0NjkwIn0.adU124rWw6-B55slVSnAn7gyd6wJA8sdWv-c2ayXkdrlGmXSRIosAnxf582ABO2ZCmguG0Lbm2S2ZlKMuRSwdT-QXfG8-EFW4LAaawiMc3xKuRn-uUmMCAhaewg_4TauEFdpAPDXAFOdO_wItNt7MoN1nQaW8C1Sa7jJzDpQhCDqv8DfEeZYfx_jQopnVyw6vUmz_W4m52wJAlmh_kW2fCJuUahywMKRHSBOBriBm1LL51gIOIcFxfLM3G6f-yb_ar9xqFqSIyaSpQ5Wj1t2T3IQPNh8gjrPw0O6A2zGo91S4reQHDB18kc-IzCFfGA2scGbLWZE7rcUpeZjK6eRiQ",
+   }
+   return http.DefaultClient.Do(req)
+}
 
 func (p *playback_request) New() {
    p.AppBundle = "beam"
@@ -20,26 +38,6 @@ func (p *playback_request) New() {
    p.EditId = "1623fe4c-ef6e-4dd1-a10c-4a181f5f6579"
    p.FirstPlay = true
    p.PlaybackSessionId = "6ccb51f6-5cd6-4ce0-9ade-7f8a47e66474"
-}
-
-func (r login_response) playback(p playback_request) (*http.Response, error) {
-   body, err := json.Marshal(p)
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://default.any-any.prd.api.max.com", bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = "/any/playback/v1/playbackInfo"
-   
-   req.Header["Content-Type"] = []string{"application/json"}
-   req.Header["Cookie"] = []string{
-      "st=eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJ0b2tlbi1iODRmOTIxMy0yMzA0LTQ4MGEtOGEzMy1lOTViMTNhOGFiZjgiLCJpc3MiOiJmcGEtaXNzdWVyIiwic3ViIjoiVVNFUklEOmJvbHQ6Y2YwMWI0ZDItZDIyNS00Njc4LThkOTItOGU0NTg1MDhkN2U4IiwiaWF0IjoxNzE3OTcwODYxLCJleHAiOjIwMzMzMzA4NjEsInR5cGUiOiJBQ0NFU1NfVE9LRU4iLCJzdWJkaXZpc2lvbiI6ImJlYW1fYW1lciIsInNjb3BlIjoiZGVmYXVsdCIsInZlcnNpb24iOiJ2MiIsImFub255bW91cyI6ZmFsc2UsImRldmljZUlkIjoiMDQxMzdhYTItMWUxZS02ZjUyLTdhMDgtMTIyNDljODY0NjkwIn0.adU124rWw6-B55slVSnAn7gyd6wJA8sdWv-c2ayXkdrlGmXSRIosAnxf582ABO2ZCmguG0Lbm2S2ZlKMuRSwdT-QXfG8-EFW4LAaawiMc3xKuRn-uUmMCAhaewg_4TauEFdpAPDXAFOdO_wItNt7MoN1nQaW8C1Sa7jJzDpQhCDqv8DfEeZYfx_jQopnVyw6vUmz_W4m52wJAlmh_kW2fCJuUahywMKRHSBOBriBm1LL51gIOIcFxfLM3G6f-yb_ar9xqFqSIyaSpQ5Wj1t2T3IQPNh8gjrPw0O6A2zGo91S4reQHDB18kc-IzCFfGA2scGbLWZE7rcUpeZjK6eRiQ",
-   }
-   return http.DefaultClient.Do(&req)
 }
 
 type playback_request struct {
