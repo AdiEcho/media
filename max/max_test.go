70c13f38cc36676cf95d84fe6626c27b30d813e7
3052 <spen+3052@protonmail.com>
2024-06-16 17:40:30 -0500

    modified:   max/token.go

--- max/max_test.go
+++ max/max_test.go
@@ -1,92 +1,92 @@
 package max
 
 import (
-	"154.pages.dev/text"
-	"154.pages.dev/widevine"
-	"encoding/hex"
-	"fmt"
-	"os"
-	"testing"
-	"time"
+   "154.pages.dev/text"
+   "154.pages.dev/widevine"
+   "encoding/hex"
+   "fmt"
+   "os"
+   "testing"
+   "time"
 )
 
 func TestLicense(t *testing.T) {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		t.Fatal(err)
-	}
-	private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
-	if err != nil {
-		t.Fatal(err)
-	}
-	client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
-	if err != nil {
-		t.Fatal(err)
-	}
-	var pssh widevine.PSSH
-	pssh.KeyId, err = hex.DecodeString(default_kid)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var module widevine.CDM
-	err = module.New(private_key, client_id, pssh.Encode())
-	if err != nil {
-		t.Fatal(err)
-	}
-	text, err := os.ReadFile("token.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	var token DefaultToken
-	token.Unmarshal(text)
-	var web WebAddress
-	web.UnmarshalText([]byte(tests[0].url))
-	play, err := token.Playback(web)
-	if err != nil {
-		t.Fatal(err)
-	}
-	key, err := module.Key(play, pssh.KeyId)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("%x\n", key)
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var pssh widevine.PSSH
+   pssh.KeyId, err = hex.DecodeString(default_kid)
+   if err != nil {
+      t.Fatal(err)
+   }
+   var module widevine.CDM
+   err = module.New(private_key, client_id, pssh.Encode())
+   if err != nil {
+      t.Fatal(err)
+   }
+   text, err := os.ReadFile("token.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var token DefaultToken
+   token.Unmarshal(text)
+   var web WebAddress
+   web.UnmarshalText([]byte(tests[0].url))
+   play, err := token.Playback(web)
+   if err != nil {
+      t.Fatal(err)
+   }
+   key, err := module.Key(play, pssh.KeyId)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
 }
 
 const default_kid = "01021e5f16aa2c5ed02c550139b5ab82"
 
 var tests = []struct {
-	video_type string
-	url        string
+   video_type string
+   url        string
 }{
-	{
-		url:        "play.max.com/video/watch/b3b1410a-0c85-457b-bcc7-e13299bea2a8/1623fe4c-ef6e-4dd1-a10c-4a181f5f6579",
-		video_type: "MOVIE",
-	},
-	{
-		url:        "play.max.com/video/watch/d0938760-d3ca-4c59-aea2-74ecbed42d17/2e7d1db4-2fd7-47fb-a7c3-a65b7c2e5d6f",
-		video_type: "EPISODE",
-	},
+   {
+      url:        "play.max.com/video/watch/b3b1410a-0c85-457b-bcc7-e13299bea2a8/1623fe4c-ef6e-4dd1-a10c-4a181f5f6579",
+      video_type: "MOVIE",
+   },
+   {
+      url:        "play.max.com/video/watch/d0938760-d3ca-4c59-aea2-74ecbed42d17/2e7d1db4-2fd7-47fb-a7c3-a65b7c2e5d6f",
+      video_type: "EPISODE",
+   },
 }
 
 func TestRoutes(t *testing.T) {
-	var token DefaultToken
-	err := token.New()
-	if err != nil {
-		t.Fatal(err)
-	}
-	for _, test := range tests {
-		var web WebAddress
-		web.UnmarshalText([]byte(test.url))
-		routes, err := token.Routes(web)
-		if err != nil {
-			t.Fatal(err)
-		}
-		fmt.Printf("%+v\n", routes)
-		name, err := text.Name(routes)
-		if err != nil {
-			t.Fatal(err)
-		}
-		fmt.Printf("%q\n", name)
-		time.Sleep(time.Second)
-	}
+   var token DefaultToken
+   err := token.New()
+   if err != nil {
+      t.Fatal(err)
+   }
+   for _, test := range tests {
+      var web WebAddress
+      web.UnmarshalText([]byte(test.url))
+      routes, err := token.Routes(web)
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%+v\n", routes)
+      name, err := text.Name(routes)
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%q\n", name)
+      time.Sleep(time.Second)
+   }
 }
--- max/token.go
+++ max/token.go
@@ -1,152 +1,152 @@
 package max
 
 import (
-	"bytes"
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"net/http"
-	"time"
+   "bytes"
+   "crypto/hmac"
+   "crypto/sha256"
+   "encoding/json"
+   "errors"
+   "fmt"
+   "net/http"
+   "time"
 )
 
 func (d *DefaultToken) Unmarshal(text []byte) error {
-	return json.Unmarshal(text, d)
+   return json.Unmarshal(text, d)
 }
 
 const home_market = "amer"
 
 // note you can use other keys, but you need to change home_market to match
 var default_key = hmac_key{
-	Id:  "android1_prd",
-	Key: []byte("6fd2c4b9-7b43-49ee-a62e-57ffd7bdfe9c"),
+   Id:  "android1_prd",
+   Key: []byte("6fd2c4b9-7b43-49ee-a62e-57ffd7bdfe9c"),
 }
 
 type default_decision struct {
-	HmacKeys struct {
-		Config struct {
-			Android   *hmac_key
-			AndroidTv *hmac_key
-			FireTv    *hmac_key
-			Hwa       *hmac_key
-			Ios       *hmac_key
-			TvOs      *hmac_key
-			Web       *hmac_key
-		}
-	}
+   HmacKeys struct {
+      Config struct {
+         Android   *hmac_key
+         AndroidTv *hmac_key
+         FireTv    *hmac_key
+         Hwa       *hmac_key
+         Ios       *hmac_key
+         TvOs      *hmac_key
+         Web       *hmac_key
+      }
+   }
 }
 
 func (d *DefaultToken) Login(key PublicKey, login DefaultLogin) error {
-	address := func() string {
-		var b bytes.Buffer
-		b.WriteString("https://default.any-")
-		b.WriteString(home_market)
-		b.WriteString(".prd.api.discomax.com/login")
-		return b.String()
-	}()
-	body, err := json.Marshal(login)
-	if err != nil {
-		return err
-	}
-	req, err := http.NewRequest("POST", address, bytes.NewReader(body))
-	if err != nil {
-		return err
-	}
-	req.Header.Set("content-type", "application/json")
-	req.Header.Set("x-disco-arkose-token", key.Token)
-	req.Header.Set("authorization", "Bearer "+d.Data.Attributes.Token)
-	req.Header.Set("x-disco-client-id", func() string {
-		timestamp := time.Now().Unix()
-		hash := hmac.New(sha256.New, default_key.Key)
-		fmt.Fprintf(hash, "%v:POST:/login:%s", timestamp, body)
-		signature := hash.Sum(nil)
-		return fmt.Sprintf("%v:%v:%x", default_key.Id, timestamp, signature)
-	}())
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		var b bytes.Buffer
-		resp.Write(&b)
-		return errors.New(b.String())
-	}
-	return json.NewDecoder(resp.Body).Decode(d)
+   address := func() string {
+      var b bytes.Buffer
+      b.WriteString("https://default.any-")
+      b.WriteString(home_market)
+      b.WriteString(".prd.api.discomax.com/login")
+      return b.String()
+   }()
+   body, err := json.Marshal(login)
+   if err != nil {
+      return err
+   }
+   req, err := http.NewRequest("POST", address, bytes.NewReader(body))
+   if err != nil {
+      return err
+   }
+   req.Header.Set("content-type", "application/json")
+   req.Header.Set("x-disco-arkose-token", key.Token)
+   req.Header.Set("authorization", "Bearer "+d.Data.Attributes.Token)
+   req.Header.Set("x-disco-client-id", func() string {
+      timestamp := time.Now().Unix()
+      hash := hmac.New(sha256.New, default_key.Key)
+      fmt.Fprintf(hash, "%v:POST:/login:%s", timestamp, body)
+      signature := hash.Sum(nil)
+      return fmt.Sprintf("%v:%v:%x", default_key.Id, timestamp, signature)
+   }())
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      var b bytes.Buffer
+      resp.Write(&b)
+      return errors.New(b.String())
+   }
+   return json.NewDecoder(resp.Body).Decode(d)
 }
 
 type hmac_key struct {
-	Id  string
-	Key []byte
+   Id  string
+   Key []byte
 }
 
 func (d DefaultToken) decision() (*default_decision, error) {
-	body, err := json.Marshal(map[string]string{
-		"projectId": "d8665e86-8706-415d-8d84-d55ceddccfb5",
-	})
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequest(
-		"POST", "https://default.any-any.prd.api.discomax.com",
-		bytes.NewReader(body),
-	)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("authorization", "Bearer "+d.Data.Attributes.Token)
-	req.URL.Path = "/labs/api/v1/sessions/feature-flags/decisions"
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	decision := new(default_decision)
-	err = json.NewDecoder(resp.Body).Decode(decision)
-	if err != nil {
-		return nil, err
-	}
-	return decision, nil
+   body, err := json.Marshal(map[string]string{
+      "projectId": "d8665e86-8706-415d-8d84-d55ceddccfb5",
+   })
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://default.any-any.prd.api.discomax.com",
+      bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("authorization", "Bearer "+d.Data.Attributes.Token)
+   req.URL.Path = "/labs/api/v1/sessions/feature-flags/decisions"
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   decision := new(default_decision)
+   err = json.NewDecoder(resp.Body).Decode(decision)
+   if err != nil {
+      return nil, err
+   }
+   return decision, nil
 }
 
 type DefaultLogin struct {
-	Credentials struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	} `json:"credentials"`
+   Credentials struct {
+      Username string `json:"username"`
+      Password string `json:"password"`
+   } `json:"credentials"`
 }
 
 func (d DefaultToken) Marshal() ([]byte, error) {
-	return json.MarshalIndent(d, "", " ")
+   return json.MarshalIndent(d, "", " ")
 }
 
 type DefaultToken struct {
-	Data struct {
-		Attributes struct {
-			Token string
-		}
-	}
+   Data struct {
+      Attributes struct {
+         Token string
+      }
+   }
 }
 
 func (d *DefaultToken) New() error {
-	req, err := http.NewRequest(
-		"", "https://default.any-any.prd.api.discomax.com/token?realm=bolt", nil,
-	)
-	if err != nil {
-		return err
-	}
-	// fuck you Max
-	req.Header.Set("x-device-info", "!/!(!/!;!/!;!)")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		var b bytes.Buffer
-		resp.Write(&b)
-		return errors.New(b.String())
-	}
-	return json.NewDecoder(resp.Body).Decode(d)
+   req, err := http.NewRequest(
+      "", "https://default.any-any.prd.api.discomax.com/token?realm=bolt", nil,
+   )
+   if err != nil {
+      return err
+   }
+   // fuck you Max
+   req.Header.Set("x-device-info", "!/!(!/!;!/!;!)")
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      var b bytes.Buffer
+      resp.Write(&b)
+      return errors.New(b.String())
+   }
+   return json.NewDecoder(resp.Body).Decode(d)
 }
