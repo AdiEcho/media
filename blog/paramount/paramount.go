5cc52d5ba97d4cff2de814558368d5ba672ebc04
3052 <spen+3052@protonmail.com>
2024-07-01 21:00:13 -0500

    +func (at AppToken) Session(content_id string) (*SessionToken, error) {

--- blog/paramount/paramount.go
+++ blog/paramount/paramount.go
@@ -15,6 +15,61 @@ import (
 )
 
 // hard geo block
+func (at AppToken) Session(content_id string) (*SessionToken, error) {
+   req, err := http.NewRequest("", "https://www.intl.paramountplus.com", nil)
+   if err != nil {
+      return nil, err
+   }
+   req.URL.RawQuery = at.v.Encode()
+   req.URL.Path = func() string {
+      var b strings.Builder
+      b.WriteString("/apps-api/v3.0/androidphone/irdeto-control")
+      b.WriteString("/anonymous-session-token.json")
+      return b.String()
+   }()
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      var b strings.Builder
+      resp.Write(&b)
+      return nil, errors.New(b.String())
+   }
+   session := new(SessionToken)
+   err = json.NewDecoder(resp.Body).Decode(session)
+   if err != nil {
+      return nil, err
+   }
+   session.Url += content_id
+   return session, nil
+}
+
+type SessionToken struct {
+   Url string
+   LsSession string `json:"ls_session"`
+}
+
+func (SessionToken) WrapRequest(b []byte) ([]byte, error) {
+   return b, nil
+}
+
+func (s SessionToken) RequestHeader() (http.Header, error) {
+   head := make(http.Header)
+   head.Set("authorization", "Bearer " + s.LsSession)
+   return head, nil
+}
+
+func (s SessionToken) RequestUrl() (string, bool) {
+   return s.Url, true
+}
+
+func (SessionToken) UnwrapResponse(b []byte) ([]byte, error) {
+   return b, nil
+}
+
+// hard geo block
 func location(content_id string, query url.Values) (string, error) {
    req, err := http.NewRequest("", "https://link.theplatform.com", nil)
    if err != nil {
@@ -154,50 +209,4 @@ func (at AppToken) Item(content_id string) (*VideoItem, error) {
       return nil, err
    }
    return &video.ItemList[0], nil
-}
-
-func (SessionToken) WrapRequest(b []byte) ([]byte, error) {
-   return b, nil
-}
-
-func (s SessionToken) RequestHeader() (http.Header, error) {
-   head := make(http.Header)
-   head.Set("authorization", "Bearer " + s.LsSession)
-   return head, nil
-}
-
-func (s SessionToken) RequestUrl() (string, bool) {
-   return s.Url, true
-}
-
-func (SessionToken) UnwrapResponse(b []byte) ([]byte, error) {
-   return b, nil
-}
-
-type SessionToken struct {
-   Url string
-   LsSession string `json:"ls_session"`
-}
-
-///////////////////////////////
-
-func (at AppToken) Session(content_id string) (*SessionToken, error) {
-   req, err := http.NewRequest("", "https://www.paramountplus.com", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = "/apps-api/v3.0/androidphone/irdeto-control/anonymous-session-token.json"
-   req.URL.RawQuery = at.v.Encode()
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   session := new(SessionToken)
-   err = json.NewDecoder(resp.Body).Decode(session)
-   if err != nil {
-      return nil, err
-   }
-   session.Url += content_id
-   return session, nil
 }
--- blog/paramount/paramount_test.go
+++ blog/paramount/paramount_test.go
@@ -1,9 +1,66 @@
 package paramount
 
 import (
+   "154.pages.dev/widevine"
+   "encoding/hex"
    "fmt"
+   "os"
    "testing"
 )
+
+func TestWidevine(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var pssh widevine.PSSH
+   pssh.KeyId, err = hex.DecodeString(test.key_id)
+   if err != nil {
+      t.Fatal(err)
+   }
+   var module widevine.CDM
+   err = module.New(private_key, client_id, pssh.Encode())
+   if err != nil {
+      t.Fatal(err)
+   }
+   var app AppToken
+   app.New()
+   session, err := app.Session(test.content_id)
+   if err != nil {
+      t.Fatal(err)
+   }
+   key, err := module.Key(session, pssh.KeyId)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
+}
+
+func TestMpd(t *testing.T) {
+   address, err := DashCenc(test.content_id)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Println(address)
+}
+
+var test = struct{
+   content_id string
+   key_id string
+   url string
+}{
+   content_id: "Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ",
+   key_id: "06c3b7eea1ce45779faee2abc8d01a55",
+   url: "paramountplus.com/movies/video/Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ",
+}
 
 func TestItem(t *testing.T) {
    var app AppToken
@@ -11,20 +68,9 @@ func TestItem(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   item, err := app.Item(content_id)
+   item, err := app.Item(test.content_id)
    if err != nil {
       t.Fatal(err)
    }
    fmt.Printf("%+v\n", item)
-}
-
-// paramountplus.com/movies/video/Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ
-const content_id = "Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ"
-
-func TestMpd(t *testing.T) {
-   address, err := DashCenc(content_id)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Println(address)
 }
