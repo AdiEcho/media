6d88bc40b517562a9ad987defb9dbbf206c0251a
3052 <spen+3052@protonmail.com>
2024-07-16 06:36:28 -0500

    +func (s Stream) segment_base(

--- internal/internal.go
+++ internal/internal.go
@@ -16,167 +16,6 @@ import (
    "strings"
 )
 
-func (s Stream) segment_base(
-   ext string, base *dash.BaseUrl, segment *dash.SegmentBase,
-) error {
-   data, _ := segment.Initialization.Range.MarshalText()
-   req := &http.Request{
-      Header: http.Header{
-         "Range": {"bytes=" + string(data)},
-      },
-      URL: base.Url,
-   }
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != http.StatusPartialContent {
-      return errors.New(resp.Status)
-   }
-   file, err := s.file(ext)
-   if err != nil {
-      return err
-   }
-   defer file.Close()
-   err = s.init_protect(file, resp.Body)
-   if err != nil {
-      return err
-   }
-   key, err := s.key()
-   if err != nil {
-      return err
-   }
-   references, err := write_sidx(req, segment.IndexRange)
-   if err != nil {
-      return err
-   }
-   var meter text.ProgressMeter
-   meter.Set(len(references))
-   var transport text.Transport
-   transport.Set(false)
-   defer transport.Set(true)
-   for _, reference := range references {
-      segment.IndexRange.Start = segment.IndexRange.End + 1
-      segment.IndexRange.End += uint64(reference.ReferencedSize())
-      data, _ := segment.IndexRange.MarshalText()
-      err := func() error {
-         req.Header.Set("Range", "bytes=" + string(data))
-         resp, err := http.DefaultClient.Do(req)
-         if err != nil {
-            return err
-         }
-         defer resp.Body.Close()
-         if resp.StatusCode != http.StatusPartialContent {
-            return errors.New(resp.Status)
-         }
-         return write_segment(file, meter.Reader(resp), key)
-      }()
-      if err != nil {
-         return err
-      }
-   }
-   return nil
-}
-
-func (s Stream) segment_template(
-   ext, initial string, base *dash.BaseUrl, media []string,
-) error {
-   req, err := http.NewRequest("", initial, nil)
-   if err != nil {
-      return err
-   }
-   req.URL = base.Url.ResolveReference(req.URL)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      return errors.New(resp.Status)
-   }
-   file, err := s.file(ext)
-   if err != nil {
-      return err
-   }
-   defer file.Close()
-   err = s.init_protect(file, resp.Body)
-   if err != nil {
-      return err
-   }
-   key, err := s.key()
-   if err != nil {
-      return err
-   }
-   client := http.Client{ // github.com/golang/go/issues/18639
-      Transport: &http.Transport{
-         Proxy: http.ProxyFromEnvironment,
-         TLSNextProto: map[string]func(string, *tls.Conn) http.RoundTripper{},
-      },
-   }
-   var meter text.ProgressMeter
-   meter.Set(len(media))
-   for _, medium := range media {
-      req.URL, err = base.Url.Parse(medium)
-      if err != nil {
-         return err
-      }
-      err := func() error {
-         resp, err := client.Do(req)
-         if err != nil {
-            return err
-         }
-         defer resp.Body.Close()
-         if resp.StatusCode != http.StatusOK {
-            var b strings.Builder
-            resp.Write(&b)
-            return errors.New(b.String())
-         }
-         return write_segment(file, meter.Reader(resp), key)
-      }()
-      if err != nil {
-         return err
-      }
-   }
-   return nil
-}
-
-func (s Stream) TimedText(url string) error {
-   resp, err := http.Get(url)
-   if err != nil {
-      return err
-   }
-   defer resp.Body.Close()
-   file, err := s.file(".vtt")
-   if err != nil {
-      return err
-   }
-   defer file.Close()
-   _, err = file.ReadFrom(resp.Body)
-   if err != nil {
-      return err
-   }
-   return nil
-}
-
-type ForwardedFor []struct {
-   Country string
-   IP string
-}
-
-func (f ForwardedFor) String() string {
-   var b strings.Builder
-   for _, each := range f {
-      if b.Len() >= 1 {
-         b.WriteByte('\n')
-      }
-      b.WriteString(each.Country)
-      b.WriteByte(' ')
-      b.WriteString(each.IP)
-   }
-   return b.String()
-}
-
 var Forward = ForwardedFor{
 {"Argentina", "186.128.0.0"},
 {"Australia", "1.128.0.0"},
@@ -380,4 +219,164 @@ func Dash(req *http.Request) ([]dash.Representation, error) {
       return nil, err
    }
    return dash.Unmarshal(data, resp.Request.URL)
+}
+func (s Stream) segment_base(
+   ext string, base *dash.BaseUrl, segment *dash.SegmentBase,
+) error {
+   data, _ := segment.Initialization.Range.MarshalText()
+   req := &http.Request{
+      Header: http.Header{
+         "Range": {"bytes=" + string(data)},
+      },
+      URL: base.Url,
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusPartialContent {
+      return errors.New(resp.Status)
+   }
+   file, err := s.file(ext)
+   if err != nil {
+      return err
+   }
+   defer file.Close()
+   err = s.init_protect(file, resp.Body)
+   if err != nil {
+      return err
+   }
+   key, err := s.key()
+   if err != nil {
+      return err
+   }
+   references, err := write_sidx(req, segment.IndexRange)
+   if err != nil {
+      return err
+   }
+   var meter text.ProgressMeter
+   meter.Set(len(references))
+   var transport text.Transport
+   transport.Set(false)
+   defer transport.Set(true)
+   for _, reference := range references {
+      segment.IndexRange.Start = segment.IndexRange.End + 1
+      segment.IndexRange.End += uint64(reference.ReferencedSize())
+      data, _ := segment.IndexRange.MarshalText()
+      err := func() error {
+         req.Header.Set("Range", "bytes=" + string(data))
+         resp, err := http.DefaultClient.Do(req)
+         if err != nil {
+            return err
+         }
+         defer resp.Body.Close()
+         if resp.StatusCode != http.StatusPartialContent {
+            return errors.New(resp.Status)
+         }
+         return write_segment(file, meter.Reader(resp), key)
+      }()
+      if err != nil {
+         return err
+      }
+   }
+   return nil
+}
+
+func (s Stream) segment_template(
+   ext, initial string, base *dash.BaseUrl, media []string,
+) error {
+   req, err := http.NewRequest("", initial, nil)
+   if err != nil {
+      return err
+   }
+   req.URL = base.Url.ResolveReference(req.URL)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return errors.New(resp.Status)
+   }
+   file, err := s.file(ext)
+   if err != nil {
+      return err
+   }
+   defer file.Close()
+   err = s.init_protect(file, resp.Body)
+   if err != nil {
+      return err
+   }
+   key, err := s.key()
+   if err != nil {
+      return err
+   }
+   client := http.Client{ // github.com/golang/go/issues/18639
+      Transport: &http.Transport{
+         Proxy: http.ProxyFromEnvironment,
+         TLSNextProto: map[string]func(string, *tls.Conn) http.RoundTripper{},
+      },
+   }
+   var meter text.ProgressMeter
+   meter.Set(len(media))
+   for _, medium := range media {
+      req.URL, err = base.Url.Parse(medium)
+      if err != nil {
+         return err
+      }
+      err := func() error {
+         resp, err := client.Do(req)
+         if err != nil {
+            return err
+         }
+         defer resp.Body.Close()
+         if resp.StatusCode != http.StatusOK {
+            var b strings.Builder
+            resp.Write(&b)
+            return errors.New(b.String())
+         }
+         return write_segment(file, meter.Reader(resp), key)
+      }()
+      if err != nil {
+         return err
+      }
+   }
+   return nil
+}
+
+func (s Stream) TimedText(url string) error {
+   resp, err := http.Get(url)
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   file, err := s.file(".vtt")
+   if err != nil {
+      return err
+   }
+   defer file.Close()
+   _, err = file.ReadFrom(resp.Body)
+   if err != nil {
+      return err
+   }
+   return nil
+}
+
+type ForwardedFor []struct {
+   Country string
+   IP string
+}
+
+func (f ForwardedFor) String() string {
+   var b strings.Builder
+   for _, each := range f {
+      if b.Len() >= 1 {
+         b.WriteByte('\n')
+      }
+      b.WriteString(each.Country)
+      b.WriteByte(' ')
+      b.WriteString(each.IP)
+   }
+   return b.String()
 }
