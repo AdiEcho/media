3d8bd057460d82f0132dfb01c6bf698ea7ae3e56
3052 <spen+3052@protonmail.com>
2024-07-27 18:34:13 -0500

    +func (o *OperationArticle) SetRaw(raw []byte) {

--- cine/member/article.go
+++ cine/member/article.go
@@ -8,52 +8,8 @@ import (
    "strconv"
 )
 
-func (a ArticleSlug) Article() (*OperationArticle, error) {
-   body, err := func() ([]byte, error) {
-      var s struct {
-         Query     string `json:"query"`
-         Variables struct {
-            ArticleUrlSlug ArticleSlug `json:"articleUrlSlug"`
-         } `json:"variables"`
-      }
-      s.Variables.ArticleUrlSlug = a
-      s.Query = query_article
-      return json.Marshal(s)
-   }()
-   if err != nil {
-      return nil, err
-   }
-   resp, err := http.Post(
-      "https://api.audienceplayer.com/graphql/2/user",
-      "application/json", bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var article OperationArticle
-   article.Data, err = io.ReadAll(resp.Body)
-   if err != nil {
-      return nil, err
-   }
-   return &article, nil
-}
-
-type OperationArticle struct {
-   Data []byte
-   v    *struct {
-      Data struct {
-         Article struct {
-            Assets         []*ArticleAsset
-            CanonicalTitle string `json:"canonical_title"`
-            Id             int
-            Metas          []struct {
-               Key   string
-               Value string
-            }
-         }
-      }
-   }
+func (o *OperationArticle) SetRaw(raw []byte) {
+   o.raw = raw
 }
 
 const query_article = `
@@ -92,23 +48,23 @@ func (OperationArticle) Show() string {
 }
 
 func (o *OperationArticle) Unmarshal() error {
-   o.v = pointer(o.v)
-   err := json.Unmarshal(o.Data, o.v)
+   o.Data = pointer(o.Data)
+   err := json.Unmarshal(o.raw, o)
    if err != nil {
       return err
    }
-   for _, asset := range o.v.Data.Article.Assets {
+   for _, asset := range o.Data.Article.Assets {
       asset.article = o
    }
    return nil
 }
 
 func (o OperationArticle) Title() string {
-   return o.v.Data.Article.CanonicalTitle
+   return o.Data.Article.CanonicalTitle
 }
 
 func (o OperationArticle) Film() (*ArticleAsset, bool) {
-   for _, asset := range o.v.Data.Article.Assets {
+   for _, asset := range o.Data.Article.Assets {
       if asset.LinkedType == "film" {
          return asset, true
       }
@@ -117,7 +73,7 @@ func (o OperationArticle) Film() (*ArticleAsset, bool) {
 }
 
 func (o OperationArticle) Year() int {
-   for _, meta := range o.v.Data.Article.Metas {
+   for _, meta := range o.Data.Article.Metas {
       if meta.Key == "year" {
          if v, err := strconv.Atoi(meta.Value); err == nil {
             return v
@@ -125,4 +81,50 @@ func (o OperationArticle) Year() int {
       }
    }
    return 0
+}
+
+func (a ArticleSlug) Article() (*OperationArticle, error) {
+   body, err := func() ([]byte, error) {
+      var s struct {
+         Query     string `json:"query"`
+         Variables struct {
+            ArticleUrlSlug ArticleSlug `json:"articleUrlSlug"`
+         } `json:"variables"`
+      }
+      s.Variables.ArticleUrlSlug = a
+      s.Query = query_article
+      return json.Marshal(s)
+   }()
+   if err != nil {
+      return nil, err
+   }
+   resp, err := http.Post(
+      "https://api.audienceplayer.com/graphql/2/user",
+      "application/json", bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   var article OperationArticle
+   article.raw, err = io.ReadAll(resp.Body)
+   if err != nil {
+      return nil, err
+   }
+   return &article, nil
+}
+
+type OperationArticle struct {
+   Data *struct {
+      Article struct {
+         Assets         []*ArticleAsset
+         CanonicalTitle string `json:"canonical_title"`
+         Id             int
+         Metas          []struct {
+            Key   string
+            Value string
+         }
+      }
+   }
+   raw []byte
 }
--- cine/member/member.go
+++ cine/member/member.go
@@ -8,89 +8,6 @@ import (
    "strings"
 )
 
-const query_play = `
-mutation($article_id: Int, $asset_id: Int) {
-   ArticleAssetPlay(article_id: $article_id asset_id: $asset_id) {
-      entitlements {
-         ... on ArticleAssetPlayEntitlement {
-            manifest
-            protocol
-         }
-      }
-   }
-}
-`
-
-// geo block, not x-forwarded-for
-func (o OperationUser) Play(asset *ArticleAsset) (*OperationPlay, error) {
-   body, err := func() ([]byte, error) {
-      var s struct {
-         Query     string `json:"query"`
-         Variables struct {
-            ArticleId int `json:"article_id"`
-            AssetId   int `json:"asset_id"`
-         } `json:"variables"`
-      }
-      s.Query = query_play
-      s.Variables.AssetId = asset.Id
-      s.Variables.ArticleId = asset.article.v.Data.Article.Id
-      return json.Marshal(s)
-   }()
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://api.audienceplayer.com/graphql/2/user",
-      bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "authorization": {"Bearer " + o.v.Data.UserAuthenticate.AccessToken},
-      "content-type":  {"application/json"},
-   }
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var play OperationPlay
-   play.Data, err = io.ReadAll(resp.Body)
-   if err != nil {
-      return nil, err
-   }
-   return &play, nil
-}
-
-func (o *OperationPlay) Unmarshal() error {
-   o.v = pointer(o.v)
-   return json.Unmarshal(o.Data, o.v)
-}
-
-type OperationPlay struct {
-   Data []byte
-   v    *struct {
-      Data struct {
-         ArticleAssetPlay struct {
-            Entitlements []struct {
-               Manifest string
-               Protocol string
-            }
-         }
-      }
-   }
-}
-
-func (o OperationPlay) Dash() (string, bool) {
-   for _, title := range o.v.Data.ArticleAssetPlay.Entitlements {
-      if title.Protocol == "dash" {
-         return title.Manifest, true
-      }
-   }
-   return "", false
-}
-
 const query_user = `
 mutation($email: String, $password: String) {
    UserAuthenticate(email: $email, password: $password) {
@@ -176,4 +93,85 @@ type OperationUser struct {
 func (o *OperationUser) Unmarshal() error {
    o.v = pointer(o.v)
    return json.Unmarshal(o.Data, o.v)
+}
+const query_play = `
+mutation($article_id: Int, $asset_id: Int) {
+   ArticleAssetPlay(article_id: $article_id asset_id: $asset_id) {
+      entitlements {
+         ... on ArticleAssetPlayEntitlement {
+            manifest
+            protocol
+         }
+      }
+   }
+}
+`
+type OperationPlay struct {
+   Data *struct {
+      ArticleAssetPlay struct {
+         Entitlements []struct {
+            Manifest string
+            Protocol string
+         }
+      }
+   }
+   raw []byte
+}
+
+/////////
+
+// geo block, not x-forwarded-for
+func (o OperationUser) Play(asset *ArticleAsset) (*OperationPlay, error) {
+   body, err := func() ([]byte, error) {
+      var s struct {
+         Query     string `json:"query"`
+         Variables struct {
+            ArticleId int `json:"article_id"`
+            AssetId   int `json:"asset_id"`
+         } `json:"variables"`
+      }
+      s.Query = query_play
+      s.Variables.AssetId = asset.Id
+      s.Variables.ArticleId = asset.article.Data.Article.Id
+      return json.Marshal(s)
+   }()
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://api.audienceplayer.com/graphql/2/user",
+      bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header = http.Header{
+      "authorization": {"Bearer " + o.v.Data.UserAuthenticate.AccessToken},
+      "content-type":  {"application/json"},
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   var play OperationPlay
+   play.Data, err = io.ReadAll(resp.Body)
+   if err != nil {
+      return nil, err
+   }
+   return &play, nil
+}
+
+func (o *OperationPlay) Unmarshal() error {
+   o.v = pointer(o.v)
+   return json.Unmarshal(o.Data, o.v)
+}
+
+func (o OperationPlay) Dash() (string, bool) {
+   for _, title := range o.v.Data.ArticleAssetPlay.Entitlements {
+      if title.Protocol == "dash" {
+         return title.Manifest, true
+      }
+   }
+   return "", false
 }
