b1bd7f5185437cda333fa29bcbd9a4365581b6d3
3052 <spen+3052@protonmail.com>
2024-07-28 09:59:33 -0500

    new file:   nbc/video.go

--- nbc/nbc.go
+++ nbc/nbc.go
@@ -1,11 +1,9 @@
 package nbc
 
 import (
-   "crypto/hmac"
-   "crypto/sha256"
+   "bytes"
    "encoding/json"
    "errors"
-   "fmt"
    "net/http"
    "net/url"
    "strconv"
@@ -13,19 +11,86 @@ import (
    "time"
 )
 
-func (v Video) RequestUrl() (string, bool) {
-   t, h := func() (int64, []byte) {
-      h := hmac.New(sha256.New, []byte(v.DrmProxySecret))
-      t := time.Now().UnixMilli()
-      fmt.Fprint(h, t, "widevine")
-      return t, h.Sum(nil)
+func NewMetadata(guid int) (*Metadata, error) {
+   body, err := func() ([]byte, error) {
+      var p page_request
+      p.Variables.Name = strconv.Itoa(guid)
+      p.Query = graphql_compact(query)
+      p.Variables.App = "nbc"
+      p.Variables.OneApp = true
+      p.Variables.Platform = "android"
+      p.Variables.Type = "VIDEO"
+      return json.Marshal(p)
    }()
-   b := []byte(v.DrmProxyUrl)
-   b = append(b, "/widevine"...)
-   b = fmt.Append(b, "?time=", t)
-   b = fmt.Appendf(b, "&hash=%x", h)
-   b = append(b, "&device=web"...)
-   return string(b), true
+   if err != nil {
+      return nil, err
+   }
+   resp, err := http.Post(
+      "https://friendship.nbc.co/v2/graphql", "application/json",
+      bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   var data struct {
+      Data struct {
+         BonanzaPage struct {
+            Metadata Metadata
+         }
+      }
+      Errors []struct {
+         Message string
+      }
+   }
+   err = json.NewDecoder(resp.Body).Decode(&data)
+   if err != nil {
+      return nil, err
+   }
+   if len(data.Errors) >= 1 {
+      return nil, errors.New(data.Errors[0].Message)
+   }
+   return &data.Data.BonanzaPage.Metadata, nil
+}
+
+func (m Metadata) Show() string {
+   return m.SeriesShortTitle
+}
+
+func (m Metadata) Season() int {
+   return m.SeasonNumber
+}
+
+func (m Metadata) Episode() int {
+   return m.EpisodeNumber
+}
+func (m Metadata) Title() string {
+   if v := m.MovieShortTitle; v != "" {
+      return v
+   }
+   return m.SecondaryTitle
+}
+
+type Metadata struct {
+   AirDate time.Time
+   EpisodeNumber int `json:",string"`
+   MovieShortTitle string
+   MpxAccountId int64 `json:",string"`
+   MpxGuid int64 `json:",string"`
+   ProgrammingType string
+   SeasonNumber int `json:",string"`
+   SecondaryTitle string
+   SeriesShortTitle string
+}
+
+func (m Metadata) Year() int {
+   return m.AirDate.Year()
+}
+
+// this is better than strings.Replace and strings.ReplaceAll
+func graphql_compact(s string) string {
+   f := strings.Fields(s)
+   return strings.Join(f, " ")
 }
 
 const query = `
@@ -62,29 +127,6 @@ query(
 }
 `
 
-func Core() Video {
-   var v Video
-   v.DrmProxySecret = "Whn8QFuLFM7Heiz6fYCYga7cYPM8ARe6"
-   v.DrmProxyUrl = func() string {
-      var b strings.Builder
-      b.WriteString("https://drmproxy.digitalsvc.apps.nbcuni.com")
-      b.WriteString("/drm-proxy/license")
-      return b.String()
-   }()
-   return v
-}
-
-func (Video) RequestHeader() (http.Header, error) {
-   head := make(http.Header)
-   head.Set("content-type", "application/octet-stream")
-   return head, nil
-}
-
-type Video struct {
-   DrmProxyUrl string
-   DrmProxySecret string
-}
-
 func (m Metadata) OnDemand() (*OnDemand, error) {
    req, err := http.NewRequest("", "https://lemonade.nbc.com", nil)
    if err != nil {
@@ -115,14 +157,6 @@ func (m Metadata) OnDemand() (*OnDemand, error) {
       return nil, err
    }
    return video, nil
-}
-
-func (Video) WrapRequest(b []byte) ([]byte, error) {
-   return b, nil
-}
-
-func (Video) UnwrapResponse(b []byte) ([]byte, error) {
-   return b, nil
 }
 
 type OnDemand struct {
--- nbc/nbc_test.go
+++ nbc/nbc_test.go
@@ -1,13 +1,35 @@
 package nbc
 
 import (
-   "154.pages.dev/widevine"
-   "encoding/hex"
+   "154.pages.dev/text"
    "fmt"
-   "os"
    "testing"
    "time"
 )
+
+var mpx_guids = []int{
+   // episode unlocked
+   // nbc.com/saturday-night-live/video/may-18-jake-gyllenhaal/9000283438
+   9000283422,
+   // movie locked
+   // nbc.com/2-fast-2-furious/video/2-fast-2-furious/2957739
+   2957739,
+}
+
+func TestMetadata(t *testing.T) {
+   for _, mpx_guid := range mpx_guids {
+      meta, err := NewMetadata(mpx_guid)
+      if err != nil {
+         t.Fatal(err)
+      }
+      name, err := text.Name(meta)
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%q\n", name)
+      time.Sleep(time.Second)
+   }
+}
 
 func TestOnDemand(t *testing.T) {
    for _, mpx_guid := range mpx_guids {
@@ -22,41 +44,4 @@ func TestOnDemand(t *testing.T) {
       fmt.Printf("%+v\n", video)
       time.Sleep(time.Second)
    }
-}
-
-const key_id = "0552e44842654a4e81b326004be47be0"
-
-func TestLicense(t *testing.T) {
-   home, err := os.UserHomeDir()
-   if err != nil {
-      t.Fatal(err)
-   }
-   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
-   if err != nil {
-      t.Fatal(err)
-   }
-   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
-   if err != nil {
-      t.Fatal(err)
-   }
-   var pssh widevine.Pssh
-   pssh.KeyId, err = hex.DecodeString(key_id)
-   if err != nil {
-      t.Fatal(err)
-   }
-   var module widevine.Cdm
-   err = module.New(private_key, client_id, pssh.Encode())
-   if err != nil {
-      t.Fatal(err)
-   }
-   key, err := module.Key(Core(), pssh.KeyId)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%x\n", key)
-}
-
-func TestVideo(t *testing.T) {
-   v, ok := Core().RequestUrl()
-   fmt.Println(v, ok)
 }
