adab9f3e7c3e3cf94ab2841e3b8885363e021efa
3052 <spen+3052@protonmail.com>
2024-08-17 11:43:03 -0500

    +var tests = []struct{

--- hulu/authenticate_test.go
+++ hulu/authenticate_test.go
@@ -9,22 +9,6 @@ import (
    "time"
 )
 
-var tests = []struct{
-   content string
-   key_id string
-   url string
-}{
-   {
-      content: "episode",
-      key_id: "21b82dc2ebb24d5aa9f8631f04726650",
-      url: "hulu.com/watch/023c49bf-6a99-4c67-851c-4c9e7609cc1d",
-   },
-   {
-      content: "film",
-      url: "hulu.com/watch/f70dfd4d-dbfb-46b8-abb3-136c841bba11",
-   },
-}
-
 func TestDetails(t *testing.T) {
    home, err := os.UserHomeDir()
    if err != nil {
@@ -71,4 +55,20 @@ func TestAuthenticate(t *testing.T) {
       t.Fatal(err)
    }
    os.WriteFile("authenticate.txt", auth.Marshal(), 0666)
+}
+
+var tests = []struct{
+   content string
+   key_id string
+   url string
+}{
+   {
+      content: "episode",
+      key_id: "21b82dc2ebb24d5aa9f8631f04726650",
+      url: "hulu.com/watch/023c49bf-6a99-4c67-851c-4c9e7609cc1d",
+   },
+   {
+      content: "film",
+      url: "hulu.com/watch/f70dfd4d-dbfb-46b8-abb3-136c841bba11",
+   },
 }
