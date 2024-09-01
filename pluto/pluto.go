97e6ec1a370797473da229ba564e3a6bcd26881c
3052 <spen+3052@protonmail.com>
2024-09-01 15:22:24 -0500

    +type Namer struct {

--- pluto/pluto.go
+++ pluto/pluto.go
@@ -10,6 +10,10 @@ import (
    "strings"
 )
 
+type Namer struct {
+   Video *OnDemand
+}
+
 var Base = []string{
    // these return `403 OK` with compressed content
    "http://siloh-fs.plutotv.net",
@@ -21,12 +25,42 @@ var Base = []string{
 }
 
 func (e *EpisodeClip) Dash() (*url.URL, bool) {
-   for _, s := range e.Sources {
-      if s.Type == "DASH" {
-         return &s.File.Url, true
+   for _, source := range e.Sources {
+      if source.Type == "DASH" {
+         return &source.File.Url, true
       }
    }
    return nil, false
+}
+
+func (n Namer) Season() int {
+   return n.Video.Slug.season
+}
+
+func (n Namer) Episode() int {
+   return n.Video.Slug.episode
+}
+
+func (n Namer) Title() string {
+   return n.Video.Name
+}
+
+func (n Namer) Year() int {
+   return n.Video.Slug.year
+}
+
+func (n Namer) Show() string {
+   if v := n.Video.parent; v != nil {
+      return v.parent.Name
+   }
+   return ""
+}
+
+///
+
+func (s *Slug) UnmarshalText(text []byte) error {
+   s.Slug = string(text)
+   return nil
 }
 
 // ex-machina-2015-1-1-ptv1
@@ -56,102 +90,6 @@ func (s *Slug) atoi() error {
    return nil
 }
 
-func (s *Slug) UnmarshalText(text []byte) error {
-   s.Slug = string(text)
-   return nil
-}
-
-type Season struct {
-   Episodes []*Video
-   parent   *Video
-}
-
-///
-
-func (a Address) Video(forward string) (*Video, error) {
-   req, err := http.NewRequest("", "https://boot.pluto.tv/v4/start", nil)
-   if err != nil {
-      return nil, err
-   }
-   if forward != "" {
-      req.Header.Set("x-forwarded-for", forward)
-   }
-   req.URL.RawQuery = url.Values{
-      "appName":           {"web"},
-      "appVersion":        {"9"},
-      "clientID":          {"9"},
-      "clientModelNumber": {"9"},
-      "drmCapabilities":   {"widevine:L3"},
-      "seriesIDs":         {a.series},
-   }.Encode()
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var data struct {
-      Vod []Video
-   }
-   err = json.NewDecoder(resp.Body).Decode(&data)
-   if err != nil {
-      return nil, err
-   }
-   demand := data.Vod[0]
-   if demand.Slug.Slug != a.series {
-      if demand.Id != a.series {
-         return nil, errors.New(demand.Slug.Slug)
-      }
-   }
-   for _, s := range demand.Seasons {
-      s.parent = &demand
-      for _, episode := range s.Episodes {
-         err := episode.Slug.atoi()
-         if err != nil {
-            return nil, err
-         }
-         episode.parent = s
-         if episode.Episode == a.episode {
-            return episode, nil
-         }
-         if episode.Slug.Slug == a.episode {
-            return episode, nil
-         }
-      }
-   }
-   err = demand.Slug.atoi()
-   if err != nil {
-      return nil, err
-   }
-   return &demand, nil
-}
-
-func (n Namer) Show() string {
-   if v := n.V.parent; v != nil {
-      return v.parent.Name
-   }
-   return ""
-}
-
-type Namer struct {
-   V *Video
-}
-
-func (n Namer) Season() int {
-   return n.V.Slug.season
-}
-
-func (n Namer) Episode() int {
-   return n.V.Slug.episode
-}
-
-func (n Namer) Title() string {
-   return n.V.Name
-}
-
-func (n Namer) Year() int {
-   return n.V.Slug.year
-}
-
 func (a Address) String() string {
    var b strings.Builder
    if a.series != "" {
@@ -174,15 +112,6 @@ func (a Address) String() string {
 type Address struct {
    series  string
    episode string
-}
-
-type Video struct {
-   Name    string
-   Seasons []*Season
-   parent  *Season
-   Slug    Slug
-   Episode string `json:"_id"`
-   Id      string
 }
 
 type Slug struct {
@@ -248,7 +177,16 @@ func (u *Url) UnmarshalText(text []byte) error {
    return u.Url.UnmarshalBinary(text)
 }
 
-func (v Video) Clip() (*EpisodeClip, error) {
+type OnDemand struct {
+   Name    string
+   Seasons []*Season
+   parent  *Season
+   Slug    Slug
+   Episode string `json:"_id"`
+   Id      string
+}
+
+func (o OnDemand) Clip() (*EpisodeClip, error) {
    req, err := http.NewRequest("", "http://api.pluto.tv", nil)
    if err != nil {
       return nil, err
@@ -256,10 +194,10 @@ func (v Video) Clip() (*EpisodeClip, error) {
    req.URL.Path = func() string {
       var b strings.Builder
       b.WriteString("/v2/episodes/")
-      if v.Id != "" {
-         b.WriteString(v.Id)
+      if o.Id != "" {
+         b.WriteString(o.Id)
       } else {
-         b.WriteString(v.Episode)
+         b.WriteString(o.Episode)
       }
       b.WriteString("/clips.json")
       return b.String()
@@ -278,4 +216,66 @@ func (v Video) Clip() (*EpisodeClip, error) {
       return nil, err
    }
    return &clips[0], nil
+}
+
+type Season struct {
+   Episodes []*OnDemand
+   parent   *OnDemand
+}
+
+func (a Address) Video(forward string) (*OnDemand, error) {
+   req, err := http.NewRequest("", "https://boot.pluto.tv/v4/start", nil)
+   if err != nil {
+      return nil, err
+   }
+   if forward != "" {
+      req.Header.Set("x-forwarded-for", forward)
+   }
+   req.URL.RawQuery = url.Values{
+      "appName":           {"web"},
+      "appVersion":        {"9"},
+      "clientID":          {"9"},
+      "clientModelNumber": {"9"},
+      "drmCapabilities":   {"widevine:L3"},
+      "seriesIDs":         {a.series},
+   }.Encode()
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   var data struct {
+      Vod []OnDemand
+   }
+   err = json.NewDecoder(resp.Body).Decode(&data)
+   if err != nil {
+      return nil, err
+   }
+   demand := data.Vod[0]
+   if demand.Slug.Slug != a.series {
+      if demand.Id != a.series {
+         return nil, errors.New(demand.Slug.Slug)
+      }
+   }
+   for _, s := range demand.Seasons {
+      s.parent = &demand
+      for _, episode := range s.Episodes {
+         err := episode.Slug.atoi()
+         if err != nil {
+            return nil, err
+         }
+         episode.parent = s
+         if episode.Episode == a.episode {
+            return episode, nil
+         }
+         if episode.Slug.Slug == a.episode {
+            return episode, nil
+         }
+      }
+   }
+   err = demand.Slug.atoi()
+   if err != nil {
+      return nil, err
+   }
+   return &demand, nil
 }
