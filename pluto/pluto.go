70d1ee2f87381387b85c3ddc94a24a69f17d911c
3052 <spen+3052@protonmail.com>
2024-09-01 19:17:31 -0500

    +func (n Namer) Show() string {

--- pluto/pluto.go
+++ pluto/pluto.go
@@ -30,13 +30,6 @@ type Namer struct {
    Video *OnDemand
 }
 
-func (n Namer) Show() string {
-   if v := n.Video.season; v != nil {
-      return v.show.Name
-   }
-   return ""
-}
-
 func (Namer) Season() int {
    return 0
 }
@@ -49,10 +42,6 @@ func (Namer) Year() int {
    return 0
 }
 
-func (n Namer) Title() string {
-   return n.Video.Slug
-}
-
 var Base = []struct{
    Scheme string
    Host string
@@ -63,6 +52,17 @@ var Base = []struct{
    {"http", "siloh-fs.plutotv.net", "403 OK"},
    {"http", "siloh-ns1.plutotv.net", "403 OK"},
    {"http", "silo-hybrik.pluto.tv.s3.amazonaws.com", "200 OK"},
+}
+
+func (n Namer) Show() string {
+   if v := n.Video.season; v != nil {
+      return v.show.Name
+   }
+   return ""
+}
+
+func (n Namer) Title() string {
+   return n.Video.Slug
 }
 
 func (e *EpisodeClip) Dash() (*Url, bool) {
@@ -80,6 +80,122 @@ type EpisodeClip struct {
       Type string
    }
 }
+
+type Url struct {
+   Url *url.URL
+}
+
+func (u *Url) UnmarshalText(text []byte) error {
+   u.Url = &url.URL{}
+   return u.Url.UnmarshalBinary(text)
+}
+
+func (a Address) String() string {
+   var b strings.Builder
+   if a[0] != "" {
+      if a[1] != "" {
+         b.WriteString("series/")
+         b.WriteString(a[0])
+         b.WriteString("/episode/")
+         b.WriteString(a[1])
+      } else {
+         b.WriteString("movies/")
+         b.WriteString(a[0])
+      }
+   }
+   return b.String()
+}
+
+func (a Address) Set(text string) error {
+   for {
+      var (
+         key string
+         ok  bool
+      )
+      key, text, ok = strings.Cut(text, "/")
+      if !ok {
+         return nil
+      }
+      switch key {
+      case "movies":
+         a[0] = text
+      case "series":
+         a[0], text, ok = strings.Cut(text, "/")
+         if !ok {
+            return errors.New("episode")
+         }
+      case "episode":
+         a[1] = text
+      }
+   }
+}
+
+type Address [2]string
+
+type VideoSeason struct {
+   Episodes []*OnDemand
+   show   *OnDemand
+}
+
+func (a Address) Video(forward string) (*OnDemand, error) {
+   req, err := http.NewRequest("", "https://boot.pluto.tv/v4/start", nil)
+   if err != nil {
+      return nil, err
+   }
+   if forward != "" {
+      req.Header.Set("x-forwarded-for", forward)
+   }
+   req.URL.RawQuery = url.Values{
+      "appName":           {"web"},
+      "appVersion":        {"9"},
+      "clientID":          {"9"},
+      "clientModelNumber": {"9"},
+      "drmCapabilities":   {"widevine:L3"},
+      "seriesIDs":         {a[0]},
+   }.Encode()
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   var value struct {
+      Vod []OnDemand
+   }
+   err = json.NewDecoder(resp.Body).Decode(&value)
+   if err != nil {
+      return nil, err
+   }
+   demand := value.Vod[0]
+   if demand.Slug != a[0] {
+      if demand.Id != a[0] {
+         return nil, errors.New(demand.Slug)
+      }
+   }
+   for _, season := range demand.Seasons {
+      season.show = &demand
+      for _, episode := range season.Episodes {
+         episode.season = season
+         if episode.Episode == a[1] {
+            return episode, nil
+         }
+         if episode.Slug == a[1] {
+            return episode, nil
+         }
+      }
+   }
+   return &demand, nil
+}
+
+type OnDemand struct {
+   Episode string `json:"_id"`
+   Id      string
+   Name    string
+   Seasons []*VideoSeason
+   Slug    string
+   season  *VideoSeason
+}
+
+///
 
 func (o OnDemand) Clip() (*EpisodeClip, error) {
    req, err := http.NewRequest("", "http://api.pluto.tv", nil)
@@ -111,123 +227,4 @@ func (o OnDemand) Clip() (*EpisodeClip, error) {
       return nil, err
    }
    return &clips[0], nil
-}
-
-type Url struct {
-   Url *url.URL
-}
-
-func (u *Url) UnmarshalText(text []byte) error {
-   u.Url = &url.URL{}
-   return u.Url.UnmarshalBinary(text)
-}
-
-type Season struct {
-   Episodes []*OnDemand
-   show   *OnDemand
-}
-
-type OnDemand struct {
-   Episode string `json:"_id"`
-   Id      string
-   Name    string
-   Seasons []*Season
-   Slug    string
-   season  *Season
-}
-
-///
-
-func (a Address) Video(forward string) (*OnDemand, error) {
-   req, err := http.NewRequest("", "https://boot.pluto.tv/v4/start", nil)
-   if err != nil {
-      return nil, err
-   }
-   if forward != "" {
-      req.Header.Set("x-forwarded-for", forward)
-   }
-   req.URL.RawQuery = url.Values{
-      "appName":           {"web"},
-      "appVersion":        {"9"},
-      "clientID":          {"9"},
-      "clientModelNumber": {"9"},
-      "drmCapabilities":   {"widevine:L3"},
-      "seriesIDs":         {a.Series},
-   }.Encode()
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var data struct {
-      Vod []OnDemand
-   }
-   err = json.NewDecoder(resp.Body).Decode(&data)
-   if err != nil {
-      return nil, err
-   }
-   demand := data.Vod[0]
-   if demand.Slug != a.Series {
-      if demand.Id != a.Series {
-         return nil, errors.New(demand.Slug)
-      }
-   }
-   for _, season := range demand.Seasons {
-      season.show = &demand
-      for _, episode := range season.Episodes {
-         episode.season = season
-         if episode.Episode == a.Episode {
-            return episode, nil
-         }
-         if episode.Slug == a.Episode {
-            return episode, nil
-         }
-      }
-   }
-   return &demand, nil
-}
-
-type Address struct {
-   Series  string
-   Episode string
-}
-
-func (a *Address) String() string {
-   var b strings.Builder
-   if a.Series != "" {
-      if a.Episode != "" {
-         b.WriteString("series/")
-         b.WriteString(a.Series)
-         b.WriteString("/episode/")
-         b.WriteString(a.Episode)
-      } else {
-         b.WriteString("movies/")
-         b.WriteString(a.Series)
-      }
-   }
-   return b.String()
-}
-
-func (a *Address) Set(text string) error {
-   for {
-      var (
-         key string
-         ok  bool
-      )
-      key, text, ok = strings.Cut(text, "/")
-      if !ok {
-         return nil
-      }
-      switch key {
-      case "episode":
-         a.Episode = text
-      case "movies":
-         a.Series = text
-      case "series":
-         a.Series, text, ok = strings.Cut(text, "/")
-         if !ok {
-            return errors.New("episode")
-         }
-      }
-   }
 }
