4b864bfabc7a6b0e8bd25ab39e9f30fd63430fd8
3052 <spen+3052@protonmail.com>
2024-09-07 22:13:40 -0500

    deleted:    pluto/address.go

--- pluto/pluto_test.go
+++ pluto/pluto_test.go
@@ -1,6 +1,7 @@
 package pluto
 
 import (
+   "154.pages.dev/text"
    "154.pages.dev/widevine"
    "encoding/hex"
    "fmt"
@@ -8,6 +9,68 @@ import (
    "testing"
    "time"
 )
+
+func TestAddress(t *testing.T) {
+   for _, test := range video_tests {
+      var web Address
+      err := web.Set(test.url)
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Println(web)
+   }
+}
+
+func TestVideo(t *testing.T) {
+   for _, test := range video_tests {
+      var web Address
+      web.Set(test.url)
+      video, err := web.Video("")
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%+v\n", video)
+      name, err := text.Name(Namer{video})
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%q\n", name)
+      time.Sleep(time.Second)
+   }
+}
+
+func TestLicense(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   for _, test := range video_tests {
+      var pssh widevine.Pssh
+      pssh.KeyId, err = hex.DecodeString(test.key_id)
+      if err != nil {
+         t.Fatal(err)
+      }
+      var module widevine.Cdm
+      err = module.New(private_key, client_id, pssh.Marshal())
+      if err != nil {
+         t.Fatal(err)
+      }
+      key, err := module.Key(Poster{}, pssh.KeyId)
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%x\n", key)
+      time.Sleep(time.Second)
+   }
+}
 
 func TestClip(t *testing.T) {
    for _, test := range video_tests {
@@ -48,37 +111,4 @@ var video_tests = []struct {
       key_id: "000000006358c035248b647dad3c09ad",
       url:    "pluto.tv/on-demand/series/frasier-cbs-tv/season/1/episode/space-quest-1992-1-2",
    },
-}
-
-func TestLicense(t *testing.T) {
-   home, err := os.UserHomeDir()
-   if err != nil {
-      t.Fatal(err)
-   }
-   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
-   if err != nil {
-      t.Fatal(err)
-   }
-   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
-   if err != nil {
-      t.Fatal(err)
-   }
-   for _, test := range video_tests {
-      var pssh widevine.Pssh
-      pssh.KeyId, err = hex.DecodeString(test.key_id)
-      if err != nil {
-         t.Fatal(err)
-      }
-      var module widevine.Cdm
-      err = module.New(private_key, client_id, pssh.Marshal())
-      if err != nil {
-         t.Fatal(err)
-      }
-      key, err := module.Key(Poster{}, pssh.KeyId)
-      if err != nil {
-         t.Fatal(err)
-      }
-      fmt.Printf("%x\n", key)
-      time.Sleep(time.Second)
-   }
 }
