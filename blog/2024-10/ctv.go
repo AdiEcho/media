5c581b64373389884c820dc298b24fe02e0c7653
3052 <spen+3052@protonmail.com>
2024-10-19 13:14:33 -0500

    +  fragment AxisPlaybackData on AxisPlayback {

--- blog/2024-10/ctv.go
+++ blog/2024-10/ctv.go
@@ -41,127 +41,6 @@ func main() {
 }
 
 const data = `
-  fragment AuthConstraintsData on AuthConstraint {
-    authRequired
-    packageName
-    endDate
-    language
-    startDate
-    subscriptionName
-    __typename
-  }
-
-  fragment AxisAdUnitData on AxisAdUnit {
-    adultAudience
-    heroBrand
-    pageType
-    product
-    revShare
-    title
-    analyticsTitle
-    keyValue {
-      webformType
-      adTarget
-      contentType
-      mediaType
-      pageTitle
-      revShare
-      subType
-    }
-  }
-
-  fragment AxisPlaybackData on AxisPlayback {
-    destinationCode
-    language
-    duration
-    playbackIndicators
-    partOfMultiLanguagePlayback
-  }
-
-  fragment LinkData on Link {
- buttonStyle
-    urlParameters
-    renderAs
-    linkType
-    linkLabel
-    longLinkLabel
-    linkTarget
-    userMgmtLinkType
-    url
-    id
-    showLinkLabel
-    internalContent {
-      title
-      __typename
-      ... on AxisContent {
-        axisId
-        authConstraints {
-          ...AuthConstraintsData
-        }
-        agvotCode
-      }
-      ... on AceWebContent {
-        path
-        pathSegment
-        __typename
-      }
-      ... on Section {
-        containerType
-        path
-      }
-      ... on AxisObject {
-        axisId
-        title
-      }
-      ... on TabItem {
-        # id
-        sectionPath
-      }
-    }
-    hoverImage {
-      title
-      imageType
-      url
-    }
-    image {
-      id
-      width
-      height
-      title
-      url
-      altText
-    }
-    bannerImages {
-      breakPoint
- image {
-        id
-        title
-        url
-        altText
-      }
-    }
-    __typename
-  }
-  
-
-  fragment RotatorConfigData on RotatorConfig {
-    displayTitle
-    displayTotalItemCount
-    displayDots
-    style
-    imageFormat
-    lightbox
-    carousel
-    titleLinkMode
-    maxItems
-    disableBadges
-    customTitleLink {
-      ...LinkData
-    }
-    hideMediaTitle
-  }
-  
-
   query axisContent(
     $id: ID!
     $subscriptions: [Subscription]!
@@ -179,73 +58,17 @@ const data = `
   ) {
     axisContent(id: $id) {
       axisId
-      id
-      path
-      title
-      duration
-      agvotCode
-      description
-      episodeNumber
-      seasonNumber
-      pathSegment
-      genres {
-        name
-      }
-      axisMedia {
-        heroBrandLogoId
-        id
-        title
-      }
-      adUnit {
-        ...AxisAdUnitData
-      }
-      authConstraints {
-        ...AuthConstraintsData
-      }
       axisPlaybackLanguages {
         ...AxisPlaybackData
       }
-      originalSpokenLanguage
-      ogFields {
-        ogDescription
-        ogImages {
-          url
-        }
-        ogTitle
-      }
-      playbackMetadata {
-        indicator
-        languages {
-          languageCode
-          languageDisplayName
-        }
-      }
-      seoFields {
-        seoDescription
-        seoTitle
-        seoKeywords
-        canonicalUrl
-      }
-      badges {
-        title
-        label
-      }
-      posterImages: images(formats: POSTER) {
-        url
-      }
-      broadcastDate
-      expiresOn
-      startsOn
-      keywords
-      videoPageLayout {
-        __typename
-        ... on Rotator {
-          id
-          config {
-            ...RotatorConfigData
-          }
-        }
-      }
     }
+  }
+
+  fragment AxisPlaybackData on AxisPlayback {
+    destinationCode
+    language
+    duration
+    playbackIndicators
+    partOfMultiLanguagePlayback
   }
 `
