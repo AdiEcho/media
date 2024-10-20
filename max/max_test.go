dc12af59cf059b70f0de2ae761e9d05fd1a186cd
3052 <spen+3052@protonmail.com>
2024-10-20 11:59:24 -0500

    renamed:    blog/itv/FILM.json -> blog/itv/film.json

--- max/max_test.go
+++ max/max_test.go
@@ -10,6 +10,35 @@ import (
    "time"
 )
 
+func TestLogin(t *testing.T) {
+   var login DefaultLogin
+   login.Credentials.Username = os.Getenv("max_username")
+   if login.Credentials.Username == "" {
+      t.Fatal("Getenv")
+   }
+   login.Credentials.Password = os.Getenv("max_password")
+   var key PublicKey
+   err := key.New()
+   if err != nil {
+      t.Fatal(err)
+   }
+   var token DefaultToken
+   err = token.New()
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = token.Unmarshal()
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = token.Login(key, login)
+   if err != nil {
+      t.Fatal(err)
+   }
+   os.WriteFile("session.txt", token.Session.Raw, os.ModePerm)
+   os.WriteFile("token.txt", token.Token.Raw, os.ModePerm)
+}
+
 func TestConfig(t *testing.T) {
    var token DefaultToken
    err := token.New()
