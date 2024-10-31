768c1bf410ee5e4867e09c4f25abc644d039f427
3052 <spen+3052@protonmail.com>
2024-10-31 05:40:10 -0500

    +   data, err := os.ReadFile(f.base() + "/play.txt")

--- internal/cine-member/cine-member.go
+++ internal/cine-member/cine-member.go
@@ -11,61 +11,13 @@ import (
    "path"
 )
 
-func (f *flags) write_user() error {
-   var user member.OperationUser
-   err := user.New(f.email, f.password)
-   if err != nil {
-      return err
-   }
-   return os.WriteFile(f.home + "/cine-member.txt", user.Raw, os.ModePerm)
-}
-
-func (f *flags) write_play() error {
-   os.Mkdir(f.base(), os.ModePerm)
-   // 1. write OperationArticle
-   article, err := f.address.Article()
-   if err != nil {
-      return err
-   }
-   err = os.WriteFile(f.base() + "/article.txt", article.Raw, os.ModePerm)
-   if err != nil {
-      return err
-   }
-   err = article.Unmarshal()
-   if err != nil {
-      return err
-   }
-   // 2. write OperationPlay
-   asset, ok := article.Film()
-   if !ok {
-      return errors.New("OperationArticle.Film")
-   }
-   var user member.OperationUser
-   user.Raw, err = os.ReadFile(f.home + "/cine-member.txt")
-   if err != nil {
-      return err
-   }
-   err = user.Unmarshal()
-   if err != nil {
-      return err
-   }
-   play, err := user.Play(asset)
-   if err != nil {
-      return err
-   }
-   return os.WriteFile(f.base() + "/play.txt", play.Raw, os.ModePerm)
-}
-
 func (f *flags) download() error {
-   var (
-      play member.OperationPlay
-      err error
-   )
-   play.Raw, err = os.ReadFile(f.base() + "/play.txt")
+   data, err := os.ReadFile(f.base() + "/play.txt")
    if err != nil {
       return err
    }
-   err = play.Unmarshal()
+   var play member.OperationPlay
+   err = play.Unmarshal(data)
    if err != nil {
       return err
    }
@@ -78,7 +30,7 @@ func (f *flags) download() error {
       return err
    }
    defer resp.Body.Close()
-   data, err := io.ReadAll(resp.Body)
+   data, err = io.ReadAll(resp.Body)
    if err != nil {
       return err
    }
@@ -91,12 +43,12 @@ func (f *flags) download() error {
       case "":
          fmt.Print(rep, "\n\n")
       case rep.Id:
-         var article member.OperationArticle
-         article.Raw, err = os.ReadFile(f.base() + "/article.txt")
+         data, err = os.ReadFile(f.base() + "/article.txt")
          if err != nil {
             return err
          }
-         err = article.Unmarshal()
+         var article member.OperationArticle
+         err = article.Unmarshal(data)
          if err != nil {
             return err
          }
@@ -109,4 +61,51 @@ func (f *flags) download() error {
 
 func (f *flags) base() string {
    return path.Base(f.address.Path)
+}
+
+func (f *flags) write_user() error {
+   var data []byte
+   err := (*member.OperationUser).New(nil, f.email, f.password, &data)
+   if err != nil {
+      return err
+   }
+   return os.WriteFile(f.home + "/cine-member.txt", data, os.ModePerm)
+}
+
+func (f *flags) write_play() error {
+   os.Mkdir(f.base(), os.ModePerm)
+   // 1. write OperationArticle
+   var data []byte
+   _, err := f.address.Article(&data)
+   if err != nil {
+      return err
+   }
+   err = os.WriteFile(f.base() + "/article.txt", data, os.ModePerm)
+   if err != nil {
+      return err
+   }
+   var article member.OperationArticle
+   err = article.Unmarshal(data)
+   if err != nil {
+      return err
+   }
+   // 2. write OperationPlay
+   data, err = os.ReadFile(f.home + "/cine-member.txt")
+   if err != nil {
+      return err
+   }
+   var user member.OperationUser
+   err = user.Unmarshal(data)
+   if err != nil {
+      return err
+   }
+   asset, ok := article.Film()
+   if !ok {
+      return errors.New("OperationArticle.Film")
+   }
+   _, err = user.Play(asset, &data)
+   if err != nil {
+      return err
+   }
+   return os.WriteFile(f.base() + "/play.txt", data, os.ModePerm)
 }
