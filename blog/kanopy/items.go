21f18f5a144b6448374f8f5a51048efc3174f810
3052 <spen+3052@protonmail.com>
2024-12-07 10:10:54 -0600

    +type video_items struct {

--- blog/kanopy/items.go
+++ blog/kanopy/items.go
@@ -1,11 +1,20 @@
 package kanopy
 
 import (
+   "encoding/json"
    "net/http"
    "strconv"
 )
 
-func (w *web_token) items(video_id int64) (*http.Response, error) {
+type video_items struct {
+   List []struct {
+      Video struct {
+         VideoId int
+      }
+   }
+}
+
+func (w *web_token) items(video_id int64) (*video_items, error) {
    req, err := http.NewRequest("", "https://www.kanopy.com", nil)
    if err != nil {
       return nil, err
@@ -21,5 +30,15 @@ func (w *web_token) items(video_id int64) (*http.Response, error) {
       "user-agent": {user_agent},
       "x-version": {x_version},
    }
-   return http.DefaultClient.Do(req)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   items := &video_items{}
+   err = json.NewDecoder(resp.Body).Decode(items)
+   if err != nil {
+      return nil, err
+   }
+   return items, nil
 }
