c70673b29497f0a814b075772c474e82d7ddb4ca
3052 <spen+3052@protonmail.com>
2024-12-19 18:16:43 -0600

    +   var pssh widevine.PsshData

--- rakuten/rakuten_test.go
+++ rakuten/rakuten_test.go
@@ -3,12 +3,83 @@ package rakuten
 import (
    "41.neocities.org/text"
    "41.neocities.org/widevine"
+   "bytes"
    "encoding/base64"
+   "errors"
    "fmt"
    "os"
+   "strings"
    "testing"
    "time"
 )
+
+func (m *movie_test) license() ([]byte, error) {
+   var web Address
+   web.Set(m.url)
+   info, err := web.Hd().Info()
+   if err != nil {
+      return nil, err
+   }
+   home, err := os.UserHomeDir()
+   if err != nil {
+      return nil, err
+   }
+   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
+   if err != nil {
+      return nil, err
+   }
+   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
+   if err != nil {
+      return nil, err
+   }
+   var pssh widevine.PsshData
+   pssh.ContentId, err = base64.StdEncoding.DecodeString(m.content_id)
+   if err != nil {
+      return nil, err
+   }
+   pssh.KeyId, err = base64.StdEncoding.DecodeString(m.key_id)
+   if err != nil {
+      return nil, err
+   }
+   var module widevine.Cdm
+   err = module.New(private_key, client_id, pssh.Marshal())
+   if err != nil {
+      return nil, err
+   }
+   data, err := module.RequestBody()
+   if err != nil {
+      return nil, err
+   }
+   data, err = info.Wrap(data)
+   if err != nil {
+      return nil, err
+   }
+   var body widevine.ResponseBody
+   err = body.Unmarshal(data)
+   if err != nil {
+      return nil, err
+   }
+   block, err := module.Block(body)
+   if err != nil {
+      return nil, err
+   }
+   containers := body.Container()
+   for {
+      container, ok := containers()
+      if !ok {
+         return nil, errors.New("ResponseBody.Container")
+      }
+      if bytes.Equal(container.Id(), pssh.KeyId) {
+         return container.Decrypt(block), nil
+      }
+   }
+}
+
+type movie_test struct {
+   content_id string
+   key_id     string
+   url        string
+}
 
 func TestMovie(t *testing.T) {
    for _, test := range tests {
@@ -28,93 +99,71 @@ func TestMovie(t *testing.T) {
    }
 }
 
-type movie_test struct {
-   content_id string
-   key_id     string
-   url        string
-}
-
-var tests = map[string]movie_test{
-   "fr": {
+var tests = []movie_test{
+   {
       content_id: "MGU1MTgwMDA2Y2Q1MDhlZWMwMGQ1MzVmZWM2YzQyMGQtbWMtMC0xNDEtMC0w",
       key_id:     "DlGAAGzVCO7ADVNf7GxCDQ==",
       url:        "rakuten.tv/fr/movies/infidele",
    },
-   "se": {
+   {
       content_id: "OWE1MzRhMWYxMmQ2OGUxYTIzNTlmMzg3MTBmZGRiNjUtbWMtMC0xNDctMC0w",
       key_id:     "mlNKHxLWjhojWfOHEP3bZQ==",
       url:        "rakuten.tv/se/movies/i-heart-huckabees",
    },
 }
 
-func (m movie_test) license() ([]byte, error) {
-   home, err := os.UserHomeDir()
-   if err != nil {
-      return nil, err
-   }
-   private_key, err := os.ReadFile(home + "/widevine/private_key.pem")
-   if err != nil {
-      return nil, err
-   }
-   client_id, err := os.ReadFile(home + "/widevine/client_id.bin")
-   if err != nil {
-      return nil, err
-   }
-   var pssh widevine.Pssh
-   pssh.ContentId, err = base64.StdEncoding.DecodeString(m.content_id)
-   if err != nil {
-      return nil, err
-   }
-   pssh.KeyId, err = base64.StdEncoding.DecodeString(m.key_id)
-   if err != nil {
-      return nil, err
-   }
-   var module widevine.Module
-   err = module.New(private_key, client_id, pssh.Marshal())
-   if err != nil {
-      return nil, err
-   }
-   var web Address
-   web.Set(m.url)
-   info, err := web.Hd().Info()
-   if err != nil {
-      return nil, err
-   }
-   return module.Key(info, pssh.KeyId)
-}
-
 func TestFr(t *testing.T) {
-   var web Address
-   web.Set(tests["fr"].url)
-   stream, err := web.Fhd().Info()
-   if err != nil {
-      t.Fatal(err)
+   for _, test := range tests {
+      if strings.Contains(test.url, "/fr/") {
+         var web Address
+         web.Set(test.url)
+         stream, err := web.Fhd().Info()
+         if err != nil {
+            t.Fatal(err)
+         }
+         fmt.Printf("%+v\n", stream)
+         time.Sleep(time.Second)
+      }
    }
-   fmt.Printf("%+v\n", stream)
-}
-
-func TestLicenseFr(t *testing.T) {
-   key, err := tests["fr"].license()
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%x\n", key)
 }
 
 func TestSe(t *testing.T) {
-   var web Address
-   web.Set(tests["se"].url)
-   stream, err := web.Fhd().Info()
-   if err != nil {
-      t.Fatal(err)
+   for _, test := range tests {
+      if strings.Contains(test.url, "/se/") {
+         var web Address
+         web.Set(test.url)
+         stream, err := web.Fhd().Info()
+         if err != nil {
+            t.Fatal(err)
+         }
+         fmt.Printf("%+v\n", stream)
+         time.Sleep(time.Second)
+      }
    }
-   fmt.Printf("%+v\n", stream)
+}
+
+func TestLicenseFr(t *testing.T) {
+   for _, test := range tests {
+      if strings.Contains(test.url, "/fr/") {
+         key, err := test.license()
+         if err != nil {
+            t.Fatal(err)
+         }
+         fmt.Printf("%x\n", key)
+         time.Sleep(time.Second)
+      }
+   }
 }
 
 func TestLicenseSe(t *testing.T) {
-   key, err := tests["se"].license()
-   if err != nil {
-      t.Fatal(err)
+   for _, test := range tests {
+      if strings.Contains(test.url, "/se/") {
+         key, err := test.license()
+         if err != nil {
+            t.Fatal(err)
+         }
+         fmt.Printf("%x\n", key)
+         time.Sleep(time.Second)
+      }
    }
-   fmt.Printf("%x\n", key)
 }
