954a8c5b9c490ef5f34602a1a7c734b81f3d6e6a
3052 <spen+3052@protonmail.com>
2024-12-21 07:46:26 -0600

    new file:   blog/2024-12/ssc3CuuS4mrQ7EyVXILH0FEQSi5yBAsA.json

--- blog/2024-12/paramount.go
+++ blog/2024-12/paramount.go
@@ -6,7 +6,10 @@ import (
    "os"
 )
 
-const content_id = "rZ59lcp4i2fU4dAaZJ_iEgKqVg_ogrIf"
+const (
+   at = "ABAAAAAAAAAAAAAAAAAAAAAAU/Emq2DGAxNGYi71fZdi2mb6UTU2+elcD3bGEhs6bo4="
+   content_id = "ssc3CuuS4mrQ7EyVXILH0FEQSi5yBAsA"
+)
 
 func main() {
    var req http.Request
@@ -15,7 +18,7 @@ func main() {
    req.URL.Host = "www.paramountplus.com"
    req.URL.Path = "/apps-api/v2.0/androidphone/video/cid/"+content_id+".json"
    value := url.Values{}
-   value["at"] = []string{"ABAAAAAAAAAAAAAAAAAAAAAA+c9AiUS4F1JOX0w0O1uzQw/+qdAuVgybB1FK7aqonjY="}
+   value["at"] = []string{at}
    req.URL.RawQuery = value.Encode()
    req.URL.Scheme = "https"
    resp, err := http.DefaultClient.Do(&req)
@@ -23,6 +26,9 @@ func main() {
       panic(err)
    }
    defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      panic(resp.Status)
+   }
    file, err := os.Create(content_id + ".json")
    if err != nil {
       panic(err)
